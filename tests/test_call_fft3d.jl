2d50fa05560a78b9965511935effb0e879681883
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-02-20 00:23:32 +0700

    New print out for test call fft3d

--- tests/test_call_fft3d.jl
+++ tests/test_call_fft3d.jl
@@ -4,14 +4,18 @@ function test_multicolumn(Ns)
     Nstates = 4
     in1 = rand(Complex128,prod(Ns),Nstates)
 
+    println("\n\nMulticolumn version")
+    println("Data size:", Ns)
+
+    println("\nC version")
     @time out1c = c_R_to_G(Ns,in1)
     @time in2c = c_G_to_R(Ns,out1c)
 
+    println("\nNaive version")
     @time out1 = R_to_G(Ns,in1)
     @time in2 = G_to_R(Ns,out1)
 
-    println("\nMulticolumn")
-    println("Data size:", Ns)
+    println("\nCheck")
     println("diff out1 = ", sum( abs.(out1c - out1) ))
     println("diff out1 = ", sum( abs.(in2c - in2) ))
 end
@@ -19,14 +23,18 @@ end
 function test_singlecolumn(Ns)
     in1 = rand(Complex128,prod(Ns))
 
+    println("\n\nSingle column version")
+    println("Data size:", Ns)
+
+    println("\nC version")
     @time out1c = c_R_to_G(Ns,in1)
     @time in2c = c_G_to_R(Ns,out1c)
 
+    println("\nNaive version")
     @time out1 = R_to_G(Ns,in1)
     @time in2 = G_to_R(Ns,out1)
 
-    println("\nSingle column")
-    println("Data size:", Ns)
+    println("\nCheck")
     println("diff out1 = ", sum( abs.(out1c - out1) ))
     println("diff out1 = ", sum( abs.(in2c - in2) ))
 end
@@ -37,3 +45,5 @@ test_multicolumn([5,5,5])
 test_singlecolumn([45,45,45])
 test_multicolumn([45,45,45])
 
+test_singlecolumn([60,60,60])
+test_multicolumn([60,60,60])
