5dabe47e69d25bf8bd1b073fdcb1fe495afd7798
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-02-20 20:04:56 +0700

    Implement and test_calc_grad

---
+++ tests/calc_grad.jl
@@ -0,0 +1,24 @@
+function calc_grad( Ham::PWHamiltonian, psi::Array{Complex128,2} )
+
+    Potentials = Ham.potentials
+    Focc = Ham.focc
+    pw = Ham.pw
+    #
+    Ngwx    = size(psi)[1]
+    Nstates = size(psi)[2]
+    Ω = pw.Ω
+    Ns = pw.Ns
+    #
+    grad = zeros( Complex128, Ngwx, Nstates )
+
+    H_psi = op_H( Ham, psi )
+    for ist = 1:Nstates
+        grad[:,ist] = H_psi[:,ist]
+        for jst = 1:Nstates
+            grad[:,ist] = grad[:,ist] - dot( psi[:,jst], H_psi[:,ist] ) * psi[:,jst]
+        end
+        grad[:,ist] = Focc[ist]*grad[:,ist]
+    end
+    return grad
+
+end
---
+++ tests/test_calc_grad.jl
@@ -0,0 +1,39 @@
+using PWDFT
+
+include("PWHamiltonian.jl")
+include("ortho_gram_schmidt.jl")
+include("calc_rhoe.jl")
+include("calc_energies.jl")
+include("calc_grad.jl")
+
+function test_main()
+    #
+    atoms = init_atoms_xyz("H.xyz")
+    println(atoms)
+    #
+    LatVecs = 16.0*diagm(ones(3))
+    pw = PWGrid(30.0, LatVecs)
+    println(pw)
+    #
+    Ham = PWHamiltonian( pw, atoms )
+    #
+    Ngwx = Ham.pw.gvecw.Ngwx
+    Nstates = 1
+    Focc = [1.0]
+    Ham.focc = Focc
+    #
+    srand(1234)
+    psi = rand(Ngwx,Nstates) + im*rand(Ngwx,Nstates)
+    psi = ortho_gram_schmidt(psi)
+    #
+    rhoe = calc_rhoe( pw, Focc, psi )
+    @printf("Integ rhoe = %18.10f\n", sum(rhoe)*pw.Ω/prod(pw.Ns))
+    #
+    update!(Ham, rhoe)
+    #
+    g = calc_grad(Ham, psi)
+    s = sum(g)
+    @printf("sum g = (%18.10f,%18.10fim)\n", s.re, s.im)
+end
+
+test_main()
