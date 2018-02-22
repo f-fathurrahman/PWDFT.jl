e0ba3182d27e3f7b9dc9160aa83047a9f1cc48d4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-02-22 16:15:54 +0700

    Rename field symbols to atsymbs

--- src/Atoms.jl
+++ src/Atoms.jl
@@ -3,7 +3,7 @@ mutable struct Atoms
     Nspecies::Int64
     positions::Array{Float64,2}
     atm2species::Array{Int64,1}
-    symbols::Array{String,1}
+    atsymbs::Array{String,1}
     SpeciesSymbols::Array{String,1}  # unique symbols
     Zvals::Array{Float64,1}
 end
@@ -16,7 +16,7 @@ function println( a::Atoms )
     @printf("Nspecies = %5d\n", a.Nspecies)
     @printf("\nCoordinates in bohr:\n\n")
     for ia = 1:a.Natoms
-        @printf("%5s %18.10f %18.10f %18.10f\n", a.symbols[ia],
+        @printf("%5s %18.10f %18.10f %18.10f\n", a.atsymbs[ia],
                 a.positions[1,ia], a.positions[2,ia], a.positions[3,ia])
     end
 end
@@ -27,11 +27,11 @@ function init_atoms_xyz(filexyz; in_bohr=false, verbose=false)
     l = readline(f)
     Natoms = parse(Int64, l)
     positions = zeros(3,Natoms)
-    symbols = Array{String}(Natoms)
+    atsymbs = Array{String}(Natoms)
     l = readline(f)
     for ia = 1:Natoms
         ll = split( readline(f) )
-        symbols[ia] = ll[1]
+        atsymbs[ia] = ll[1]
         positions[1,ia] = parse( Float64, ll[2] )
         positions[2,ia] = parse( Float64, ll[3] )
         positions[3,ia] = parse( Float64, ll[4] )
@@ -56,7 +56,7 @@ function init_atoms_xyz(filexyz; in_bohr=false, verbose=false)
     for ia = 1:Natoms
         k2 = 0
         for k1 = 1:ia-1
-            if symbols[k1] == symbols[ia]
+            if atsymbs[k1] == atsymbs[ia]
                 k2 = 1
             end
         end
@@ -72,14 +72,14 @@ function init_atoms_xyz(filexyz; in_bohr=false, verbose=false)
     for ia = 1:Natoms
         k2 = 0
         for k1 = 1:ia-1
-            if symbols[k1] == symbols[ia]
+            if atsymbs[k1] == atsymbs[ia]
                 k2 = 1
             end
         end
         # Found different species
         if k2==0
             idx1 = idx1 + 1
-            SpeciesSymbols[idx1] = symbols[ia]
+            SpeciesSymbols[idx1] = atsymbs[ia]
         end
     end
 
@@ -87,7 +87,7 @@ function init_atoms_xyz(filexyz; in_bohr=false, verbose=false)
     atm2species = Array{Int64}(Natoms)
     for ia = 1:Natoms
         for isp = 1:Nspecies
-            if symbols[ia] == SpeciesSymbols[isp]
+            if atsymbs[ia] == SpeciesSymbols[isp]
                 atm2species[ia] = isp
             end
         end
@@ -97,7 +97,7 @@ function init_atoms_xyz(filexyz; in_bohr=false, verbose=false)
     # It will be set later if using
     Zvals = zeros(Float64,Nspecies)
 
-    return Atoms(Natoms, Nspecies, positions, atm2species, symbols, SpeciesSymbols, Zvals )
+    return Atoms(Natoms, Nspecies, positions, atm2species, atsymbs, SpeciesSymbols, Zvals )
 
     #ALLOCATE( AtomicMasses(Nspecies) )
     #AtomicMasses(:) = 0.d0 ! FIXME: Use internal database to set this
