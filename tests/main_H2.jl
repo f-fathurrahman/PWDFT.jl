b56c0b646c78a9e0ff9eecaa4ac3b3d516ecf9a8
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-03-07 11:45:20 +0700

    Renaming again

--- tests/main_H2.jl
+++ tests/main_H2.jl
@@ -1,10 +1,6 @@
 using PWDFT
 
 function test_main( ; method="SCF" )
-    const LatVecs = 16.0*diagm( ones(3) )
-    ecutwfc_Ry = 40.0
-    pw = PWGrid( ecutwfc_Ry*0.5, LatVecs )
-    println(pw)
 
     #
     # Atoms
@@ -13,39 +9,36 @@ function test_main( ; method="SCF" )
     println(atoms)
 
     #
-    # Structure factor
-    #
-    strf = calc_strfact( atoms, pw )
-
-    #
     # Initialize Hamiltonian
     #
-    Ham = PWHamiltonian(pw)
-    update!(Ham, atoms, strf, ["../pseudopotentials/pade_gth/H-q1.gth"])
+    pspfiles = ["../pseudopotentials/pade_gth/H-q1.gth"]
+    LatVecs = 16.0*diagm( ones(3) )
+    ecutwfc_Ry = 60.0
+    Ham = PWHamiltonian( atoms, pspfiles, ecutwfc_Ry*0.5, LatVecs )
+
+
     println("sum V Ps loc = ", sum(Ham.potentials.Ps_loc))
 
     #
     # calculate E_NN
     #
-    Ham.energies.NN = calc_E_NN( pw, strf, atoms.positions, atoms.Nspecies, atoms.atm2species, [1.0])
+    strf = calc_strfact( atoms, Ham.pw )   # structure factor
+    Ham.energies.NN = calc_E_NN( Ham.pw, strf, atoms.positions, atoms.Nspecies, atoms.atm2species, [1.0])
 
     println("\nAfter calculating E_NN")
     println(Ham.energies)
 
-    # states
-    Nstates = 1
-    Ham.focc = [2.0]
-
     if method == "SCF"
-        λ, v = KS_solve_SCF!( Ham, Nstates )
+        λ, v = KS_solve_SCF!( Ham )
         println("\nAfter calling KS_solve_SCF:")
     elseif method == "Emin"
-        λ, v = KS_solve_Emin_PCG!( Ham, Nstates, I_CG_BETA=4 )
+        λ, v = KS_solve_Emin_PCG!( Ham, I_CG_BETA=4 )
         println("\nAfter calling KS_solve_Emin_PCG:")
     else
         println("ERROR: unknow method = ", method)
     end
 
+    Nstates = Ham.electrons.Nstates
     println("\nEigenvalues")
     for ist = 1:Nstates
         @printf("%8d  %18.10f\n", ist, λ[ist])
