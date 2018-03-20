1f86d4f89d04b870e6eea64fe700b6ea0fd61bd4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-03-20 22:27:58 +0700

    Add Vxc PBE

--- src/extlibs/build_xc.sh
+++ src/extlibs/build_xc.sh
@@ -3,6 +3,10 @@ INC=/home/efefer/mysoftwares/libxc-3.0.0/include
 
 gcc -c -I$INC -fPIC -O3 calc_Vxc_VWN.c
 gcc -c -I$INC -fPIC -O3 calc_epsxc_VWN.c
+gcc -c -I$INC -fPIC -O3 calc_Vxc_PBE.c
 
-gcc -shared -o libxc_interface.so -Wl,--whole-archive calc_Vxc_VWN.o calc_epsxc_VWN.o $LIBXC -Wl,--no-whole-archive
+gcc -shared -o libxc_interface.so -Wl,--whole-archive \
+calc_Vxc_VWN.o calc_epsxc_VWN.o \
+calc_Vxc_PBE.o \
+$LIBXC -Wl,--no-whole-archive
 
