a9e3941662aa3003f68194c15a6829c41e73394f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-04-25 21:52:11 +0700

    Fix mistake in fractional coordinates for Si diamond structure

--- examples/Si_fcc/main_Si_fcc.jl
+++ examples/Si_fcc/main_Si_fcc.jl
@@ -7,7 +7,7 @@ function test_main( ; method="SCF" )
         2
 
         Si  0.0  0.0  0.0
-        Si  0.5  0.5  0.5
+        Si  0.25  0.25  0.25
         """, in_bohr=true)
     atoms.LatVecs = gen_lattice_fcc(5.431*ANG2BOHR)
     atoms.positions = atoms.LatVecs*atoms.positions
