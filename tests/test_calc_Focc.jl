eed3a4fb32d8a2da599e0ec1133d09725bc82274
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-05-07 10:35:12 +0700

    Handle kpoints separately, efermi for each kpoints

--- tests/test_calc_Focc.jl
+++ tests/test_calc_Focc.jl
@@ -4,23 +4,38 @@ include("../src/calc_entropy.jl")
 include("../src/sum_upto_E_fermi.jl")
 
 function test_main(kT::Float64)
+    
     Nstates = 8
     Nelectrons = 6.0
-    Nkpt = 1
-    evals = Array{Float64}(Nstates)
-    evals = [-2.4, -1.0, -0.5, -0.2, -0.19, -0.10, -0.05, -0.05]
+    Nkpt = 2
+
+    evals = Array{Float64}(Nstates,Nkpt)
+    evals[:,1] = [-2.4, -1.0, -0.5, -0.2, -0.19, -0.10, -0.05, -0.05]
+    evals[:,2] = [-2.4, -1.0, -0.5, -0.2, -0.18, -0.11, -0.10, -0.05]
+
+    Focc = Array{Float64}(Nstates,Nkpt)
+
     println("\nkT = ", kT)
     spinpol = false
     println("\nspinpol = ", spinpol)
-    Focc, E_fermi = calc_Focc(evals, Nelectrons, kT)
-    for ist = 1:Nstates
-        @printf("%18.10f %18.10f\n", evals[ist], Focc[ist])
+    
+    E_fermi = zeros(Nkpt)
+
+    for ik = 1:Nkpt
+        Focc[:,ik], E_fermi[ik] = calc_Focc( evals[:,ik], Nelectrons, kT )
+        @printf("%d E_fermi = %18.10f\n", ik, E_fermi[ik])
+
+        for ist = 1:Nstates
+            @printf("%18.10f %18.10f\n", evals[ist,ik], Focc[ist,ik])
+        end
+        
+        integFocc = sum_upto_E_fermi( Focc[:,ik], evals[:,ik], E_fermi[ik] )    
+        @printf("integFocc = %18.10f\n", integFocc)
+        @printf("sum(Focc) = %18.10f\n", sum(Focc[:,ik]))
+        @printf("Entropy (-TS) = %18.10f\n", calc_entropy( Focc[:,ik], kT, is_spinpol=spinpol ))
     end
-    @printf("E_fermi = %18.10f\n", E_fermi)
-    integFocc = sum_upto_E_fermi( Focc, evals, E_fermi )
-    @printf("integFocc = %18.10f\n", integFocc)
-    @printf("sum(Focc) = %18.10f\n", sum(Focc))
-    @printf("Entropy (-TS) = %18.10f\n", calc_entropy(Focc, kT, is_spinpol=spinpol))
+
 end
 
 test_main(0.01)
+
