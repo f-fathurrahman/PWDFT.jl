e102c879b242d93c005ffda353ed3f66a6822822
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-05-14 21:36:50 +0700

    Function to create random wavefunction

---
+++ src/gen_wavefun.jl
@@ -0,0 +1,22 @@
+
+# XXX Need to be updated for spin-polarized case
+function gen_rand_wavefun( pw::PWGrid, electrons::Electrons; seed=1234 )
+
+    Nkpt = pw.gvecw.kpoints.Nkpt
+    Ngw = pw.gvecw.Ngw
+    Nstates = electrons.Nstates
+
+    psik = Array{Array{Complex128,2},1}(Nkpt)
+
+    srand(seed)
+
+    for ik = 1:Nkpt
+        psik[ik] = rand(Ngw[ik],Nstates) + im*rand(Ngw[ik],Nstates)
+        psik[ik] = ortho_gram_schmidt( psik[ik] )
+    end
+
+	return psik
+
+end
+
+
