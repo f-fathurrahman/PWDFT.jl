6cc179bdb33abd1396bdaa25c3f1ed0929c3903c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-05-19 23:59:00 +0700

    Test N2 again, include the result in the script

--- examples/N2/main_N2.jl
+++ examples/N2/main_N2.jl
@@ -16,7 +16,7 @@ function test_main( ; method="SCF" )
     Ham.energies.NN = calc_E_NN( atoms )
 
     if method == "SCF"
-        KS_solve_SCF!( Ham )
+        KS_solve_SCF!( Ham, mix_method="anderson" )
 
     elseif method == "Emin"
         KS_solve_Emin_PCG!( Ham )
