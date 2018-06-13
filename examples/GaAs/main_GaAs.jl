011507faf71266bdd2712ef9b95b807024cf4b2c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-06-14 03:16:40 +0700

    New comparison with ABINIT for GaAs

--- examples/GaAs/main_GaAs.jl
+++ examples/GaAs/main_GaAs.jl
@@ -17,7 +17,7 @@ function test_main( ; method="SCF" )
     # Initialize Hamiltonian
     pspfiles = ["../../pseudopotentials/pade_gth/Ga-q3.gth",
                 "../../pseudopotentials/pade_gth/As-q5.gth"]
-    ecutwfc_Ry = 40.0
+    ecutwfc_Ry = 30.0
     Ham = PWHamiltonian( atoms, pspfiles, ecutwfc_Ry*0.5, meshk=[4,4,4], verbose=true )
 
     # calculate E_NN
@@ -34,7 +34,7 @@ function test_main( ; method="SCF" )
         KS_solve_SCF!( Ham )
 
     elseif method == "Emin"
-        KS_solve_Emin_PCG!( Ham, verbose=true )
+        KS_solve_Emin_PCG!( Ham, verbose=true, savewfc=false )
 
     elseif method == "DCM"
         KS_solve_DCM!( Ham, NiterMax=15 )
@@ -78,36 +78,24 @@ end
      xc contribution           =      -4.80801106 Ry
      ewald contribution        =     -16.84241132 Ry = -4.21060283 Ha
 
-Kinetic energy  =  3.26604416655604E+00
-Hartree energy  =  8.16569937191940E-01
-XC energy       = -2.39970840396867E+00
+Kinetic energy  =  3.26683000819830E+00
+Hartree energy  =  8.17221804456118E-01
+XC energy       = -2.40229498371500E+00
 Ewald energy    = -8.42120578386954E+00
 PspCore energy  =  3.78008143191317E-01
-Loc. psp. energy= -3.13594871632497E+00
-NL   psp  energy=  8.58003632435184E-01
->>>>>>>>> Etotal= -8.63823702478870E+00
+Loc. psp. energy= -3.13731037111346E+00
+NL   psp  energy=  8.58159437985374E-01
+>>>>>>>>> Etotal= -8.64059174486689E+00 
 
-PCG result
-Kinetic    energy:       3.2667303969
-Ps_loc     energy:      -2.7591418239
-Ps_nloc    energy:       0.8579762330
-Hartree    energy:       0.8240957924
-XC         energy:      -2.4078020740
+Kinetic    energy:       3.2350078422
+Ps_loc     energy:      -2.7329323313
+Ps_nloc    energy:       0.8610819832
+Hartree    energy:       0.8024651116
+XC         energy:      -2.3993127351
 -------------------------------------
-Electronic energy:      -0.2181414755
+Electronic energy:      -0.2336901294
 NN         energy:      -8.4212062785
 -------------------------------------
-Total      energy:      -8.6393477541
+Total      energy:      -8.6548964079
 
-SCF result
-Kinetic    energy:       3.2667262728
-Ps_loc     energy:      -2.7592009319
-Ps_nloc    energy:       0.8580163543
-Hartree    energy:       0.8241183786
-XC         energy:      -2.4078096283
--------------------------------------
-Electronic energy:      -0.2181495546
-NN         energy:      -8.4212062785
--------------------------------------
-Total      energy:      -8.6393558331
 """
