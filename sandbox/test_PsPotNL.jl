33933f601dac292c7a024ba900ab554448865aff
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-06-19 15:05:29 +0700

    Update test_PsPotNL

--- sandbox/test_PsPotNL.jl
+++ sandbox/test_PsPotNL.jl
@@ -1,3 +1,7 @@
+push!(LOAD_PATH, "../src")
+
+using Random
+using Printf
 using PWDFT
 
 function test_main()
@@ -10,24 +14,29 @@ function test_main()
                 "../pseudopotentials/pade_gth/O-q6.gth"]
 
     ecutwfc = 60.0
-    LatVecs = 20.0*diagm(ones(3))
-    Ham = PWHamiltonian( atoms, pspfiles, ecutwfc, LatVecs )
+    LatVecs = gen_lattice_sc(20.0)
+    Ham = PWHamiltonian( atoms, pspfiles, ecutwfc, verbose=true, extra_states=2 )
 
-    println(Ham.pw)
-    for isp = 1:atoms.Nspecies
-        println(Ham.pspots[isp])
-    end
-
-    Nstates = 4
-    Ngwx = Ham.pw.gvecw.Ngwx
+    Nkpt = Ham.pw.gvecw.kpoints.Nkpt
+    Nspin = Ham.electrons.Nspin
+    Nkspin = Nkpt*Nspin
+    Nstates = Ham.electrons.Nstates
+    Ngw = Ham.pw.gvecw.Ngw
     srand(1234)
-    psi = rand(Complex128,Ngwx,Nstates)
-    psi = ortho_gram_schmidt(psi)
+    psiks = Array{Array{ComplexF64,2},1}(undef,Nkspin)
+    #
+    for ispin = 1:Nspin
+    for ik = 1:Nkpt
+        ikspin = ik + (ispin-1)*Nkpt
+        psiks[ikspin] = ortho_gram_schmidt(rand(ComplexF64,Ngw[ik],Nstates))
+    end
+    end
 
     if Ham.pspotNL.NbetaNL > 0
-        betaNL_psi = calc_betaNL_psi( Ham.pspotNL.betaNL, psi )
-        E_ps_NL = calc_E_Ps_nloc( Ham, psi )
+        E_ps_NL = calc_E_Ps_nloc( Ham, psiks )
+        println("E ps NL = ", E_ps_NL)
     end
+    
 
 end
 
