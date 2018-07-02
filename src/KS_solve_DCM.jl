3f2c08e74292889d0318aa2c21b71071654db437
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-02 21:23:59 +0700

    Extend KS solve DCM to Nspin=2

--- src/KS_solve_DCM.jl
+++ src/KS_solve_DCM.jl
@@ -11,55 +11,73 @@ function KS_solve_DCM!( Ham::PWHamiltonian;
     Focc = Ham.electrons.Focc
     Nstates = Ham.electrons.Nstates
     Nkpt = Ham.pw.gvecw.kpoints.Nkpt
+    Nspin = Ham.electrons.Nspin
 
-    psik = Array{Array{ComplexF64,2},1}(undef,Nkpt)
+    Nkspin = Nkpt*Nspin
+
+    psiks = Array{Array{ComplexF64,2},1}(undef,Nkspin)
 
     #
     # Initial wave function
     #
     if startingwfc == nothing
         srand(1234)
+        for ispin = 1:Nspin
         for ik = 1:Nkpt
-            psi = rand(Ngw[ik],Nstates) + im*rand(Ngw[ik],Nstates)
-            psik[ik] = ortho_gram_schmidt(psi)
+            ikspin = ik + (ispin - 1)*Nkpt
+            psiks[ikspin] = ortho_gram_schmidt( rand(ComplexF64,Ngw[ik],Nstates) )
+        end
         end
     else
-        psik = startingwfc
+        psiks = startingwfc
     end
 
     #
     # Calculated electron density from this wave function and update Hamiltonian
     #
-    rhoe = calc_rhoe( pw, Focc, psik )
-    update!(Ham, rhoe)
+    Rhoe = zeros(Float64,Npoints,Nspin)
+    for ispin = 1:Nspin
+        idxset = (Nkpt*(ispin-1)+1):(Nkpt*ispin)
+        Rhoe[:,ispin] = calc_rhoe( pw, Focc[:,idxset], psiks[idxset] )
+    end
+    update!(Ham, Rhoe)
 
-    rhoe_old = copy(rhoe)
+    Rhoe_old = copy(Rhoe)
 
-    evals = zeros(Float64,Nstates,Nkpt)
+    evals = zeros(Float64,Nstates,Nkspin)
+    
     # Starting eigenvalues and psi
+    for ispin = 1:Nspin
     for ik = 1:Nkpt
         Ham.ik = ik
-        evals[:,ik], psik[ik] = diag_lobpcg( Ham, psik[ik], verbose_last=false, NiterMax=10 )
+        Ham.ispin = ispin
+        ikspin = ik + (ispin - 1)*Nspin
+        evals[:,ikspin], psiks[ikspin] =
+        diag_lobpcg( Ham, psiks[ikspin], verbose_last=false, NiterMax=10 )
+    end
     end
 
-    energies = calc_energies( Ham, psik )
-    Ham.energies = energies
-    Etot_old = energies.Total
+    Ham.energies = calc_energies( Ham, psiks )
+    
+    Etot_old = Ham.energies.Total
 
     # subspace
-    Y = Array{Array{ComplexF64,2},1}(undef,Nkpt)
-    R = Array{Array{ComplexF64,2},1}(undef,Nkpt)
-    P = Array{Array{ComplexF64,2},1}(undef,Nkpt)
-    G = Array{Array{ComplexF64,2},1}(undef,Nkpt)
-    T = Array{Array{Float64,2},1}(undef,Nkpt)
-    B = Array{Array{Float64,2},1}(undef,Nkpt)
+    Y = Array{Array{ComplexF64,2},1}(undef,Nkspin)
+    R = Array{Array{ComplexF64,2},1}(undef,Nkspin)
+    P = Array{Array{ComplexF64,2},1}(undef,Nkspin)
+    G = Array{Array{ComplexF64,2},1}(undef,Nkspin)
+    T = Array{Array{Float64,2},1}(undef,Nkspin)
+    B = Array{Array{Float64,2},1}(undef,Nkspin)
+    for ispin = 1:Nspin
     for ik = 1:Nkpt
-        Y[ik] = zeros( ComplexF64, Ngw[ik], 3*Nstates )
-        R[ik] = zeros( ComplexF64, Ngw[ik], Nstates )
-        P[ik] = zeros( ComplexF64, Ngw[ik], Nstates )
-        G[ik] = zeros( ComplexF64, 3*Nstates, 3*Nstates )
-        T[ik] = zeros( Float64, 3*Nstates, 3*Nstates )
-        B[ik] = zeros( Float64, 3*Nstates, 3*Nstates )
+        ikspin = ik + (ispin - 1)*Nkpt
+        Y[ikspin] = zeros( ComplexF64, Ngw[ik], 3*Nstates )
+        R[ikspin] = zeros( ComplexF64, Ngw[ik], Nstates )
+        P[ikspin] = zeros( ComplexF64, Ngw[ik], Nstates )
+        G[ikspin] = zeros( ComplexF64, 3*Nstates, 3*Nstates )
+        T[ikspin] = zeros( Float64, 3*Nstates, 3*Nstates )
+        B[ikspin] = zeros( Float64, 3*Nstates, 3*Nstates )
+    end
     end
 
     set1 = 1:Nstates
@@ -71,75 +89,85 @@ function KS_solve_DCM!( Ham::PWHamiltonian;
 
     for iter = 1:NiterMax
         
+        for ispin = 1:Nspin
         for ik = 1:Nkpt
             Ham.ik = ik
-            Hpsi = op_H( Ham, psik[ik] )
+            Ham.ispin = ispin
+            ikspin = ik + (ispin - 1)*Nkpt
             #
-            psiHpsi = psik[ik]' * Hpsi
+            Hpsi = op_H( Ham, psiks[ikspin] )
+            #
+            psiHpsi = psiks[ikspin]' * Hpsi
             psiHpsi = 0.5*( psiHpsi + psiHpsi' )
             #
-            R[ik] = Hpsi - psik[ik]*psiHpsi
-            R[ik] = Kprec( ik, pw, R[ik] )
+            R[ikspin] = Hpsi - psiks[ikspin]*psiHpsi
+            R[ikspin] = Kprec( ik, pw, R[ikspin] )
             #
-            Y[ik][:,set1] = psik[ik]
-            Y[ik][:,set2] = R[ik]
+            Y[ikspin][:,set1] = psiks[ikspin]
+            Y[ikspin][:,set2] = R[ikspin]
+            #
             if iter > 1
-                Y[ik][:,set3] = P[ik]
+                Y[ikspin][:,set3] = P[ikspin]
             end
             
             #
             # Project kinetic and ionic potential
             #
-            KY = op_K( Ham, Y[ik] ) + op_V_Ps_loc( Ham, Y[ik] )
+            KY = op_K( Ham, Y[ikspin] ) + op_V_Ps_loc( Ham, Y[ikspin] )
             #
-            T[ik] = real( Y[ik]'*KY )
-            B[ik] = real( Y[ik]'*Y[ik] )
-            B[ik] = 0.5*( B[ik] + B[ik]' )
+            T[ikspin] = real( Y[ikspin]'*KY )
+            B[ikspin] = real( Y[ikspin]'*Y[ikspin] )
+            B[ikspin] = 0.5*( B[ikspin] + B[ikspin]' )
 
             if iter > 1
-                G[ik] = Matrix(1.0I, 3*Nstates, 3*Nstates) #eye(3*Nstates)
+                G[ikspin] = Matrix(1.0I, 3*Nstates, 3*Nstates) #eye(3*Nstates)
             else
-                G[ik] = Matrix(1.0I, 2*Nstates, 2*Nstates)
+                G[ikspin] = Matrix(1.0I, 2*Nstates, 2*Nstates)
             end
+        end
         end
         
         @printf("DCM iter: %3d\n", iter)
 
         for iterscf = 1:MaxInnerSCF
-            ispin = 1  # FIXME setup properly for loop over Nspin
-            V_loc = Ham.potentials.Hartree + Ham.potentials.XC[:,ispin]
-
+            
+            for ispin = 1:Nspin
             for ik = 1:Nkpt
+                #
                 Ham.ik = ik
+                Ham.ispin = ispin
+                ikspin = ik + (ispin - 1)*Nkpt
+                #
+                V_loc = Ham.potentials.Hartree + Ham.potentials.XC[:,ispin]
+                #
                 if Ham.pspotNL.NbetaNL > 0
-                    VY = op_V_Ps_nloc( Ham, Y[ik] ) + op_V_loc( ik, pw, V_loc, Y[ik] )
+                    VY = op_V_Ps_nloc( Ham, Y[ikspin] ) + op_V_loc( ik, pw, V_loc, Y[ikspin] )
                 else
-                    VY = op_V_loc( ik, pw, V_loc, Y[ik] )
+                    VY = op_V_loc( ik, pw, V_loc, Y[ikspin] )
                 end
                 #
-                A = real( T[ik] + Y[ik]'*VY )
+                A = real( T[ikspin] + Y[ikspin]'VY )
                 A = 0.5*( A + A' )
                 #
-                D, G[ik] = eigen( A, B[ik] )
-                evals[:,ik] = D[1:Nstates]
+                D, G[ikspin] = eigen( A, B[ikspin] )
+                evals[:,ikspin] = D[1:Nstates]
                 #
-                
-                #E_kin = trace( G[:,set1]' * T * G[:,set1] )
-                #println("Ekin from trace = ", E_kin)
-
-                # update psik
-                psik[ik] = Y[ik]*G[ik][:,set1]
+                # update
+                psiks[ikspin] = Y[ikspin]*G[ikspin][:,set1]
+            end
             end
             
-            #rhoe = 0.9*calc_rhoe( pw, Focc, psi ) + 0.1*rhoe_old # use mixing
-            rhoe = calc_rhoe( pw, Focc, psik )
-            
-            update!( Ham, rhoe )
 
-            rhoe_old = copy(rhoe)
+            for ispin = 1:Nspin
+                idxset = (Nkpt*(ispin-1)+1):(Nkpt*ispin)
+                Rhoe[:,ispin] = calc_rhoe( pw, Focc[:,idxset], psiks[idxset] )
+            end
+            update!( Ham, Rhoe )
+
+            Rhoe_old = copy(Rhoe)
 
             # Calculate energies once again
-            Ham.energies = calc_energies( Ham, psik )
+            Ham.energies = calc_energies( Ham, psiks )
             Etot = Ham.energies.Total
             diffE = -(Etot - Etot_old)
 
@@ -154,9 +182,8 @@ function KS_solve_DCM!( Ham::PWHamiltonian;
         end
 
         # Calculate energies once again
-        energies = calc_energies( Ham, psik )
-        Ham.energies = energies
-        Etot = energies.Total
+        Ham.energies = calc_energies( Ham, psiks )
+        Etot = Ham.energies.Total
         diffE = abs( Etot - Etot_old )
         @printf("DCM: %5d %18.10f %18.10e\n", iter, Etot, diffE)
 
@@ -168,22 +195,24 @@ function KS_solve_DCM!( Ham::PWHamiltonian;
         Etot_old = Etot
 
         # No need to update potential, it is already updated in inner SCF loop
-
+        for ispin = 1:Nspin
         for ik = 1:Nkpt
+            ikspin = ik + (ispin - 1)*Nkpt
             if iter > 1
-                P[ik] = Y[ik][:,set4]*G[ik][set4,set1]
+                P[ikspin] = Y[ikspin][:,set4]*G[ikspin][set4,set1]
             else
-                P[ik] = Y[ik][:,set2]*G[ik][set2,set1]
+                P[ikspin] = Y[ikspin][:,set2]*G[ikspin][set2,set1]
             end
+        end
         end
     end  # end of DCM iteration
     
     Ham.electrons.ebands = evals[:,:]
 
     if savewfc
-        for ik = 1:Nkpt
-            wfc_file = open("WFC_k_"*string(ik)*".data","w")
-            write( wfc_file, psik[ik] )
+        for ikspin = 1:Nkpt*Nspin
+            wfc_file = open("WFC_ikspin_"*string(ikspin)*".data","w")
+            write( wfc_file, psiks[ikspin] )
             close( wfc_file )
         end
     end
