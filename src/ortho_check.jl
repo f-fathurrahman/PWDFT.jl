61837cce17b26bbd9c05f36962a8eb920b2fc179
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-03 15:39:39 +0700

    Print both real and imaginary part irrespective of their values

--- src/ortho_check.jl
+++ src/ortho_check.jl
@@ -6,20 +6,12 @@ function ortho_check( psi::Array{ComplexF64,2}; dVol=1.0 )
     @printf("\nNorm check:\n")
     for ist = 1:Nstates
         c = dot( psi[:,ist], psi[:,ist] ) * dVol
-        if c.im <= eps()
-            @printf("State: #%5d: %18.10f\n", ist, c.re)
-        else
-            @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
-        end
+        @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
     end
     @printf("\nOrtho check w.r.t state #1:\n")
     for ist = 2:Nstates
         c = dot( psi[:,ist], psi[:,1] ) * dVol
-        if c.im <= eps()
-            @printf("State: #%5d: %18.10f\n", ist, c.re)
-        else
-            @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
-        end
+        @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
     end
     @printf("\n")
 end
