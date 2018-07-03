4fc40c627460f5028c9182a3f39123eab975d5f1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-03 22:03:34 +0700

    Add O2 example

---
+++ examples/O2/main_O2.jl
@@ -0,0 +1,47 @@
+using Printf
+using PWDFT
+
+function test_main( ; method="SCF" )
+
+    # Atoms
+    atoms = init_atoms_xyz("../../structures/O2.xyz")
+    atoms.LatVecs = gen_lattice_cubic(16.0)
+    println(atoms)
+
+    # Initialize Hamiltonian
+    ecutwfc_Ry = 30.0
+    pspfiles = ["../../pseudopotentials/pade_gth/O-q6.gth"]
+    Ham = PWHamiltonian( atoms, pspfiles, ecutwfc_Ry*0.5 )
+
+    # calculate E_NN
+    Ham.energies.NN = calc_E_NN( atoms )
+
+    if method == "SCF"
+        KS_solve_SCF!( Ham, mix_method="anderson" )
+
+    elseif method == "Emin"
+        KS_solve_Emin_PCG!( Ham )
+
+    elseif method == "DCM"
+        KS_solve_DCM!( Ham, NiterMax=15 )
+
+    else
+        println("ERROR: unknow method = ", method)
+    end
+
+    Nstates = Ham.electrons.Nstates
+    ebands = Ham.electrons.ebands
+
+    println("\nBand energies:")
+    for ist = 1:Nstates
+        @printf("%8d  %18.10f = %18.10f eV\n", ist, ebands[ist], ebands[ist]*Ry2eV*2)
+    end
+
+    println("\nTotal energy components")
+    println(Ham.energies)
+end
+
+@time test_main(method="Emin")
+@time test_main(method="SCF")
+@time test_main(method="DCM")
+
