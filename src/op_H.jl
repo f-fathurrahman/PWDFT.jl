92101828e26a4d8ea77a24c960250747590dfd50
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-12 17:17:38 +0700

    Overload * operator for op_H

--- src/op_H.jl
+++ src/op_H.jl
@@ -7,3 +7,8 @@ function op_H( Ham::Hamiltonian, psi )
         return op_K( Ham, psi ) + op_V_loc( Ham, psi )
     end
 end
+
+import Base: *
+*( Ham::Hamiltonian, psi ) = op_H( Ham, psi )
+*( psi::LinearAlgebra.Adjoint{Complex{Float64},Array{Complex{Float64},2}},
+   Ham::Hamiltonian ) = adjoint( op_H( Ham, adjoint(psi) ) )
