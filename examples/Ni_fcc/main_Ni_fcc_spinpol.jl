d518c4b1053b6a4e020590f62065d6a1aeb157ab
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-16 16:38:34 +0700

    Remove printing band energies information

--- examples/Ni_fcc/main_Ni_fcc_spinpol.jl
+++ examples/Ni_fcc/main_Ni_fcc_spinpol.jl
@@ -23,19 +23,6 @@ function main()
     # Solve the KS problem
     #
     KS_solve_SCF_smearing!( Ham, mix_method="anderson", β=0.2 )
-
-    Nstates = Ham.electrons.Nstates
-    ebands = Ham.electrons.ebands
-    Nkpt = Ham.pw.gvecw.kpoints.Nkpt
-    k = Ham.pw.gvecw.kpoints.k
-    
-    println("\nBand energies:")
-    for ik = 1:Nkpt
-        @printf("%d k = [%f,%f,%f]\n", ik, k[1,ik], k[2,ik], k[3,ik])
-        for ist = 1:Nstates
-            @printf("%8d  %18.10f = %18.10f eV\n", ist, ebands[ist,ik], ebands[ist,ik]*Ry2eV*2)
-        end
-    end
     
     println("\nTotal energy components")
     println(Ham.energies)
