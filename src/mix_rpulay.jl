fc2e4d41bd212015edbc74a0c5249417f21cf581
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-21 23:35:28 +0700

    Clean up mix_rpulay

--- src/mix_rpulay.jl
+++ src/mix_rpulay.jl
@@ -2,9 +2,6 @@ function mix_rpulay!( x, gx, beta, X, F, iter, MIXDIM, x_old, f_old )
 
     f = gx - x
 
-    println("iter = ", iter)
-    println("norm f = ", norm(f))
-
     if iter > 1
         dx = x - x_old
         df = f - f_old
@@ -21,14 +18,10 @@ function mix_rpulay!( x, gx, beta, X, F, iter, MIXDIM, x_old, f_old )
 
     active_dim = mod(iter-1,MIXDIM+1)
 
-    println("\nin mix_rpulay:")
-    println("active_dim = ", active_dim)
-
     if iter > 1
         # Restart, use the last history
         if active_dim == 0
             #
-            println("Restart rpulay")
             X[:,1] = X[:,MIXDIM]
             F[:,1] = F[:,MIXDIM]
             X[:,2:MIXDIM] .= 0.0
@@ -38,33 +31,23 @@ function mix_rpulay!( x, gx, beta, X, F, iter, MIXDIM, x_old, f_old )
             Fk = F[:,1]
             addv = (Xk + beta*Fk)*inv(Fk'*Fk)*(Fk'*f)
             xnew = x + beta*f - addv
-            println("norm addv = ", norm(addv))
-            println("norm xnew = ", norm(xnew))
         else
             for i = active_dim:-1:2
                 X[:,i] = X[:,i-1]
                 F[:,i] = F[:,i-1]
-                println(i, "norm X[:,i] = ", norm(X[:,i]))
-                println(i, "norm F[:,i] = ", norm(F[:,i]))
             end
             X[:,1] = dx
             F[:,1] = df
-            println("norm X[:,1] = ", norm(X[:,1]))
-            println("norm F[:,1] = ", norm(F[:,1]))
             #
             Xk = X[:,1:active_dim]
             Fk = F[:,1:active_dim]
             addv = (Xk + beta*Fk)*inv(Fk'*Fk)*(Fk'*f)
             xnew = x + beta*f - addv
-            println("norm addv = ", norm(addv))
-            println("norm xnew = ", norm(xnew))
         end
     else
         xnew = x + beta*f
         X[:,1] = x
         F[:,1] = f
-        println("norm X[:,1] = ", norm(X[:,1]))
-        println("norm F[:,1] = ", norm(F[:,1]))
     end
 
     x_old[:] = x
