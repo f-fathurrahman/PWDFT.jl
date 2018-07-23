c993aedde755971452816376c4b673d9ce7a187f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-23 18:55:15 +0700

    Use free_electrons_Hamiltonian in test_bandstructure

--- sandbox/test_bandstructure.jl
+++ sandbox/test_bandstructure.jl
@@ -57,76 +57,17 @@ function test_empty_lattice(lattice::String, band_file::String)
     end
     println(kpoints)
 
-    pw = PWGrid(15.0, atoms.LatVecs, kpoints=kpoints)
-    #
-    # Prepare Hamiltonian
-    #
+    ecutwfc = 15.0
     pspfiles = ["../pseudopotentials/pade_gth/H-q1.gth"]
-    #
-    Nspecies = atoms.Nspecies
-    if Nspecies != size(pspfiles)[1]
-        @printf("ERROR length of pspfiles is not equal to %d\n", Nspecies)
-        exit()
-    end
+    Ham = free_electron_Hamiltonian(
+            atoms, pspfiles, 15.0, kpoints=kpoints, extra_states=3,
+            verbose=true
+          )
 
-    Npoints = prod(pw.Ns)
-    Ω = pw.Ω
-    G2 = pw.gvec.G2
-    Ng = pw.gvec.Ng
-    idx_g2r = pw.gvec.idx_g2r
-
-    strf = calc_strfact( atoms, pw )
-
-    #
-    # Initialize pseudopotentials and local potentials
-    #
-    #Vg = zeros(ComplexF64, Npoints)
-    V_Ps_loc = zeros(Float64, Npoints)
-
-    Pspots = Array{PsPot_GTH}(undef,Nspecies)
-
-    for isp = 1:Nspecies
-        Pspots[isp] = PsPot_GTH( pspfiles[isp] )
-        psp = Pspots[isp]
-        println(psp)
-    end
-
-    Nspin = 1
-
-    # other potential terms are set to zero
-    V_Hartree = zeros( Float64, Npoints )
-    V_XC = zeros( Float64, Npoints, Nspin )
-    potentials = Potentials( V_Ps_loc, V_Hartree, V_XC )
-    #
-    energies = Energies()
-    #
-    rhoe = zeros( Float64, Npoints, Nspin )
-
+    pw = Ham.pw
     Nkpt = kpoints.Nkpt
-    
-    #
-    # Construct Electrons manually so that we are not limited by our dummy H atom
-    #
-    Nstates = 4
-    electrons = Electrons(
-        2*Nstates, #Nelectrons::Float64
-        Nstates, #Nstates::Int64
-        Nstates, #Nstates_occ::Int64
-        2*ones(Nstates,Nkpt), #Focc::Array{Float64,2}
-        zeros(Nstates,Nkpt), #ebands::Array{Float64,2}
-        1 #Nspin::Int64
-    )
-
-    # NL pseudopotentials
-    pspotNL = PsPotNL()
-
-    atoms.Zvals = get_Zvals( Pspots ) # not really needed
-
-    ik = 1
-    ispin = 1
-    xcfunc = "VWN"
-    Ham = Hamiltonian( pw, potentials, energies, rhoe,
-                         electrons, atoms, Pspots, pspotNL, xcfunc, ik, ispin )
+    Nspin = Ham.electrons.Nspin
+    Nstates = Ham.electrons.Nstates
 
     Nkspin = Nkpt*Nspin
     Ngw = pw.gvecw.Ngw
@@ -138,7 +79,7 @@ function test_empty_lattice(lattice::String, band_file::String)
     for ispin = 1:Nspin
     for ik = 1:Nkpt
         ikspin = ik + (ispin - 1)*Nkpt
-        psiks[ikspin] = ortho_gram_schmidt(rand(ComplexF64,Ngw[ik],Nstates))
+        psiks[ikspin] = ortho_sqrt(rand(ComplexF64,Ngw[ik],Nstates))
     end
     end
 
@@ -151,10 +92,15 @@ function test_empty_lattice(lattice::String, band_file::String)
         #
         @printf("\nispin = %d, ik = %d, ikspin=%d, Ngw = %d\n", ispin, ik, ikspin, Ngw[ik])
         @printf("kpts = [%f,%f,%f]\n", k[1,ik], k[2,ik], k[3,ik])
-        evals[:,ikspin], psiks[ikspin] =
-        diag_lobpcg( Ham, psiks[ikspin], verbose_last=true )
+        
+        #evals[:,ikspin], psiks[ikspin] =
+        #diag_lobpcg( Ham, psiks[ikspin], verbose_last=true )
+        
         #evals[:,ikspin], psiks[ikspin] =
         #diag_davidson( Ham, psiks[ikspin], verbose_last=true )
+
+        evals[:,ikspin], psiks[ikspin] =
+        diag_Emin_PCG( Ham, psiks[ikspin], verbose_last=true )
 
     end
     end
@@ -165,7 +111,7 @@ end
 
 #test_empty_lattice("sc", "TEMP_empty_lattice_sc.dat")
 
-#test_empty_lattice("fcc", "TEMP_empty_lattice_fcc.dat")
+test_empty_lattice("fcc", "TEMP_empty_lattice_fcc.dat")
 
 #test_empty_lattice("bcc", "TEMP_empty_lattice_bcc.dat")
 
@@ -359,4 +305,4 @@ function test_Cu_fcc()
 
 end
 
-test_Cu_fcc()
+#test_Cu_fcc()
