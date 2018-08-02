234d6110fb2164af440e5e0cac8ec0e683f5fbc6
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-08-02 17:34:32 +0700

    Implement new keyword xyz_string_frac. Extended xyz format, not yet tested. More examples in test_atoms

--- sandbox/test_atoms.jl
+++ sandbox/test_atoms.jl
@@ -14,4 +14,42 @@ function test02()
 end
 
 
+function test03()
+    atoms = Atoms(xyz_string="""
+    2
+
+    H  0.0  0.0  0.0
+    Cl 1.5  0.0  0.0
+    """, in_bohr=true)
+    println(atoms)
+end
+
+
+function test04()
+    atoms = Atoms(xyz_string_frac="""
+    2
+
+    Si  0.0   0.0   0.0
+    Si  0.25  0.25  0.25
+    """, in_bohr=true, LatVecs=gen_lattice_fcc(10.2631))
+    println(atoms)
+    write_xsf("TEMP_Si_fcc.xsf", atoms)
+end
+
+function test05()
+    atoms = Atoms(xyz_string_frac="""
+    2
+
+    Ga  0.0   0.0   0.0
+    As  0.25  0.25  0.25
+    """, in_bohr=true, LatVecs=gen_lattice_fcc(5.6536*ANG2BOHR))
+    println(atoms)
+    write_xsf("TEMP_GaAs_fcc.xsf", atoms)
+end
+
+test01()
 test02()
+test03()
+test04()
+test05()
+
--- src/Atoms.jl
+++ src/Atoms.jl
@@ -13,7 +13,32 @@ mutable struct Atoms
     Zvals::Array{Float64,1}   # unique
 end
 
-function Atoms( ;xyz_file="", xyz_string="",
+
+"""
+Creates an instance of `Atoms`.
+
+If no arguments are given, then a dummy `Atoms` is returned.
+
+Use only one of `xyz_file`, `xyz_string` and `xyz_string_frac` for specifying
+number of atoms, atomic symbols, and positions, following xyz format,
+for example:
+```
+3
+
+C   0.0   0.0   0.0
+O   0.0   0.0   1.5
+```
+
+The argument `xyz_file` is used when the structure is given in an xyz file, which
+in this case `xyz_file` is a string of path to the xyz file. By default the unit
+used is angstrom. If the coordinate is given in bohr, set `in_bohr=true`.
+
+Alternatively, one can pass the content of xyz file as string in `xyz_string`.
+
+`xyz_string_frac` is the same as `xyz_string`, however the actual coordinates will be
+transformed by multiplying it with `LatVecs`. This is useful for crystals.
+"""
+function Atoms( ;xyz_file="", xyz_string="", xyz_string_frac="",
     in_bohr=false, LatVecs=10*[1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0] )
 
     if xyz_file != ""
@@ -22,6 +47,11 @@ function Atoms( ;xyz_file="", xyz_string="",
         return atoms
     elseif xyz_string != ""
         atoms = init_atoms_xyz_string(xyz_string, in_bohr=in_bohr)
+        atoms.LatVecs = LatVecs
+        return atoms
+    elseif xyz_string_frac != ""
+        atoms = init_atoms_xyz_string(xyz_string_frac, in_bohr=in_bohr)
+        atoms.positions = LatVecs*atoms.positions
         atoms.LatVecs = LatVecs
         return atoms
     else
@@ -40,8 +70,54 @@ function Atoms( ;xyz_file="", xyz_string="",
 end
 
 
+# extended XYZ format used in ASE
 function init_atoms_xyz_ext( filexyz; in_bohr=false, verbose=false )
-    # extended XYZ format used in ASE
+    f = open(xyz_file, "r")
+    l = readline(f)
+    Natoms = parse(Int64, l)
+    positions = zeros(3,Natoms)
+    atsymbs = Array{String}(undef,Natoms)
+    #
+    l = readline(f)
+    ll = replace( split(l,"=")[2], "\"" => "") # remove "s
+    rll = split(ll)
+    LatVecs_ = zeros(9)
+    for i = 1:9
+        LatVecs_[i] = parse( Float64, rll[i] )
+    end
+    LatVecs = reshape(LatVecs_, (3,3))
+    #
+    for ia = 1:Natoms
+        ll = split( readline(f) )
+        atsymbs[ia] = ll[1]
+        positions[1,ia] = parse( Float64, ll[2] )
+        positions[2,ia] = parse( Float64, ll[3] )
+        positions[3,ia] = parse( Float64, ll[4] )
+    end
+    close(f)
+
+    # convert from angstrom to bohr
+    if !in_bohr
+        if verbose
+            println("Coordinate in xyz file is assumed to be given in angstrom")
+            println("It will be converted to bohr")
+        end
+        positions[:,:] = positions[:,:] * ANG2BOHR
+    else
+        if verbose
+            println("Coordinate in xyz file is assumed to be given in bohr")
+        end
+    end
+
+    SpeciesSymbols = unique(atsymbs)
+    Nspecies = length(SpeciesSymbols)
+
+    # Mapping of atoms to species index
+    atm2species = get_atm2species( atsymbs, SpeciesSymbols )
+
+    Zvals = zeros(Nspecies)
+    return Atoms(Natoms, Nspecies, positions, atm2species, atsymbs, SpeciesSymbols, LatVecs, Zvals )
+
 end
 
 
