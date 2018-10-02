91339c0f16cfbaa81fd804b33a5401432b6cb9ee
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-10-02 22:25:46 +0700

    Print Energies.mTS if it is not zero

--- src/Energies.jl
+++ src/Energies.jl
@@ -24,23 +24,29 @@ end
 import Base: println, sum
 
 function println( energies::Energies; use_smearing=false )
-    
+
     @printf("Kinetic    energy: %18.10f\n", energies.Kinetic )
     @printf("Ps_loc     energy: %18.10f\n", energies.Ps_loc )
     @printf("Ps_nloc    energy: %18.10f\n", energies.Ps_nloc )
     @printf("Hartree    energy: %18.10f\n", energies.Hartree )
     @printf("XC         energy: %18.10f\n", energies.XC )
     @printf("PspCore    energy: %18.10f\n", energies.PspCore )
-    if use_smearing
+
+    if ( abs(energies.mTS) > eps() ) || use_smearing
         @printf("-TS              : %18.10f\n", energies.mTS)
     end
+
     @printf("-------------------------------------\n")
+    
     E_elec = energies.Kinetic + energies.Ps_loc + energies.Ps_nloc +
              energies.Hartree + energies.XC + energies.PspCore + energies.mTS
+    
     @printf("Electronic energy: %18.10f\n", E_elec)
     @printf("NN         energy: %18.10f\n", energies.NN )
     @printf("-------------------------------------\n")
+    
     E_total = E_elec + energies.NN
+    
     if use_smearing
         @printf("Total free energy: %18.10f\n", E_total)
         @printf("\n")
