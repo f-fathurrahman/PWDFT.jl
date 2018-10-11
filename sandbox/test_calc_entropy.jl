4b96d151d983e7ba4cbfdced09a19c58c32952cf
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-10-11 16:12:44 +0700

    Compare two entropy calc results

---
+++ sandbox/test_calc_entropy.jl
@@ -0,0 +1,51 @@
+using Printf
+using PWDFT
+
+function test_no_spin()
+    
+    Nstates = 5
+    Nkpt = 1
+    Nspin = 1
+    Nkspin = Nkpt*Nspin
+    kT = 0.01
+
+    Focc = zeros(Nstates,Nkspin)
+    Focc[:,1] = [2.0, 2.0, 1.8, 0.2, 0.0]
+    
+    evals = zeros(Nstates,Nkspin)
+    evals[:,1] = [-2.0, -1.0, -0.5, 0.5, 1.0]
+    
+    E_fermi = 0.1
+    wk = ones(Nkpt)/Nkpt
+
+    mTS = calc_entropy( Focc, wk, kT, evals, E_fermi )
+    @printf("mTS = %18.10f\n", mTS)
+end
+
+function test_with_spin()
+    
+    Nstates = 5
+    Nkpt = 1
+    Nspin = 2
+    Nkspin = Nkpt*Nspin
+    kT = 0.01
+
+    Focc = zeros(Nstates,Nkspin)
+    Focc[:,1] = [1.0, 1.0, 0.9, 0.1, 0.0]
+    Focc[:,2] = [1.0, 1.0, 0.9, 0.1, 0.0]
+    
+    evals = zeros(Nstates,Nkspin)
+    evals[:,1] = [-2.0, -1.0, -0.5, 0.5, 1.0]
+    evals[:,2] = [-2.0, -1.0, -0.5, 0.5, 1.0]
+    
+    E_fermi = 0.1
+    wk = ones(Nkpt)/Nkpt
+
+    mTS = calc_entropy( Focc, wk, kT, evals, E_fermi, Nspin=Nspin )
+    @printf("mTS = %18.10f\n", mTS)
+end
+
+
+test_no_spin()
+test_with_spin()
+
