a4bf781f25f4638b98592924f9d9d1964e0232ee
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-10-22 15:52:11 +0700

    New version of calc_rhoe, loop over spin is included inside the function.

--- src/calc_rhoe.jl
+++ src/calc_rhoe.jl
@@ -1,50 +1,62 @@
-function calc_rhoe( pw::PWGrid, Focc::Array{Float64,2},
-                    psik::Array{Array{ComplexF64,2},1}; renormalize=true )
+function calc_rhoe(
+    Nelectrons_true::Float64,
+    pw::PWGrid, Focc::Array{Float64,2},
+    psiks::Array{Array{ComplexF64,2},1}, Nspin::Int64;
+    renormalize=true
+)
+
     CellVolume  = pw.CellVolume
     Ns = pw.Ns
     Nkpt = pw.gvecw.kpoints.Nkpt
     Ngw = pw.gvecw.Ngw
     wk = pw.gvecw.kpoints.wk
     Npoints = prod(Ns)
-    Nstates = size(psik[1])[2]
+    Nstates = size(psiks[1])[2]
 
     cpsi = zeros(ComplexF64, Npoints, Nstates)
     psiR = zeros(ComplexF64, Npoints, Nstates)
-    rho = zeros(Float64,Npoints)
+    Rhoe = zeros(Float64, Npoints, Nspin)
 
+    for ispin = 1:Nspin
     for ik = 1:Nkpt
+
+        ikspin = ik + (ispin - 1)*Nkpt
+
         cpsi[:,:] .= 0.0 + im*0.0
+        
         # Transform to real space
         idx = pw.gvecw.idx_gw2r[ik]
-        psi = psik[ik]
+        psi = psiks[ikspin]
         cpsi[idx,:] = psi[:,:]
-        psiR = G_to_R(pw, cpsi)
+        psiR[:,:] = G_to_R(pw, cpsi)
+
         # orthonormalization in real space
         ortho_gram_schmidt!( Nstates, psiR )
         psiR = sqrt(Npoints/CellVolume)*psiR
         #
         for ist = 1:Nstates
+            w = wk[ik]*Focc[ist,ikspin]
             for ip = 1:Npoints
-                rho[ip] = rho[ip] + wk[ik]*Focc[ist,ik]*real( conj(psiR[ip,ist])*psiR[ip,ist] )
+                Rhoe[ip,ispin] = Rhoe[ip,ispin] + w*real( conj(psiR[ip,ist])*psiR[ip,ist] )
             end
         end
-    end
+    end # ik
+    end # ikspin
 
     # Ensure that there is no negative rhoe
-    for ip = 1:Npoints
-        if rho[ip] < eps()
-            rho[ip] = eps()
+    for rho in Rhoe
+        if rho < eps()
+            rho = eps()
         end
     end
 
     # renormalize
     if renormalize
-        integ_rho = sum(rho)*CellVolume/Npoints
-        Nelectrons = sum(Focc)/Nkpt
-        rho = Nelectrons/integ_rho * rho
+        integ_rho = sum(Rhoe)*CellVolume/Npoints
+        Rhoe = Nelectrons_true/integ_rho * Rhoe
     end
 
-    return rho
+    return Rhoe
 end
 
 
