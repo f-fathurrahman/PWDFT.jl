3d91b6fbe676e4e5e066df5e758bb1390bd7a09b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-11-04 23:47:43 +0700

    Nicer output

--- timing/do_all.jl
+++ timing/do_all.jl
@@ -1,11 +1,38 @@
+import InteractiveUtils
+using Printf
 using BenchmarkTools
 using PWDFT
 
-function time_01()
+function time_gen_lattice()
 
+    @printf("\n")
+    @printf("------------------------------\n")
+    @printf("Timing gen_lattice_* functions\n")
+    @printf("------------------------------\n")
+    @printf("\n")
+
+    @printf("gen_lattice_sc  : ")
+    @btime gen_lattice_sc(10.0)
+
+    @printf("gen_lattice_fcc : ")
+    @btime gen_lattice_fcc(10.0)
+        
+    @printf("gen_lattice_bcc : ")
+    @btime gen_lattice_bcc(10.0)
+end
+
+function time_Atoms()
+
+    @printf("\n")
+    @printf("----------------------\n")
+    @printf("Timing Atoms functions\n")
+    @printf("----------------------\n")
+    @printf("\n")
+
+    @printf("Using default constructor (dummy Atoms)          : ")
     @btime Atoms()
 
-
+    @printf("Using xyz_string with two atoms, default LatVecs : ")
     @btime Atoms(xyz_string="""
     2
 
@@ -13,31 +40,37 @@ function time_01()
     Cl  1.0   0.0   0.0
     """)
 
-
+    @printf("Using xyz_string with two atoms, given LatVecs   : ")
     @btime Atoms(xyz_string="""
     2
 
     H   0.0   0.0   0.0
     Cl  1.0   0.0   0.0
     """, LatVecs=gen_lattice_sc(10.0))
-
-    @btime gen_lattice_sc(10.0)
-    @btime gen_lattice_fcc(10.0)
-    @btime gen_lattice_bcc(10.0)
 end
 
 
-function time_02()
+function time_PWGrid()
 
-    ECUTWFC = ( 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0 )    
+    @printf("\n")
+    @printf("-----------------------\n")
+    @printf("Timing PWGrid functions\n")
+    @printf("-----------------------\n")
+    @printf("\n")
+
+    ECUTWFC = range(15.0, stop=50.0, step=5.0)    
     LatVecs = gen_lattice_sc(16.0)
 
     for ecutwfc in ECUTWFC
+        @printf("ecutwfc = %4.1f Ha : ", ecutwfc)
         @btime PWGrid($ecutwfc, $LatVecs)
     end
 end
 
+println()
+InteractiveUtils.versioninfo()
 
-#time_01()
-time_02()
+time_gen_lattice()
+time_Atoms()
+time_PWGrid()
 
