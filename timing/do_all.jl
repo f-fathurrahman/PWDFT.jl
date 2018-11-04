2e3fa968f167c6923470d666338de1fdd2171b72
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-11-05 00:04:59 +0700

    @btime and Base.summarysize of Hamiltonian

--- timing/do_all.jl
+++ timing/do_all.jl
@@ -53,9 +53,9 @@ end
 function time_PWGrid()
 
     @printf("\n")
-    @printf("-----------------------\n")
-    @printf("Timing PWGrid functions\n")
-    @printf("-----------------------\n")
+    @printf("----------------------\n")
+    @printf("Timing PWGrid function\n")
+    @printf("----------------------\n")
     @printf("\n")
 
     ECUTWFC = range(15.0, stop=50.0, step=5.0)    
@@ -67,10 +67,63 @@ function time_PWGrid()
     end
 end
 
+function time_Hamiltonian()
+    
+    @printf("\n")
+    @printf("---------------------------\n")
+    @printf("Timing Hamiltonian function\n")
+    @printf("---------------------------\n")
+    @printf("\n")
+
+    atoms = Atoms( xyz_string="""
+        1
+    
+        H   0.0   0.0   0.0
+        """,
+        LatVecs=gen_lattice_sc(16.0) )
+    
+    pspfiles = ["../pseudopotentials/pade_gth/H-q1.gth"]    
+    ECUTWFC = range(15.0, stop=50.0, step=5.0)    
+
+    for ecutwfc in ECUTWFC
+        @printf("ecutwfc = %4.1f Ha : ", ecutwfc)
+        @btime Hamiltonian( $atoms, $pspfiles, $ecutwfc )
+    end
+
+end
+
+
+function size_Hamiltonian()
+    
+    @printf("\n")
+    @printf("---------------------\n")
+    @printf("Memory of Hamiltonian\n")
+    @printf("---------------------\n")
+    @printf("\n")
+
+    atoms = Atoms( xyz_string="""
+        1
+    
+        H   0.0   0.0   0.0
+        """,
+        LatVecs=gen_lattice_sc(16.0) )
+    
+    pspfiles = ["../pseudopotentials/pade_gth/H-q1.gth"]    
+    ECUTWFC = range(15.0, stop=50.0, step=5.0)    
+
+    for ecutwfc in ECUTWFC
+        mem_MB = Base.summarysize( Hamiltonian( atoms, pspfiles, ecutwfc ) )/1024/1024
+        @printf("ecutwfc = %4.1f Ha : %f MB\n", ecutwfc, mem_MB)
+    end
+
+end
+
+
 println()
 InteractiveUtils.versioninfo()
 
-time_gen_lattice()
-time_Atoms()
-time_PWGrid()
-
+#time_gen_lattice()
+#time_Atoms()
+#time_PWGrid()
+#time_Hamiltonian()
+size_Hamiltonian()
