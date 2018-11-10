adb707b66001421266df4f5f6a6681332feec0f0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-11-10 22:55:25 +0700

    1d Gaussian func

--- sandbox/pw_interpolation_1d.jl
+++ sandbox/pw_interpolation_1d.jl
@@ -1,19 +1,25 @@
 using Printf
 using FFTW
 
-#import Plots
-#Plots.gr()
+import Plots
+Plots.gr()
 
-#Plots.default( markersize=4 )
-#Plots.default( markershape=:circle )
-#Plots.default( framestyle=:box )
-#Plots.default( leg=false )
+Plots.default( markersize=4 )
+Plots.default( markershape=:circle )
+Plots.default( framestyle=:box )
+Plots.default( leg=false )
 
 # a periodic function
 function funcx( cx, L, x, alpha )
     return exp( alpha*cos(2.0*pi/L *(x - cx) ) )
 end
 
+function gauss1d( alpha::Float64, sigma::Float64, r::Float64 )
+    r2 = r[1]^2
+    c1 = 2*sigma^2
+    cc1 = sqrt(2*pi*sigma^2)
+    return alpha*exp(-r2/c1)/cc1
+end
 
 function test_main()
     Npoints = 25
@@ -22,12 +28,11 @@ function test_main()
     fx = zeros(Npoints)
     cx = 5.0
     alpha = 1.0
-    for i = 1:Npoints
-        fx[i] = funcx( cx, L, x[i], alpha)
+    sigma = 1.0
+    for ip = 1:Npoints
+        #fx[ip] = funcx( cx, L, x[ip], alpha)
+        fx[ip] = gauss1d(alpha, sigma, x[ip]-cx)
     end
-    
-    #Plots.plot(x, fx)
-    #Plots.savefig("TEMP_funcx_1d.pdf")
 
     fg = fft( fx )
 
@@ -45,14 +50,22 @@ function test_main()
 
     println()
 
+    f_interp = zeros(ComplexF64,Ng)
     for ip = 1:Npoints
         s = 0.0 + im*0.0
         for ig = 1:Ng
             ipp = idx_g2r[ig]
-            s = s + fg[ipp]*exp(im*G[ig]*x[ip])/sqrt(L)
+            s = s + fg[ipp]*exp(im*G[ig]*x[ip])/L
         end
+        f_interp[ip] = s
         @printf("%4d %18.10f %18.10f %18.10f\n", ip, fx[ip], real(s), imag(s))
     end
+
+    Plots.plot(x, abs.(real(f_interp)), leg=true)
+    Plots.plot!(x, imag(f_interp))
+    Plots.plot!(x, abs.(f_interp))
+    Plots.plot!(x, fx)
+    Plots.savefig("TEMP_funcx_1d_interp.pdf")
 end
 
 function mm_to_nn( mm::Int64,S::Int64 )
