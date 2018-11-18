ee1aec5a3c92df2d1dedf681bba5ceaf34f0b307
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-11-18 17:33:16 +0700

    Visualize the resulting Rhoe from guess_rhoe

--- sandbox/test_guess_rhoe.jl
+++ sandbox/test_guess_rhoe.jl
@@ -20,6 +20,19 @@ function test_atmlength()
 end
 #test_atmlength()
 
+
+function test_guess_rhoe_H2()
+    atoms = Atoms( xyz_file="../structures/H2.xyz", LatVecs=gen_lattice_sc(16.0) )
+    pspfiles = ["../pseudopotentials/pade_gth/H-q1.gth"]
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, 15.0 )
+    Rhoe = guess_rhoe( Ham )
+
+    write_xsf( "TEMP_H2_guess_rhoe.xsf", atoms )
+    write_xsf_data3d_crystal( "TEMP_H2_guess_rhoe.xsf", atoms, Ham.pw.Ns, Rhoe )
+end
+test_guess_rhoe_H2()
+
 function test_guess_rhoe_PtO()
     atoms = Atoms( xyz_file="../structures/PtO.xyz", LatVecs=gen_lattice_sc(16.0) )
     pspfiles = ["../pseudopotentials/pade_gth/Pt-q18.gth",
@@ -27,5 +40,8 @@ function test_guess_rhoe_PtO()
     ecutwfc = 15.0
     Ham = Hamiltonian( atoms, pspfiles, 15.0 )
     Rhoe = guess_rhoe( Ham )
+
+    write_xsf( "TEMP_PtO_guess_rhoe.xsf", atoms )
+    write_xsf_data3d_crystal( "TEMP_PtO_guess_rhoe.xsf", atoms, Ham.pw.Ns, Rhoe )
 end
 test_guess_rhoe_PtO()
