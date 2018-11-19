886733dcdf1f81523916c2e3acb96488d898a2db
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-11-19 12:47:33 +0700

    Test Pt_fcc

--- sandbox/ABINIT.jl
+++ sandbox/ABINIT.jl
@@ -301,9 +301,30 @@ function test_Al_fcc()
                   prefix_psp="../_compare/abinit_psp/",
                   prefix="../TEMP_ABINIT/" )
 end
-test_Al_fcc()
+#test_Al_fcc()
 
 function test_read_abinit_etotal()
     read_abinit_etotal("../TEMP_ABINIT/LOG1")
 end
-test_read_abinit_etotal()
+#test_read_abinit_etotal()
+
+function test_Pt_fcc()
+    # Atoms
+    atoms = Atoms(xyz_string_frac=
+        """
+        1
+
+        Pt  0.0  0.0  0.0
+        """, LatVecs=gen_lattice_fcc(3.9231*ANG2BOHR))
+
+    # Initialize Hamiltonian
+    pspfiles = ["../pseudopotentials/pade_gth/Pt-q18.gth"]
+    ecutwfc_Ry = 30.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc_Ry*0.5, xcfunc="PBE",
+                        meshk=[3,3,3], extra_states=4 )
+    
+    write_abinit( Ham, use_smearing=true,
+                  prefix_psp="../_compare/abinit_psp/",
+                  prefix="../TEMP_ABINIT/" )
+end
+test_Pt_fcc()
