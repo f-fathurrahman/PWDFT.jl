c5dbb1e5e2d977fd80f301181b39ab9bc8a3c095
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-12-12 21:33:45 +0700

    Test the resulting psp with pwscf

--- sandbox/PWSCF.jl
+++ sandbox/PWSCF.jl
@@ -1,13 +1,13 @@
 using Printf
 using PWDFT
 
-function write_psp_pwscf( psp::PsPot_GTH )
+function write_psp_pwscf( psp::PsPot_GTH; prefix="." )
     atsymb = psp.atsymb
     zval = psp.zval
     zatom = PWDFT.ZATOMS[atsymb]
     lmax = psp.lmax
     
-    f = open("TEMP_"*atsymb*".gth", "w")
+    f = open(prefix*"/"*atsymb*".gth", "w")
     @printf(f, "Hartwigsen-Goedecker-Hutter psp for %s, from PRB58, 3641 (1998)\n", atsymb)
     @printf(f, "%d %d 010605\n", Int(zatom), Int(zval))
     @printf(f, "10 1 %d 0 2001 0\n", lmax)
@@ -16,24 +16,47 @@ function write_psp_pwscf( psp::PsPot_GTH )
     rlocal = psp.rlocal
     c = psp.c
     ncoef = count(abs.(c) .> 0.0)
-    @printf(f, "%18.10f %d", rlocal, ncoef)
+    @printf(f, "%18.10f %5d", rlocal, ncoef)
     for i = 1:ncoef
         @printf(f, "%18.10f ", c[i])
     end
     @printf(f, "\n")
+
+
+    # Nonlocal part
+    Nproj_l = psp.Nproj_l
+    rc = psp.rc
+    h = psp.h
+    @printf(f, "%5d\n", lmax+1)
+    for l = 0:lmax
+        @printf(f, "%18.10f %5d", rc[l+1], Nproj_l[l+1])
+        for i = 1:Nproj_l[l+1]
+            for j = i:Nproj_l[l+1]
+                @printf(f, "%18.10f ", h[l+1,i,j])
+            end
+            @printf(f, "               \n")
+        end
+        if l > 0
+            for i = 1:Nproj_l[l+1]
+                for j = i:Nproj_l[l+1]
+                    @printf(f, "%18.10f ", 0.0)
+                end
+                @printf(f, "               \n")
+            end
+        end
+    end
+
     close(f)
 
-    @printf("%d\n", lmax+1)
-    for l = 0:lmax
-    end
     return
 end
 
 function test_main()
     psp = PsPot_GTH("../pseudopotentials/pade_gth/Pd-q10.gth")
-    println(psp)
-
-    write_psp_pwscf(psp)
+    #psp = PsPot_GTH("../pseudopotentials/pade_gth/Ga-q3.gth")
+    
+    kT = 0.001  # degauss in PWSCF uses Ry unit
+    write_psp_pwscf(psp, prefix="../TEMP_PWSCF")
 end
 
 test_main()
