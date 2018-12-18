8b0a103f88b37b3f3720e1406898900e65009c7a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-12-18 15:44:35 +0700

    Test SCF with potential (Ha and XC) mixing

--- sandbox/my_scf_potmix.jl
+++ sandbox/my_scf_potmix.jl
@@ -1,4 +1,4 @@
-function my_scf!( Ham::Hamiltonian; NiterMax=150, betamix=0.2 )
+function my_scf_potmix!( Ham::Hamiltonian; NiterMax=150, betamix=0.2 )
 
     Npoints = prod(Ham.pw.Ns)
     Nspin = Ham.electrons.Nspin
@@ -11,7 +11,9 @@ function my_scf!( Ham::Hamiltonian; NiterMax=150, betamix=0.2 )
     psiks = rand_BlochWavefunc(Ham)
     
     Rhoe = zeros(Float64,Npoints,Nspin)
-    Rhoe_new = zeros(Float64,Npoints,Nspin)
+
+    Vxc_inp = zeros(Float64,Npoints,Nspin)
+    VHa_inp = zeros(Float64,Npoints)
 
     Rhoe = calc_rhoe(Ham,psiks)
     update!(Ham, Rhoe)
@@ -24,23 +26,42 @@ function my_scf!( Ham::Hamiltonian; NiterMax=150, betamix=0.2 )
     Etot_old = 0.0
 
     @printf("\n")
-    @printf("SCF iteration starts (with density mixing), betamix = %f\n", betamix)
+    @printf("SCF iteration starts (with potential mixing), betamix = %f\n", betamix)
     @printf("\n")
 
     for iterSCF = 1:NiterMax
+        
         evals = diag_LOBPCG!( Ham, psiks )
-        Rhoe_new[:,:] = calc_rhoe( Ham, psiks )
-        Rhoe[:,:] = betamix*Rhoe_new + (1-betamix)*Rhoe
+        
+        Rhoe[:,:] = calc_rhoe( Ham, psiks )
+
+        # Save the old (input) potential
+        Vxc_inp[:,:] = Ham.potentials.XC
+        VHa_inp[:] = Ham.potentials.Hartree
+
+        # Update potentials
         update!(Ham, Rhoe)
+
+        # Now Ham.potentials contains new (output) potential
+        
+        # Calculate energies
         Ham.energies = calc_energies(Ham, psiks)
+
         Etot = sum(Ham.energies)
+
         diffEtot = abs(Etot - Etot_old)
+
         @printf("%5d %18.10f %18.10e\n", iterSCF, Etot, diffEtot)
         if diffEtot <= 1e-6
             @printf("SCF is converged in %d iterations\n", iterSCF)
             return
         end
         Etot_old = Etot
+
+        # Mix potentials (only Hartree and XC)
+        Ham.potentials.Hartree = betamix*Ham.potentials.Hartree + (1-betamix)*VHa_inp
+        Ham.potentials.XC = betamix*Ham.potentials.XC + (1-betamix)*Vxc_inp
+
         flush(stdout)
     end
     @printf("WARNING: SCF is not converged after %d iterations\n", NiterMax)
--- sandbox/test_my_scf_H2.jl
+++ sandbox/test_my_scf_H2.jl
@@ -5,24 +5,37 @@ using PWDFT
 Random.seed!(1234)
 
 include("my_scf.jl")
+include("my_scf_potmix.jl")
 
 const DIR_PWDFT = joinpath( dirname(pathof(PWDFT)), "..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
 
-function test_my_scf(β::Float64)
+function test_my_scf(β::Float64; mix_what=:density)
     atoms = Atoms(
         xyz_file=joinpath(DIR_PWDFT, "structures/H2.xyz"),
         LatVecs=gen_lattice_sc(16.0))
-    pspfiles = [joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth", "H-q1.gth")]
+    pspfiles = [joinpath(DIR_PSP, "H-q1.gth")]
     Ham = Hamiltonian( atoms, pspfiles, 15.0 )
     println(Ham)
 
-    my_scf!(Ham, betamix=β)
+    if mix_what == :density
+        my_scf!(Ham, betamix=β)
+    elseif mix_what == :potential
+        my_scf_potmix!(Ham, betamix=β)
+    else
+        println("mix_what = ", mix_what)
+        error("Unknown mix_what")
+    end
+
 
     println()
     println("Kohn-Sham energy components\n")
     println(Ham.energies)
 end
 
-for β in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    test_my_scf(β)
-end
+test_my_scf(0.4, mix_what=:density)
+test_my_scf(0.4, mix_what=:potential)
+
+#for β in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+#    test_my_scf(β)
+#end
