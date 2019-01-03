69eb1f6f402d61dd77a4de18e56a3fe0f4b8bb82
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-01-03 16:36:37 +0700

    Add timing comparison between calc_rhoe and calc_rhoe!

--- timing/run_timing.jl
+++ timing/run_timing.jl
@@ -14,6 +14,7 @@ if Nargs == 0
         "time_gen_lattice.jl",
         "time_Atoms.jl",
         "time_PWGrid.jl",
+        "time_calc_rhoe.jl",
         "time_Hamiltonian.jl",
         "time_op_kpt_1.jl"
     ]    
---
+++ timing/time_calc_Rhoe.jl
@@ -0,0 +1,39 @@
+function time_calc_rhoe()
+
+    @printf("\n")
+    @printf("---------------------------------\n")
+    @printf("Timing calc_rhoe (Nkpt=fcc-8x8x8)\n")
+    @printf("---------------------------------\n")
+    @printf("\n")
+
+    atoms = Atoms(xyz_string_frac=
+        """
+        1
+
+        Pt  0.0  0.0  0.0
+        """, LatVecs=gen_lattice_fcc(3.9231*ANG2BOHR))
+    
+    pspfiles = ["../pseudopotentials/pade_gth/Pt-q18.gth"]
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc,
+                       meshk=[8,8,8], extra_states=4 )
+        
+    # Shortcuts
+    pw = Ham.pw
+    Focc = Ham.electrons.Focc
+    CellVolume = pw.CellVolume
+    Ns = pw.Ns
+
+    Random.seed!(4321)
+    psiks = rand_BlochWavefunc( Ham )
+
+    Rhoe = zeros(prod(Ham.pw.Ns),2)
+
+    @printf("Using calc_rhoe:  ")
+    @btime $Rhoe = calc_rhoe( $Ham, $psiks )
+
+    @printf("Using calc_rhoe!: ")
+    @btime calc_rhoe!( $Ham, $psiks, $Rhoe )
+end
+
+time_calc_rhoe()
