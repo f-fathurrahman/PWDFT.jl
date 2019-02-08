f8f0c53083751a5237369ec899d435be15833bfb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-02-08 13:35:03 +0700

    Test for Si and GaAs, forces are different from QE result, it also varies with the choice of LatVecs, need to fix this

--- sandbox/forces_local_01/test_calc_forces_Ps_loc.jl
+++ sandbox/forces_local_01/test_calc_forces_Ps_loc.jl
@@ -47,4 +47,108 @@ function test_H2()
 
 end
 
-test_H2()
+function test_Si_fcc()
+
+    atoms = Atoms(xyz_string_frac=
+        """
+        2
+
+        Si  0.0  0.0  0.0
+        Si  0.25  0.25  0.25
+        """, in_bohr=true, LatVecs=gen_lattice_fcc(5.431*ANG2BOHR))
+
+    pspfiles = [joinpath(DIR_PSP, "Si-q4.gth")]
+
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, meshk=[3,3,3] )
+
+    println(Ham)
+
+    Random.seed!(1234)
+    #KS_solve_Emin_PCG!(Ham, ETOT_CONV_THR=1e-8)
+    KS_solve_SCF!(Ham, mix_method="rpulay", ETOT_CONV_THR=1e-6)
+
+    Natoms = atoms.Natoms
+    atsymbs = atoms.atsymbs
+
+    println("")
+    F_Ps_loc = calc_forces_Ps_loc( Ham.atoms, Ham.pw, Ham.pspots, Ham.rhoe )*2.0
+    for ia = 1:Natoms
+        @printf("%s %18.10f %18.10f %18.10f\n", atsymbs[ia],
+                F_Ps_loc[1,ia], F_Ps_loc[2,ia], F_Ps_loc[3,ia] )
+    end
+
+    println("")
+    F_Ps_loc = calc_forces_Ps_loc_finite_diff( Ham.atoms, Ham.pw, Ham.pspots, Ham.rhoe )*2.0
+    println("Using finite difference")
+    for ia = 1:Natoms
+        @printf("%s %18.10f %18.10f %18.10f\n", atsymbs[ia],
+                F_Ps_loc[1,ia], F_Ps_loc[2,ia], F_Ps_loc[3,ia] )
+    end
+
+
+end
+
+
+function test_GaAs()
+
+    LatVecs = zeros(3,3)
+    LatVecs[:,1] = [0.5, 0.5, 0.0]
+    LatVecs[:,2] = [0.5, 0.0, 0.5]
+    LatVecs[:,3] = [0.0, 0.5, 0.5]
+    LatVecs = LatVecs*5.6537*ANG2BOHR
+
+    atoms = Atoms(xyz_string_frac=
+        """
+        2
+
+        Ga  0.0  0.0  0.0
+        As  0.25  0.25  0.25
+        """, in_bohr=true, LatVecs=gen_lattice_fcc(5.6537*ANG2BOHR))
+
+#    atoms = Atoms(xyz_string_frac=
+#        """
+#        2
+#
+#        Ga  0.0  0.0  0.0
+#        As  0.25  0.25  0.25
+#        """, in_bohr=true, LatVecs=LatVecs)
+
+    pspfiles = [joinpath(DIR_PSP, "Ga-q3.gth"),
+                joinpath(DIR_PSP, "As-q5.gth")]
+
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, meshk=[3,3,3] )
+
+    println(Ham)
+
+    Random.seed!(1234)
+    #KS_solve_Emin_PCG!(Ham, ETOT_CONV_THR=1e-8)
+    KS_solve_SCF!(Ham, mix_method="rpulay", ETOT_CONV_THR=1e-6)
+
+    Natoms = atoms.Natoms
+    atsymbs = atoms.atsymbs
+
+    println("")
+    F_Ps_loc = calc_forces_Ps_loc( Ham.atoms, Ham.pw, Ham.pspots, Ham.rhoe )*2.0
+    for ia = 1:Natoms
+        @printf("%s %18.10f %18.10f %18.10f\n", atsymbs[ia],
+                F_Ps_loc[1,ia], F_Ps_loc[2,ia], F_Ps_loc[3,ia] )
+    end
+
+    println("")
+    F_Ps_loc = calc_forces_Ps_loc_finite_diff( Ham.atoms, Ham.pw, Ham.pspots, Ham.rhoe )*2.0
+    println("Using finite difference")
+    for ia = 1:Natoms
+        @printf("%s %18.10f %18.10f %18.10f\n", atsymbs[ia],
+                F_Ps_loc[1,ia], F_Ps_loc[2,ia], F_Ps_loc[3,ia] )
+    end
+
+
+
+end
+
+
+#test_H2()
+#test_Si_fcc()
+test_GaAs()
