c94ec66f2547f71e43950152be6f2c24af04c56d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-03-31 01:41:32 +0700

    Add script to generate some data for EOS calculation vs ABINIT

---
+++ sandbox/eos_02/Pt_fcc/gen_data.jl
@@ -0,0 +1,78 @@
+using Printf
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)), "..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+include(joinpath(DIR_PWDFT, "sandbox", "ABINIT.jl"))
+include(joinpath(DIR_PWDFT, "sandbox", "KS_solve_SCF_potmix.jl"))
+
+
+function init_Ham_Pt_fcc( a::Float64, meshk::Array{Int64,1} )
+    atoms = Atoms(xyz_string_frac=
+        """
+        1
+
+        Pt  0.0  0.0  0.0
+        """, LatVecs=gen_lattice_fcc(a))
+
+    # Initialize Hamiltonian
+    pspfiles = [joinpath(DIR_PSP, "Pt-q10.gth")]
+    ecutwfc = 30.0
+    return Hamiltonian( atoms, pspfiles, ecutwfc, xcfunc="VWN",
+                       meshk=meshk, extra_states=4 )
+end
+
+
+
+
+function main()
+
+    LATCONST = 3.9231*ANG2BOHR
+
+    scal_i = 0.8
+    scal_f = 1.2
+    Ndata = 15
+    Δ = (scal_f - scal_i)/(Ndata-1)
+    
+    stdout_orig = stdout
+
+    f = open("TEMP_EOS_data.dat", "w")
+
+    for i = 1:Ndata
+
+        scal = scal_i + (i-1)*Δ
+
+        a = LATCONST*scal
+
+        fname = "TEMP_LOG_"*string(i)
+        fileout = open(fname, "w")
+        
+        redirect_stdout(fileout)
+    
+        Ham = init_Ham_Pt_fcc( a, [8,8,8] )
+        KS_solve_SCF!( Ham, use_smearing=true, kT=0.001,
+                       mix_method="rpulay", print_final_ebands=false )
+
+        run(`rm -fv TEMP_abinit/\*`)
+        write_abinit(Ham, prefix_dir="./TEMP_abinit/", use_smearing=true, kT=0.001)
+        cd("./TEMP_abinit")
+        run(pipeline(`abinit`, stdin="FILES", stdout="ABINIT_o_LOG"))
+        cd("../")
+
+        abinit_energies = read_abinit_etotal("TEMP_abinit/LOG1")
+        println("\nABINIT result\n")
+        println(abinit_energies)
+
+        close(fileout)
+
+        @printf(f, "%18.10f %18.10f %18.10f\n", a, sum(Ham.energies), sum(abinit_energies) )
+    end
+
+    close(f)
+
+end
+
+main()
+
---
+++ sandbox/eos_02/Si_fcc/gen_data.jl
@@ -0,0 +1,72 @@
+using Printf
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)), "..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+include(joinpath(DIR_PWDFT, "sandbox", "ABINIT.jl"))
+include(joinpath(DIR_PWDFT, "sandbox", "KS_solve_SCF_potmix.jl"))
+
+function init_Ham_Si_fcc( a::Float64, meshk::Array{Int64,1} )
+    atoms = Atoms(xyz_string_frac=
+        """
+        2
+
+        Si  0.0  0.0  0.0
+        Si  0.25  0.25  0.25
+        """, in_bohr=true, LatVecs=gen_lattice_fcc(a))
+
+    pspfiles = [joinpath(DIR_PSP, "Si-q4.gth")]
+    ecutwfc = 15.0
+    return Hamiltonian( atoms, pspfiles, ecutwfc, meshk=meshk )
+end
+
+function main()
+
+    LATCONST = 10.2631
+
+    scal_i = 0.8
+    scal_f = 1.2
+    Ndata = 15
+    Δ = (scal_f - scal_i)/(Ndata-1)
+    
+    stdout_orig = stdout
+
+    f = open("TEMP_EOS_data.dat", "w")
+
+    for i = 1:Ndata
+
+        scal = scal_i + (i-1)*Δ
+
+        a = LATCONST*scal
+
+        fname = "TEMP_LOG_"*string(i)
+        fileout = open(fname, "w")
+        
+        redirect_stdout(fileout)
+    
+        Ham = init_Ham_Si_fcc( a, [3,3,3] )
+        KS_solve_SCF_potmix!( Ham )
+
+        run(`rm -fv TEMP_abinit/\*`)
+        write_abinit(Ham, prefix_dir="./TEMP_abinit/")
+        cd("./TEMP_abinit")
+        run(pipeline(`abinit`, stdin="FILES", stdout="ABINIT_o_LOG"))
+        cd("../")
+
+        abinit_energies = read_abinit_etotal("TEMP_abinit/LOG1")
+        println("\nABINIT result\n")
+        println(abinit_energies)
+
+        close(fileout)
+
+        @printf(f, "%18.10f %18.10f %18.10f\n", a, sum(Ham.energies), sum(abinit_energies) )
+    end
+
+    close(f)
+
+end
+
+main()
+
