4a69d03a0ee5555bf8b4cdb0260a2e5e34334c70
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-05-14 17:23:15 +0700

    Adding non_symmorphic to SymmetryInfo

--- src/SymmetryInfo.jl
+++ src/SymmetryInfo.jl
@@ -3,6 +3,7 @@ struct SymmetryInfo
     s::Array{Int64,3}
     inv_s::Array{Int64,3}
     ft::Array{Float64,2}
+    non_symmorphic::Array{Bool,1}
 end
 
 
@@ -14,7 +15,15 @@ function SymmetryInfo( atoms::Atoms )
         inv_s[:,:,isym] = Base.convert(Array{Int64,2}, inv(s[:,:,isym]))
     end
 
-    return SymmetryInfo(Nsyms, s, inv_s, ft)
+    non_symmorphic = zeros(Bool,Nsyms)
+    SMALL = 1e-10
+    for isym = 1:Nsyms
+        non_symmorphic[isym] = ( (abs(ft[1,isym]) >= SMALL) ||
+                                 (abs(ft[2,isym]) >= SMALL) ||
+                                 (abs(ft[3,isym]) >= SMALL) )
+    end
+
+    return SymmetryInfo(Nsyms, s, inv_s, ft, non_symmorphic)
 end
 
 include("SymmetryInfo_io.jl")
--- src/SymmetryInfo_io.jl
+++ src/SymmetryInfo_io.jl
@@ -3,6 +3,7 @@ function println( sym_info::SymmetryInfo )
     s = sym_info.s
     inv_s = sym_info.inv_s
     ft = sym_info.ft
+    non_symmorphic = sym_info.non_symmorphic
     
     @printf("Nsyms = %d\n", sym_info.Nsyms)
     
@@ -13,6 +14,8 @@ function println( sym_info::SymmetryInfo )
             @printf("%2d %2d %2d\n", s[i,1,isym], s[i,2,isym], s[i,3,isym])
         end
         @printf("Translation: %13.10f %13.10f %13.10f\n", ft[1,isym], ft[2,isym], ft[3,isym])
+        println("non_symmorphic:  ", non_symmorphic[isym])
     end
+    println("No of non_symmorphic translations = ", count(non_symmorphic))
     return
 end
