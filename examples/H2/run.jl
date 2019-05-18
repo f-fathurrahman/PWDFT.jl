41f416bfe30775f5881ee7a85a02cfe82a1d9ede
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-05-18 23:32:49 +0700

    Remove compare dir and add some examples

---
+++ examples/H2/run.jl
@@ -0,0 +1,39 @@
+using LinearAlgebra
+using Random
+using Printf
+
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+function main( ; method="SCF" )
+
+    # Atoms
+    atoms = Atoms( xyz_file=joinpath(DIR_STRUCTURES, "H2.xyz"),
+                   LatVecs = gen_lattice_sc(16.0) )
+
+    # Initialize Hamiltonian
+    pspfiles = [joinpath(DIR_PSP, "H-q1.gth")]
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc )
+    println(Ham)
+
+    if method == "SCF"
+        KS_solve_SCF!( Ham, mix_method="pulay" )
+
+    elseif method == "Emin"
+        KS_solve_Emin_PCG!( Ham )
+
+    else
+        error( @sprintf("ERROR: unknown method = %s", method) )
+    end
+
+end
+
+@time main(method="SCF")
+@time main(method="Emin")
+
+@time main(method="SCF")
+@time main(method="Emin")
---
+++ examples/Si_fcc_PBE/run.jl
@@ -0,0 +1,46 @@
+using Random
+using Printf
+
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pbe_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+
+function main( ; method="SCF" )
+    # Atoms
+    atoms = Atoms(xyz_string_frac=
+        """
+        2
+
+        Si  0.0  0.0  0.0
+        Si  0.25  0.25  0.25
+        """, in_bohr=true, LatVecs=gen_lattice_fcc(10.2631))
+
+    # Initialize Hamiltonian
+    pspfiles = [joinpath(DIR_PSP, "Si-q4.gth")]
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, meshk=[3,3,3], xcfunc="PBE" )
+    println(Ham)
+
+    #
+    # Solve the KS problem
+    #
+    if method == "SCF"
+        KS_solve_SCF!( Ham, mix_method="anderson" )
+
+    elseif method == "Emin"
+        KS_solve_Emin_PCG!( Ham, verbose=true )
+
+    else
+        error( @sprintf("Unknown method %s", method) )
+    end
+    
+end
+
+@time main(method="SCF")
+@time main(method="Emin")
+
+@time main(method="SCF")
+@time main(method="Emin")
