89d6fd2f362c5e7e1825f4d95e3a13d64cabd1ad
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-06-07 10:10:19 +0700

    Also print V0

--- sandbox/eos_02/Si_fcc/do_fit_EOS_fcc.py
+++ sandbox/eos_02/Si_fcc/do_fit_EOS_fcc.py
@@ -14,15 +14,18 @@ energies_pwscf  = dat[:,3]*Hartree
 
 eos = EquationOfState(volumes, energies)
 v0, e0, B = eos.fit()
-print("PWDFT.jl: B = %18.10f GPa" % (B/kJ * 1.0e24))
+print("PWDFT.jl: B  = %18.10f GPa" % (B/kJ * 1.0e24))
+print("PWDFT.jl: V0 = %18.10f angstrom^3/atom" % (v0/2))
 eos.plot("pwdft-eos-ase.pdf")
 
 eos = EquationOfState(volumes, energies_abinit)
 v0, e0, B = eos.fit()
-print("ABINIT  : B = %18.10f GPa" % (B/kJ * 1.0e24))
+print("ABINIT  : B  = %18.10f GPa" % (B/kJ * 1.0e24))
+print("ABINIT  : V0 = %18.10f angstrom^3/atom" % (v0/2))
 eos.plot("abinit-eos-ase.pdf")
 
 eos = EquationOfState(volumes, energies_pwscf)
 v0, e0, B = eos.fit()
-print("PWSCF   : B = %18.10f GPa" % (B/kJ * 1.0e24))
+print("PWSCF   : B  = %18.10f GPa" % (B/kJ * 1.0e24))
+print("PWSCF   : V0 = %18.10f angstrom^3/atom" % (v0/2))
 eos.plot("pwscf-eos-ase.pdf")
