6272d92a78d2f69008e8cefccf29a9c85ef78adb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-06-14 21:31:51 +0700

    Test ABINIT again, simplify

--- sandbox/abinit_01/test_ABINIT.jl
+++ sandbox/abinit_01/test_ABINIT.jl
@@ -17,7 +17,14 @@ function test_CuSO4()
     ecutwfc = 15.0
     Ham = Hamiltonian( atoms, pspfiles, ecutwfc )
 
-    write_abinit( Ham, prefix_dir="./TEMP_abinit/" )
+    run(`rm -rfv TEMP_abinit/\*`)
+    write_abinit( Ham, use_smearing=true, kT=0.01, prefix_dir="./TEMP_abinit/" )
+    cd("./TEMP_abinit")
+    run(pipeline(`abinit`, stdin="FILES", stdout="LOG1"))
+    cd("../")
+
+    energies = read_abinit_etotal("TEMP_abinit/LOG1")
+    println(energies)
 end
 
 
@@ -29,7 +36,14 @@ function test_H2()
     ecutwfc = 15.0
     Ham = Hamiltonian( atoms, pspfiles, ecutwfc )
 
+    run(`rm -rfv TEMP_abinit/\*`)
     write_abinit( Ham, prefix_dir="./TEMP_abinit/" )
+    cd("./TEMP_abinit")
+    run(pipeline(`abinit`, stdin="FILES", stdout="LOG1"))
+    cd("../")
+
+    energies = read_abinit_etotal("TEMP_abinit/LOG1")
+    println(energies)
 end
 
 
@@ -47,13 +61,17 @@ function test_Al_fcc()
     Ham = Hamiltonian( atoms, pspfiles, ecutwfc, xcfunc="LDA",
                        Nspin=1, meshk=[8,8,8], extra_states=4 )
 
-    write_abinit( Ham, use_smearing=true,
-                  prefix_dir="./TEMP_abinit_Al_fcc/" )
+    run(`rm -rfv TEMP_abinit/\*`)
+    write_abinit( Ham, use_smearing=true, kT=0.01, prefix_dir="./TEMP_abinit/" )
+    cd("./TEMP_abinit")
+    run(pipeline(`abinit`, stdin="FILES", stdout="LOG1"))
+    cd("../")
+
+    energies = read_abinit_etotal("TEMP_abinit/LOG1")
+    println(energies)
+
 end
 
-function test_read_abinit_etotal(filename)
-    println( read_abinit_etotal(filename) )
-end
 
 function test_Pt_fcc()
     # Atoms
@@ -66,23 +84,22 @@ function test_Pt_fcc()
 
     # Initialize Hamiltonian
     pspfiles = [joinpath(DIR_PSP, "Pt-q18.gth")]
-    ecutwfc_Ry = 30.0
-    Ham = Hamiltonian( atoms, pspfiles, ecutwfc_Ry*0.5, xcfunc="PBE",
+    ecutwfc = 30.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, xcfunc="PBE",
                         meshk=[3,3,3], extra_states=4 )
-
-    #KS_solve_SCF!(Ham, use_smearing=true, mix_method="rpulay")
     
-    write_abinit( Ham, use_smearing=true,
-                  prefix_dir="./TEMP_abinit_Pt_fcc/" )
+    run(`rm -rfv TEMP_abinit/\*`)
+    write_abinit( Ham, use_smearing=true, kT=0.01, prefix_dir="TEMP_abinit/" )
+    cd("./TEMP_abinit")
+    run(pipeline(`abinit`, stdin="FILES", stdout="LOG1"))
+    cd("../")
+
+    energies = read_abinit_etotal("TEMP_abinit/LOG1")
+    println(energies)
+
 end
 
 test_CuSO4()
 test_H2()
 test_Al_fcc()
 test_Pt_fcc()
-
-test_read_abinit_etotal("TEMP_abinit_Al_fcc/LOG1")
-println("")
-test_read_abinit_etotal("TEMP_abinit_Si/LOG1")
-println("")
-test_read_abinit_etotal("TEMP_abinit_Pt_fcc/LOG1")
