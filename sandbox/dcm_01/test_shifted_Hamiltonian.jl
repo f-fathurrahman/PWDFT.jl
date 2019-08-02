fb0c03bb4a84d74cc463cd270010abaf6957e611
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-08-02 13:52:33 +0700

    Movin the code related to shifted Hamiltonian

--- sandbox/dcm_01/test_shifted_Hamiltonian.jl
+++ sandbox/dcm_01/test_shifted_Hamiltonian.jl
@@ -1,3 +1,17 @@
+using PWDFT
+
+include("my_trscf.jl")
+include("test_shifted_Hamiltonian.jl")
+
+mutable struct ShiftedHamiltonian
+    Ham::Ref{Hamiltonian}
+    σ::Float64
+    psiks_old::BlochWavefunc
+end
+
+function op_H( sHam::ShiftedHamiltonian, psi::BlochWavefunc )
+end
+
 function test_shifted_Hamiltonian( σ::Float64, Ham, psiks_old, psiks )
 
     Hpsi = op_H( Ham, psiks )
@@ -7,7 +21,22 @@ function test_shifted_Hamiltonian( σ::Float64, Ham, psiks_old, psiks )
         term1[i] = Hpsi[i] - σ*psiks_old[i] * ( psiks_old[i]' * psiks[i] )
     end
 
-    #println("Pass here")
-
     return
 end
+
+function main()
+
+    Ham = create_Ham_Si_fcc()
+
+    psiks_old = rand_BlochWavefunc( Ham )
+    Rhoe = calc_rhoe( Ham, psiks_old )
+
+    update!( Ham, Rhoe )
+
+    psiks = rand_BlochWavefunc( Ham )
+    @time test_shifted_Hamiltonian( 0.5, Ham, psiks_old, psiks )
+    @time test_shifted_Hamiltonian( 0.5, Ham, psiks_old, psiks )
+
+end
+
+main()
