f9aeb32070b5f859aba0210f114da3045f051cf3
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-08-19 00:30:56 +0700

    Add Pt atom without extra_states

--- sandbox/NLopt_v2/create_Ham.jl
+++ sandbox/NLopt_v2/create_Ham.jl
@@ -23,6 +23,19 @@ function create_Ham_atom_Pt_smearing()
     return Hamiltonian( atoms, pspfiles, ecutwfc, extra_states=4 )
 end
 
+# without extra_states
+function create_Ham_atom_Pt()
+    atoms = Atoms(xyz_string_frac=
+        """
+        1
+
+        Pt  0.0  0.0  0.0
+        """, LatVecs=gen_lattice_sc(16.0))
+    pspfiles = [joinpath(DIR_PSP, "Pt-q10.gth")]
+    ecutwfc = 30.0
+    return Hamiltonian( atoms, pspfiles, ecutwfc )
+end
+
 function create_Ham_atom_Al_smearing()
     atoms = Atoms(xyz_string_frac=
         """
