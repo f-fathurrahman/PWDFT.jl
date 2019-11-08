7e7b9ee84a776f03e0e41f6198ee3e9482339677
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-11-09 01:21:58 +0700

    Add VWN

---
+++ sandbox/xc_01/XC_c_vwn.jl
@@ -0,0 +1,24 @@
+function XC_c_vwn( rs )
+
+    a = 0.0310907
+    b = 3.72744
+    c = 12.9352
+    x0 = -0.10498
+
+    q = sqrt(4.0 * c - b * b)
+    f1 = 2.0 * b / q
+    f2 = b * x0 / (x0 * x0 + b * x0 + c)
+    f3 = 2.0 * (2.0 * x0 + b) / q
+    rs12 = sqrt(rs)
+    fx = rs + b * rs12 + c
+    qx = atan( q/(2.0 * rs12 + b) )
+    ec = a * ( log(rs/fx) + f1 * qx - f2 * ( log( (rs12 - x0)^2 /fx) + f3 * qx) )
+    
+    tx = 2.0 * rs12 + b
+    tt = tx * tx + q * q
+    vc = ec - rs12 * a / 6.0 * (2.0 / rs12 - tx / fx - 4.0 * b / tt 
+        - f2 * (2.0 / (rs12 - x0) - tx / fx - 4.0 * (2.0 * x0 + b) / tt) )
+
+    return ec, vc
+
+end
--- sandbox/xc_01/test_XC.jl
+++ sandbox/xc_01/test_XC.jl
@@ -2,6 +2,9 @@ using Printf
 
 include("XC_x_slater.jl")
 include("XC_c_pw.jl")
+include("XC_c_vwn.jl")
+
+include("XC_c_vwn_spin.jl")
 
 include("XC_x_pbe.jl")
 include("XC_c_pbe.jl")
@@ -9,6 +12,10 @@ include("XC_c_pbe.jl")
 function test_main()
     println( XC_x_slater(1.2) )
     println( XC_c_pw(1.2) )
+
+    println( XC_c_vwn(1.2) )
+
+    println( XC_c_vwn_spin(1.2, 0.1) )
 
     println( XC_x_pbe(1.2, 1.1) )
     println( XC_c_pbe(1.2, 1.1) )
