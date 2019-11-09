a18bf02a14cf062d8f56c6666f9556397629fcfd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-11-09 23:21:03 +0700

    Test slater_spin

--- sandbox/xc_01/test_XC.jl
+++ sandbox/xc_01/test_XC.jl
@@ -4,6 +4,7 @@ include("XC_x_slater.jl")
 include("XC_c_pw.jl")
 include("XC_c_vwn.jl")
 
+include("XC_x_slater_spin.jl")
 include("XC_c_vwn_spin.jl")
 
 include("XC_x_pbe.jl")
@@ -15,6 +16,7 @@ function test_main()
 
     println( XC_c_vwn(1.2) )
 
+    println( XC_x_slater_spin(1.2, 0.1) )
     println( XC_c_vwn_spin(1.2, 0.1) )
 
     println( XC_x_pbe(1.2, 1.1) )
