470f5e307bfd27e401d17785d991946fff08fb73
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-04 16:56:26 +0700

    Don't override

--- sandbox/cuda_01/utils_CuArrays.jl
+++ sandbox/cuda_01/utils_CuArrays.jl
@@ -1,7 +1,14 @@
 #
 # XXX: Should only works for Hermitian matrix
 #
-import LinearAlgebra: eigen
-function eigen( d_A::CuArray{ComplexF64,2,Nothing} )
-    return CUSOLVER.heevd!('V', 'U', d_A)
+import LinearAlgebra: eigen, eigvals
+
+function eigen( d_A::CuArray{ComplexF64,2} )
+    d_A_c = copy(d_A)
+    return CUSOLVER.heevd!('V', 'U', d_A_c)
+end
+
+function eigvals( d_A::CuArray{ComplexF64,2} )
+    d_A_c = copy(d_A)
+    return CUSOLVER.heevd!('N', 'U', d_A_c)
 end
