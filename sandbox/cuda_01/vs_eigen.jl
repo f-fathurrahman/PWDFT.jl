cbaf6cf1905a3e75924b3ff738981f61110a66d1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-04 16:50:12 +0700

    Eigenvectors calculations cpu vs gpu

---
+++ sandbox/cuda_01/vs_eigen.jl
@@ -0,0 +1,31 @@
+using CuArrays
+using CuArrays.CUSOLVER
+using LinearAlgebra
+using Printf
+
+include("utils_CuArrays.jl")
+
+function main( ; N=5)
+
+    U = rand(ComplexF64,N,N)
+    U = 0.5*(U + U')
+
+    d_U = CuArray(U)
+
+    println("N = ", N)
+
+    @printf("Using CPU     : ")
+    F = eigen(U)
+    @time F = eigen(U)
+
+    @printf("Using CUSOLVER: ")
+    d_V, d_W = eigen(d_U)
+    @time d_V, d_W = eigen(d_U)
+
+    if N <= 5
+        println(F.values)
+        println(d_V)
+    end
+end
+
+main(N=1000)
