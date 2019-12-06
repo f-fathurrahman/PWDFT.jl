057ea2fcfca937a07e71cebc96acd8c3656e6011
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-06 23:25:06 +0700

    CuGVectors and CuGVectorsW

--- sandbox/cuda_01/test_copy_PWGrid.jl
+++ sandbox/cuda_01/test_copy_PWGrid.jl
@@ -4,23 +4,39 @@ using PWDFT
 
 struct CuGVectors
     Ng::Int64
-    G::Array{Float64,2}
-    G2::Array{Float64,1}
-    idx_g2r::Array{Int64,1}
-    G2_shells::Array{Float64,1}
-    idx_g2shells::Array{Int64,1}
+    G::CuArray{Float64,2}
+    G2::CuArray{Float64,1}
+    idx_g2r::CuArray{Int64,1}
+    G2_shells::CuArray{Float64,1}
+    idx_g2shells::CuArray{Int64,1}
 end
+
+function CuGVectors( gvec::GVectors )
+
+    G = CuArray( gvec.G )
+    G2 = CuArray( gvec.G2 )
+    idx_g2r = CuArray( gvec.idx_g2r )
+    G2_shells = CuArray( gvec.G2_shells )
+    idx_g2shells  = CuArray( gvec.idx_g2shells )
+
+    return CuGVectors(gvec.Ng, G, G2, idx_g2r, G2_shells, idx_g2shells)
+end
+
+
+
 
 struct CuGVectorsW
     Ngwx::Int64
     Ngw::Array{Int64,1}
-    idx_gw2g  # two dim array the size if idx_gw2r
-    idx_gw2r
-    kpoints
+    idx_gw2g::Array{CuArray{Int64,1},1}
+    idx_gw2r::Array{CuArray{Int64,1},1}
+    kpoints::KPoints
 end
 
-
-function init_CuGVectorsW( gvecw::GVectorsW )
+"""
+Initialize CuGVectorsW from an instance of `GVectorsW` by direct copy.
+"""
+function CuGVectorsW( gvecw::GVectorsW )
     
     Ngwx = gvecw.Ngwx
     Ngw = gvecw.Ngw
@@ -28,18 +44,15 @@ function init_CuGVectorsW( gvecw::GVectorsW )
     kpoints = gvecw.kpoints
     Nkpt = kpoints.Nkpt
     
-    idx_gw2g = CuArrays.fill(0, (Ngwx, Nkpt))
-    idx_gw2r = CuArrays.fill(0, (Ngwx, Nkpt))
+    TYP = typeof(CuArray([1]))
+    idx_gw2g = Array{TYP,1}(undef,Nkpt)
+    idx_gw2r = Array{TYP,1}(undef,Nkpt)
     
     # Copy to GPU
     for ik in 1:Nkpt
-        Ngwk = Ngw[ik]
-        idx_gw2g[1:Ngwk] = CuArray( gvecw.idx_gw2g[ik] )
-        idx_gw2r[1:Ngwk] = CuArray( gvecw.idx_gw2r[ik] )
+        idx_gw2g[ik] = CuArray( gvecw.idx_gw2g[ik] )
+        idx_gw2r[ik] = CuArray( gvecw.idx_gw2r[ik] )
     end
-    
-    println(typeof(idx_gw2g))
-    println(typeof(idx_gw2r))
 
     return CuGVectorsW( Ngwx, Ngw, idx_gw2g, idx_gw2g, kpoints )
 
@@ -48,7 +61,9 @@ end
 function main()
 
     pw = PWGrid(15.0, gen_lattice_fcc(10.0))
-    init_CuGVectorsW( pw.gvecw)
+    
+    cu_gvec = CuGVectors( pw.gvec )
+    cu_gvecw = CuGVectorsW( pw.gvecw )
 
     println("Pass here")
 end
