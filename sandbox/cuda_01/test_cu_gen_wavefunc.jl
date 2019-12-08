85bcd1b14bcd17ce44911f3d462a3d299d117b66
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-08 17:27:55 +0700

    Set Nspin 1

--- sandbox/cuda_01/test_cu_gen_wavefunc.jl
+++ sandbox/cuda_01/test_cu_gen_wavefunc.jl
@@ -1,28 +1,14 @@
-using Printf
-using FFTW
-using LinearAlgebra
-
-using CuArrays
-
-using PWDFT
-
-include("CuPWGrid.jl")
-
-include("cu_types_aliases.jl")
-
-include("cu_ortho_gram_schmidt.jl")
-include("cu_gen_wavefunc.jl")
-include("cu_ortho_check.jl")
+include("PWDFT_cuda.jl")
 
 function main()
 
     pw = CuPWGrid(15.0, gen_lattice_fcc(10.0))
 
-    psiks = zeros_CuBlochWavefunc(pw, 10, 3)
+    psiks = zeros_CuBlochWavefunc(pw, 10, 1)
 
     println(typeof(psiks))
 
-    psiks2 = rand_CuBlochWavefunc(pw, 10, 3)
+    psiks2 = rand_CuBlochWavefunc(pw, 10, 1)
     ortho_check(psiks2[1])
 
     println("Pass here")
