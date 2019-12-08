72f00e2d891b6bf5fa6c9e85af045fe3f4debbbb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-08 11:50:42 +0700

    Moving to another file

--- sandbox/cuda_01/test_kernel_op_K.jl
+++ sandbox/cuda_01/test_kernel_op_K.jl
@@ -4,21 +4,6 @@ using PWDFT
 
 include("CuPWGrid.jl")
 
-function kernel_op_K( G, idx_gw2g_k, k1, k2, k3, psi, Kpsi )
-
-    igk = ( blockIdx().x - 1 )*blockDim().x + threadIdx().x
-    #strd = blockDim().x * gridDim().x
-
-    if igk <= length(idx_gw2g_k)
-        @inbounds ig = idx_gw2g_k[igk]
-        @inbounds Gw2 = (G[1,ig] + k1)^2 + (G[2,ig] + k2)^2 + (G[3,ig] + k3)^2
-        @inbounds Kpsi[igk] = psi[igk]*Gw2
-    end
-
-    return
-end
-
-
 function main()
 
     pw = PWGrid(20.0, gen_lattice_fcc(10.0))
