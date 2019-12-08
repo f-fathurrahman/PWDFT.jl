e4fde73537bdaa186ddcaa8cc3aefb724bf2405b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-08 10:11:22 +0700

    Moving to CuPWGrid

--- sandbox/cuda_01/CuPWGrid.jl
+++ sandbox/cuda_01/CuPWGrid.jl
@@ -1,45 +1,72 @@
-using CuArrays
-using LinearAlgebra
+# CuArray version of GVectors
+struct CuGVectors
+    Ng::Int64
+    G::CuArray{Float64,2}
+    G2::CuArray{Float64,1}
+    idx_g2r::CuArray{Int64,1}
+    G2_shells::CuArray{Float64,1}
+    idx_g2shells::CuArray{Int64,1}
+end
 
-using PWDFT
+#
+# Simple copy version
+#
+function CuGVectors( gvec::GVectors )
+    G = CuArray( gvec.G )
+    G2 = CuArray( gvec.G2 )
+    idx_g2r = CuArray( gvec.idx_g2r )
+    G2_shells = CuArray( gvec.G2_shells )
+    idx_g2shells = CuArray( gvec.idx_g2shells )
 
-function CuPWGrid( ecutwfc::Float64, LatVecs::Array{Float64,2}; kpoints=nothing, Ns_=(0,0,0) )
+    return CuGVectors(gvec.Ng, G, G2, idx_g2r, G2_shells, idx_g2shells)
+end
 
-    ecutrho = 4.0*ecutwfc
-    #
-    RecVecs = 2*pi*inv(Matrix(LatVecs'))
-    #RecVecs = 2*pi*invTrans_m3x3(LatVecs)
 
-    CellVolume = abs(det(LatVecs))
-    #
-    LatVecsLen = Array{Float64}(undef,3)
-    LatVecsLen[1] = norm(LatVecs[:,1])
-    LatVecsLen[2] = norm(LatVecs[:,2])
-    LatVecsLen[3] = norm(LatVecs[:,3])
+# CuArray version of GVectorsW
+struct CuGVectorsW
+    Ngwx::Int64
+    Ngw::Array{Int64,1}
+    idx_gw2g::Array{CuArray{Int64,1},1}
+    idx_gw2r::Array{CuArray{Int64,1},1}
+    kpoints::KPoints
+end
 
-    Ns1 = 2*round( Int64, sqrt(ecutrho/2)*LatVecsLen[1]/pi ) + 1
-    Ns2 = 2*round( Int64, sqrt(ecutrho/2)*LatVecsLen[2]/pi ) + 1
-    Ns3 = 2*round( Int64, sqrt(ecutrho/2)*LatVecsLen[3]/pi ) + 1
 
-    if any(Ns_ .== 0)
-        Ns1 = good_fft_order(Ns1)
-        Ns2 = good_fft_order(Ns2)
-        Ns3 = good_fft_order(Ns3)
-        Ns = (Ns1,Ns2,Ns3)
-    else
-        Ns = Ns_[:]
+# Naive copy
+function CuGVectorsW( gvecw::GVectorsW )
+    
+    Ngwx = gvecw.Ngwx
+    Ngw = gvecw.Ngw
+
+    kpoints = gvecw.kpoints
+    Nkpt = kpoints.Nkpt
+    
+    TYP = typeof(CuArray([1]))
+    idx_gw2g = Array{TYP,1}(undef,Nkpt)
+    idx_gw2r = Array{TYP,1}(undef,Nkpt)
+    
+    # Copy to GPU
+    for ik in 1:Nkpt
+        idx_gw2g[ik] = CuArray( gvecw.idx_gw2g[ik] )
+        idx_gw2r[ik] = CuArray( gvecw.idx_gw2r[ik] )
     end
 
-    return
+    return CuGVectorsW( Ngwx, Ngw, idx_gw2g, idx_gw2g, kpoints )
 
 end
 
-
-
-function main()
-    CuPWGrid( 15.0, gen_lattice_fcc(10.0) )
-
-    println("Pass here")
+# CuArray version of PWGrid
+struct CuPWGrid
+    ecutwfc::Float64
+    ecutrho::Float64
+    Ns::Tuple{Int64,Int64,Int64}
+    LatVecs::Array{Float64,2}
+    RecVecs::Array{Float64,2}
+    CellVolume::Float64
+    r::Array{Float64,2} # not really used for now
+    gvec::CuGVectors
+    gvecw::CuGVectorsW
+    planfw::FFTW.cFFTWPlan{Complex{Float64},-1,false,3}
+    planbw::AbstractFFTs.ScaledPlan{Complex{Float64},FFTW.cFFTWPlan{Complex{Float64},1,false,3},Float64}
 end
 
-main()
--- sandbox/cuda_01/test_copy_PWGrid.jl
+++ sandbox/cuda_01/test_copy_PWGrid.jl
@@ -1,62 +1,7 @@
 using CuArrays
-
 using PWDFT
 
-struct CuGVectors
-    Ng::Int64
-    G::CuArray{Float64,2}
-    G2::CuArray{Float64,1}
-    idx_g2r::CuArray{Int64,1}
-    G2_shells::CuArray{Float64,1}
-    idx_g2shells::CuArray{Int64,1}
-end
-
-function CuGVectors( gvec::GVectors )
-
-    G = CuArray( gvec.G )
-    G2 = CuArray( gvec.G2 )
-    idx_g2r = CuArray( gvec.idx_g2r )
-    G2_shells = CuArray( gvec.G2_shells )
-    idx_g2shells = CuArray( gvec.idx_g2shells )
-
-    return CuGVectors(gvec.Ng, G, G2, idx_g2r, G2_shells, idx_g2shells)
-end
-
-
-
-
-struct CuGVectorsW
-    Ngwx::Int64
-    Ngw::Array{Int64,1}
-    idx_gw2g::Array{CuArray{Int64,1},1}
-    idx_gw2r::Array{CuArray{Int64,1},1}
-    kpoints::KPoints
-end
-
-"""
-Initialize CuGVectorsW from an instance of `GVectorsW` by direct copy.
-"""
-function CuGVectorsW( gvecw::GVectorsW )
-    
-    Ngwx = gvecw.Ngwx
-    Ngw = gvecw.Ngw
-
-    kpoints = gvecw.kpoints
-    Nkpt = kpoints.Nkpt
-    
-    TYP = typeof(CuArray([1]))
-    idx_gw2g = Array{TYP,1}(undef,Nkpt)
-    idx_gw2r = Array{TYP,1}(undef,Nkpt)
-    
-    # Copy to GPU
-    for ik in 1:Nkpt
-        idx_gw2g[ik] = CuArray( gvecw.idx_gw2g[ik] )
-        idx_gw2r[ik] = CuArray( gvecw.idx_gw2r[ik] )
-    end
-
-    return CuGVectorsW( Ngwx, Ngw, idx_gw2g, idx_gw2g, kpoints )
-
-end
+include("CuPWGrid.jl")
 
 function main()
 
