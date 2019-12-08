148155981bbf4d46b14e123f369fdf25b7cbd72f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-08 17:44:14 +0700

    Using CuArrays.fill insteads of cu zeros

--- sandbox/cuda_01/CuPWGrid.jl
+++ sandbox/cuda_01/CuPWGrid.jl
@@ -110,8 +110,8 @@ function CuPWGrid( ecutwfc::Float64, LatVecs::Array{Float64,2}; kpoints=nothing,
     gvecw_ = PWDFT.init_gvecw( ecutwfc, gvec_, kpoints )
     gvecw = CuGVectorsW( gvecw_ )
 
-    planfw = plan_fft( cu(zeros(ComplexF64,Ns)) )
-    planbw = plan_ifft( cu(zeros(ComplexF64,Ns)) )
+    planfw = plan_fft( CuArrays.fill(0.0 + im*0.0, Ns) )
+    planbw = plan_ifft( CuArrays.fill(0.0 + im*0.0, Ns) )
 
     return CuPWGrid( ecutwfc, ecutrho, Ns, LatVecs, RecVecs, CellVolume, r, gvec, gvecw,
                      planfw, planbw )
