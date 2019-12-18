fab93f2f6d350fc64c39ea4fa1804518f45a66eb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-19 05:33:15 +0700

    Test internals XC

---
+++ sandbox/test_xc_internals/Si_fcc/run.jl
@@ -0,0 +1,52 @@
+using LinearAlgebra
+using Random
+using Printf
+
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+import InteractiveUtils
+InteractiveUtils.versioninfo()
+
+function main( ; method="SCF" )
+
+    Random.seed!(1234)
+
+    # Atoms
+    atoms = Atoms(xyz_string_frac=
+        """
+        2
+
+        Si  0.0  0.0  0.0
+        Si  0.25  0.25  0.25
+        """, in_bohr=true, LatVecs=gen_lattice_fcc(10.2631))
+
+    # Initialize Hamiltonian
+    pspfiles = [joinpath(DIR_PSP, "Si-q4.gth")]
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, meshk=[3,3,3], use_xc_internal=true )
+    println(Ham)
+
+    #
+    # Solve the KS problem
+    #
+    if method == "SCF"
+        KS_solve_SCF!( Ham, mix_method="rpulay", betamix=0.5 )
+
+    elseif method == "Emin"
+        KS_solve_Emin_PCG!( Ham, verbose=true )
+
+    else
+        error( @sprintf("Unknown method %s", method) )
+    end
+    
+end
+
+@time main(method="SCF")
+@time main(method="Emin")
+
+@time main(method="SCF")
+@time main(method="Emin")
