040ea45387f266993f4121d35dea66189f67f9ec
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-20 12:53:31 +0700

    Test update Ham

---
+++ sandbox/cuda_01/test_cu_updateHam.jl
@@ -0,0 +1,29 @@
+include("PWDFT_cuda.jl")
+
+function main()
+    atoms = Atoms(xyz_string=
+    """
+    3
+
+    H   0.0  0.0  0.0
+    H   0.0  1.5  0.0
+    H   1.5  0.0  0.0
+    """, LatVecs=gen_lattice_fcc(10.0))
+
+    pspfiles = ["../../pseudopotentials/pade_gth/H-q1.gth"]
+    ecutwfc = 15.0
+
+    Ham = CuHamiltonian( atoms, pspfiles, ecutwfc )
+
+    psiks = rand_CuBlochWavefunc( Ham )
+    Rhoe = calc_rhoe( Ham, psiks )
+
+    dVol = Ham.pw.CellVolume/prod(Ham.pw.Ns)
+    println("integ Rhoe = ", sum(Rhoe)*dVol)
+
+    update!( Ham, Rhoe )
+
+    println("Pass here")
+end
+
+main()
