f458edc0bb912de6c4b62c6d1a45ab18c46786ea
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-23 15:05:50 +0700

    Test cu_KS_solve_Emin_PCG! for H2

--- sandbox/cuda_01/test_cu_KS_solve_Emin_PCG.jl
+++ sandbox/cuda_01/test_cu_KS_solve_Emin_PCG.jl
@@ -10,20 +10,30 @@ const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
 
 function main()
 
-    Random.seed!(1234)
-
     atoms = Atoms( xyz_file=joinpath(DIR_STRUCTURES, "H2.xyz"),
-                   LatVecs = gen_lattice_sc(16.0) )
+                   LatVecs=gen_lattice_sc(16.0) )
     pspfiles = [joinpath(DIR_PSP, "H-q1.gth")]
-    ecutwfc = 15.0
+    ecutwfc = 50.0
 
     Nspin = 1
 
     Ham = CuHamiltonian( atoms, pspfiles, ecutwfc, use_symmetry=false, Nspin=Nspin )
-
+    
+    Random.seed!(1234)
     KS_solve_Emin_PCG!( Ham, skip_initial_diag=true, startingrhoe=:random )
 
-    println("Pass here")
+    Ham_cpu = Hamiltonian( atoms, pspfiles, ecutwfc, use_symmetry=false, Nspin=Nspin )
+    println( Ham_cpu )
+    
+    Random.seed!(1234)
+    KS_solve_Emin_PCG!( Ham_cpu, skip_initial_diag=true, startingrhoe=:random )
+
+    Random.seed!(1234)
+    @time KS_solve_Emin_PCG!( Ham, skip_initial_diag=true, startingrhoe=:random )
+    
+    Random.seed!(1234)
+    @time KS_solve_Emin_PCG!( Ham_cpu, skip_initial_diag=true, startingrhoe=:random )
+
 end
 
 main()
