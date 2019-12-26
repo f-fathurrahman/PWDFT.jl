5529b0459700146a87b1f3a1331dc76a91bc335f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-26 08:51:50 +0700

    Update text_xc

--- test/test_xc.jl
+++ test/test_xc.jl
@@ -1,7 +1,7 @@
 rho = [0.1, 0.2, 0.3, 0.4, 0.5]
 @testset "LDA_VWN xc" begin
-    @test calc_epsxc_VWN(rho) ≈ [-0.396206, -0.490557, -0.556226, -0.608272, -0.652089] atol=1e-5
-    @test calc_Vxc_VWN(rho) ≈ [-0.51789, -0.64225, -0.728923, -0.797671, -0.85558] atol=1e-5
+    @test calc_epsxc_VWN(LibxcXCCalculator(), rho) ≈ [-0.396206, -0.490557, -0.556226, -0.608272, -0.652089] atol=1e-5
+    @test calc_Vxc_VWN(LibxcXCCalculator(), rho) ≈ [-0.51789, -0.64225, -0.728923, -0.797671, -0.85558] atol=1e-5
 end
 
 # @testset "GGA_PBE xc" begin
