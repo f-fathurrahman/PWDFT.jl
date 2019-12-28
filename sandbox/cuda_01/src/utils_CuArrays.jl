ab99c690204cff1f3ee6d5ba22d51777006e6bef
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-28 12:22:00 +0700

    Rename, don't extend eigen and eigvals

--- sandbox/cuda_01/src/utils_CuArrays.jl
+++ sandbox/cuda_01/src/utils_CuArrays.jl
@@ -1,14 +1,13 @@
 #
 # XXX: Should only works for Hermitian matrix
 #
-import LinearAlgebra: eigen, eigvals
 
-function eigen( d_A::CuArray{ComplexF64,2} )
+function eigen_heevd_gpu( d_A::CuArray{ComplexF64,2} )
     d_A_c = copy(d_A)
     return CUSOLVER.heevd!('V', 'U', d_A_c)
 end
 
-function eigvals( d_A::CuArray{ComplexF64,2} )
+function eigvals_heevd_gpu( d_A::CuArray{ComplexF64,2} )
     d_A_c = copy(d_A)
     return CUSOLVER.heevd!('N', 'U', d_A_c)
 end
