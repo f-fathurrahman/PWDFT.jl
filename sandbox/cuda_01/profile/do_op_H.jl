221c076611e07c045d6e819c844025b9e2539175
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-28 10:45:07 +0700

    Test profiling

---
+++ sandbox/cuda_01/profile/do_op_H.jl
@@ -0,0 +1,37 @@
+using CUDAdrv
+
+using PWDFT
+using PWDFT_cuda
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+
+function main()
+
+    atoms = Atoms(xyz_string=
+    """
+    3
+
+    H   0.0  0.0  0.0
+    H   0.0  1.51  0.0
+    H   1.5  0.0  0.0
+    """, LatVecs=gen_lattice_sc(10.0))
+
+    pspfiles = [joinpath(DIR_PSP, "H-q1.gth")]
+    ecutwfc = 50.0
+
+    Ham = CuHamiltonian( atoms, pspfiles, ecutwfc, use_symmetry=false )
+
+    Npoints = prod(Ham.pw.Ns)
+
+    psiks = rand_CuBlochWavefunc( Ham )
+    Rhoe = calc_rhoe( Ham, psiks )
+
+    update!( Ham, Rhoe )
+    
+    Hpsi = op_H( Ham, psiks[1] )
+
+    println("Pass here")
+end
+
+CUDAdrv.@profile main()
