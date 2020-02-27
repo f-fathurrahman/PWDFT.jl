80cfb10aa3110c5072754b02be4ec522ef6598a9
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-02-28 06:00:45 +0700

    Test GPAW

---
+++ sandbox/GPAW.jl
@@ -0,0 +1,64 @@
+function write_gpaw( Ham::Hamiltonian; filename="main.py",
+                     prefix_dir="./",
+                     use_smearing=false, kT=0.001,
+                     etot_conv_thr=1e-6 )
+
+    atoms = Ham.atoms
+
+    if !Base.Filesystem.isdir(prefix_dir)
+        Base.Filesystem.mkdir(prefix_dir)
+    end
+
+    f = open( joinpath(prefix_dir, filename), "w")
+
+    @printf(f, "from ase import Atoms\n")
+    @printf(f, "from gpaw import GPAW, PW, FermiDirac\n")
+    @printf(f, "from ase.units import Bohr, Hartree\n")
+
+    atoms_name = ""
+    for ia = 1:atoms.Natoms
+        atoms_name = atoms_name * atoms.SpeciesSymbols[atoms.atm2species[ia]]
+    end
+
+    atpos = atoms.positions/ANG2BOHR
+    @printf(f, "atoms = Atoms('%s', positions=[\n", atoms_name)
+    for ia in 1:atoms.Natoms
+        @printf(f, "    [%18.10f, %18.10f, %18.10f],\n", atpos[1,ia], atpos[2,ia], atpos[3,ia])
+    end
+    @printf(f, "    ],\n")
+
+    @printf(f, "    pbc = [True, True, True],\n")
+
+    v = atoms.LatVecs' / ANG2BOHR
+    @printf(f, "    cell=[\n")
+    for i in 1:3
+        @printf(f, "    [%18.10f, %18.10f, %18.10f],\n", v[i,1], v[i,2], v[i,3])
+    end
+    @printf(f, "    ]\n")
+    @printf(f, ")\n")
+
+    @printf(f, "atoms.write('ATOMS.xsf')\n")
+
+    @printf(f, "ecutwfc = %f*Hartree\n", Ham.pw.ecutwfc)
+    @printf(f, "calc = GPAW( mode=PW(ecutwfc),\n")
+    @printf(f, "             setups='hgh',\n")
+    @printf(f, "             xc='LDA_X+LDA_C_VWN',\n")
+    if Ham.electrons.Nspin == 2
+        @printf(f, "             spinpol=True,\n")
+    else
+        @printf(f, "             spinpol=False,\n")
+    end
+    if use_smearing
+        @printf(f, "             occupations=FermiDirac(%f*Hartree),\n", kT)
+    else
+        @printf(f, "              occupations=FermiDirac(0),\n")
+    end
+    meshk = Ham.pw.gvecw.kpoints.mesh
+    @printf(f, "             kpts={'size': (%d, %d, %d), 'gamma':True},\n", meshk[1], meshk[2], meshk[3])
+    @printf(f, "             txt='-')\n")
+    @printf(f, "atoms.set_calculator(calc)\n")
+    @printf(f, "e1 = atoms.get_potential_energy()\n")
+    @printf(f, "print('\\n!!!!!! Total energy      : %%18.10f eV = %%18.10f Hartree\\n' %% (e1,e1/Hartree))\n")
+
+    close(f)
+end
---
+++ sandbox/gpaw_01/test_GPAW.jl
@@ -0,0 +1,86 @@
+using Printf
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)), "..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+include("../GPAW.jl")
+
+function test_H2()
+    # initialize atoms and Hamiltonian
+    atoms = Atoms( xyz_file=joinpath(DIR_STRUCTURES, "H2.xyz"),
+                   LatVecs=gen_lattice_sc(16.0) )
+    pspfiles = [joinpath(DIR_PSP, "H-q1.gth")]
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc )
+
+    run(`rm -rfv TEMP_abinit/\*`)
+    write_abinit( Ham, prefix_dir="./TEMP_abinit/" )
+    cd("./TEMP_abinit")
+    run(pipeline(`abinit`, stdin="FILES", stdout="LOG1"))
+    cd("../")
+
+    energies = read_abinit_etotal("TEMP_abinit/LOG1")
+    println(energies)
+end
+
+
+function test_Al_fcc()
+
+    atoms = Atoms( xyz_string_frac=
+    """
+    1
+
+    Al  0.0  0.0  0.0
+    """, in_bohr=true,
+    LatVecs = gen_lattice_fcc(7.6525970200) )
+    pspfiles = [joinpath(DIR_PSP, "Al-q3.gth")]
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, xcfunc="LDA",
+                       Nspin=1, meshk=[8,8,8], extra_states=4 )
+
+    write_gpaw( Ham, use_smearing=true, kT=0.01, prefix_dir="./" )
+
+    #run(`rm -rfv TEMP_abinit/\*`)
+    #write_abinit( Ham, use_smearing=true, kT=0.01, prefix_dir="./TEMP_abinit/" )
+    #cd("./TEMP_abinit")
+    #run(pipeline(`abinit`, stdin="FILES", stdout="LOG1"))
+    #cd("../")
+#
+#    #energies = read_abinit_etotal("TEMP_abinit/LOG1")
+    #println(energies)
+
+end
+
+
+function test_Pt_fcc()
+    # Atoms
+    atoms = Atoms(xyz_string_frac=
+        """
+        1
+
+        Pt  0.0  0.0  0.0
+        """, LatVecs=gen_lattice_fcc(3.9231*ANG2BOHR))
+
+    # Initialize Hamiltonian
+    pspfiles = [joinpath(DIR_PSP, "Pt-q18.gth")]
+    ecutwfc = 30.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, xcfunc="PBE",
+                        meshk=[3,3,3], extra_states=4 )
+    
+    run(`rm -rfv TEMP_abinit/\*`)
+    write_abinit( Ham, use_smearing=true, kT=0.01, prefix_dir="TEMP_abinit/" )
+    cd("./TEMP_abinit")
+    run(pipeline(`abinit`, stdin="FILES", stdout="LOG1"))
+    cd("../")
+
+    energies = read_abinit_etotal("TEMP_abinit/LOG1")
+    println(energies)
+
+end
+
+#test_CuSO4()
+#test_H2()
+test_Al_fcc()
+#test_Pt_fcc()
