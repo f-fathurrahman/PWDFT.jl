97205193f82f4660b1681e9d0967703864911c15
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-03-12 15:25:58 +0700

    Also do timing for fiind_sym_brav_lattice

--- timing/time_SymmetryInfo.jl
+++ timing/time_SymmetryInfo.jl
@@ -8,10 +8,17 @@ function time_SymmetryInfo()
     DIR_PWINPUT = joinpath(DIR_STRUCTURES, "DATA_DeltaCodes", "PWINPUT")
     
     for fil in ["Si.in", "Pt.in", "N.in", "H.in"]
+
         atoms, _ = read_pwscf_input(joinpath(DIR_PWINPUT, fil))
         @printf("File = %5s, Natoms = %5d : ", fil, atoms.Natoms)
         @btime sym_info = SymmetryInfo($atoms)
+
+        # The result is far better for v.1.4.0-rc2 as compared to v1.3.1
+        @printf("find_symm_bravais_latt: ")
+        @btime Nrots, s, sname = PWDFT.find_symm_bravais_latt( $atoms.LatVecs )
+        println()
     end
 end
 
 time_SymmetryInfo()
+
