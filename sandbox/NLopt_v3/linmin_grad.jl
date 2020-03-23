af754d602aa846d6107b37e731639ebbb48d2099
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-03-23 15:56:32 +0700

    Add abs

--- sandbox/NLopt_v3/linmin_grad.jl
+++ sandbox/NLopt_v3/linmin_grad.jl
@@ -11,32 +11,32 @@ function linmin_grad!( Ham, psiks, g, Kg, d; αt = 3e-5 )
     
     Etrial = calc_energies_grad!( Ham, psic, gt, Kgt ) # Kgt is not really used
     
-    α = zeros(Nkspin)
-    for i in 1:Nkspin
-        denum = real( sum(conj(g[i]-gt[i]) .* d[i]) )
-        if denum != 0.0
-            α[i] = abs( αt*real( sum(conj(g[i]) .* d[i]) )/denum )
-        else
-            α[i] = 0.0
-        end
-        println("α = ", α[i])
-        # Update wavefunction
-        psiks[i] = psiks[i] + α[i]*d[i]
-        ortho_sqrt!( psiks[i] )
-    end
-
-    #denum = dot_BlochWavefunc( g .- gt, d )
-    #if denum != 0.0
-    #    α = αt * dot_BlochWavefunc(g,d)/denum
-    #else
-    #    α = 0.0
-    #end
-    #println("α = ", α)
-    # Update wavefunction
+    #α = zeros(Nkspin)
     #for i in 1:Nkspin
-    #    psiks[i] = psiks[i] + α * d[i]
+    #    denum = real( sum(conj(g[i]-gt[i]) .* d[i]) )
+    #    if denum != 0.0
+    #        α[i] = abs( αt*real( sum(conj(g[i]) .* d[i]) )/denum )
+    #    else
+    #        α[i] = 0.0
+    #    end
+    #    println("α = ", α[i])
+    #    # Update wavefunction
+    #    psiks[i] = psiks[i] + α[i]*d[i]
     #    ortho_sqrt!( psiks[i] )
     #end
+
+    denum = dot_BlochWavefunc( g .- gt, d )
+    if denum != 0.0
+        α = abs( αt * dot_BlochWavefunc(g,d)/denum )
+    else
+        α = 0.0
+    end
+    println("α = ", α)
+    # Update wavefunction
+    for i in 1:Nkspin
+        psiks[i] = psiks[i] + α * d[i]
+        ortho_sqrt!( psiks[i] )
+    end
 
 
     Etot = calc_energies_grad!( Ham, psiks, g, Kg )
