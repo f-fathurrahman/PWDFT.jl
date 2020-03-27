5c7f669a1b22f3398e2157cd97a73cc10860f487
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-03-27 15:10:44 +0700

    Add do_step, orthonormalization is done inside do_step

--- sandbox/NLopt_v3/calc_energies_grad.jl
+++ sandbox/NLopt_v3/calc_energies_grad.jl
@@ -1,12 +1,13 @@
+function ortho_BlochWavefunc!( psiks )
+    for i = 1:length(psiks)
+        ortho_sqrt!(psiks[i])
+    end
+end
+
 """
 Return total energies
 """
-function calc_energies_grad!( Ham, psiks, g, Kg; skip_ortho=false )
-    if !skip_ortho
-        for i = 1:length(psiks)
-            ortho_sqrt!(psiks[i])
-        end
-    end
+function calc_energies_grad!( Ham, psiks, g, Kg )
 
     Rhoe = calc_rhoe( Ham, psiks )
     update!( Ham, Rhoe )
@@ -28,14 +29,9 @@ function calc_energies_grad!( Ham, psiks, g, Kg; skip_ortho=false )
 end
 
 
-function calc_energies_grad_no_modify!( Ham, psiks_orig, g, Kg; skip_ortho=false )
+function calc_energies_grad_no_modify!( Ham, psiks_orig, g, Kg )
     
     psiks = deepcopy(psiks_orig)
-    if !skip_ortho
-        for i = 1:length(psiks)
-            ortho_sqrt!(psiks[i])
-        end
-    end
 
     Rhoe = calc_rhoe( Ham, psiks )
     update!( Ham, Rhoe )
@@ -58,45 +54,46 @@ end
 
 
 # Modify Ham and psiks
-function calc_energies_only!( Ham, psiks; skip_ortho=false )
-    if !skip_ortho
-        for i = 1:length(psiks)
-            ortho_sqrt!(psiks[i])
-        end
-    end
-
+function calc_energies_only!( Ham, psiks )
     Rhoe = calc_rhoe( Ham, psiks )
     update!( Ham, Rhoe )
-    
     Ham.energies = calc_energies( Ham, psiks )
-
     return sum( Ham.energies )
 end
 
 
 # Modify Ham only, psiks is not modified
-function calc_energies_no_modify!( Ham, psiks_orig; skip_ortho=false )
-    
+function calc_energies_no_modify!( Ham, psiks_orig )
     psiks = deepcopy(psiks_orig)
-    if !skip_ortho
-        for i = 1:length(psiks)
-            ortho_sqrt!(psiks[i])
-        end
-    end
-
     Rhoe = calc_rhoe( Ham, psiks )
     update!( Ham, Rhoe )
-    
     Ham.energies = calc_energies( Ham, psiks )
-
     return sum( Ham.energies )
 end
 
+# Wrapper for calc_grad! for BlochWavefunc
+function calc_grad!( Ham::Hamiltonian, psiks::BlochWavefunc, g::BlochWavefunc )
+    #
+    Rhoe = calc_rhoe( Ham, psiks )
+    update!( Ham, Rhoe )
+    #
+    Nspin = Ham.electrons.Nspin
+    Nkpt = Ham.pw.gvecw.kpoints.Nkpt
+    #
+    for ispin in 1:Nspin, ik in 1:Nkpt
+        Ham.ispin = ispin
+        Ham.ik = ik
+        i = ik + (ispin-1)*Nkpt
+        calc_grad!( Ham, psiks[i], g[i] )
+    end
+    return
+end
 
-function calc_grad!( Ham::Hamiltonian, ψ, g )
-    
+function calc_grad!( Ham::Hamiltonian, ψ::Array{ComplexF64,2}, g::Array{ComplexF64,2} )
+
     ik = Ham.ik
     ispin = Ham.ispin
+
     Nstates = size(ψ,2)
     Nkpt = Ham.pw.gvecw.kpoints.Nkpt
     ikspin = ik + (ispin - 1)*Nkpt
@@ -126,6 +123,14 @@ function Kprec!( ik::Int64, pw::PWGrid, psi, Kpsi )
             Gw2 = (G[1,ig] + k[1])^2 + (G[2,ig] + k[2])^2 + (G[3,ig] + k[3])^2
             Kpsi[igk,ist] = psi[igk,ist] / ( 1.0 + Gw2 )
         end
+    end
+    return
+end
+
+function do_step!( psiks, α, d )
+    for i in 1:length(psiks)
+        psiks[i] = psiks[i] + α*d[i]
+        ortho_sqrt!( psiks[i] )
     end
     return
 end
