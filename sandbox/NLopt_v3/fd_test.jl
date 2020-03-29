5b6c0342c69d5b516bf550020cd521ffb0976868
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-03-30 06:27:37 +0700

    Fix fd_test

--- sandbox/NLopt_v3/fd_test.jl
+++ sandbox/NLopt_v3/fd_test.jl
@@ -9,6 +9,7 @@ const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
 const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
 
 include(joinpath(DIR_PWDFT, "sandbox", "KS_solve_SCF_NLsolve.jl"))
+include(joinpath(DIR_PWDFT, "utilities", "PWSCF.jl"))
 
 include("calc_energies_grad.jl")
 include("create_Ham.jl")
@@ -19,9 +20,9 @@ function main()
     #Ham = create_Ham_H2()
     #Ham = create_Ham_H_atom()
     #Ham = create_Ham_Si_fcc()
-    Ham = create_Ham_GaAs()
+    #Ham = create_Ham_GaAs()
     #Ham = create_Ham_NH3()
-    #Ham = create_Ham_ZnO()
+    Ham = create_Ham_ZnO()
 
     psiks = rand_BlochWavefunc( Ham )
 
@@ -37,11 +38,41 @@ function main()
     E0 = calc_energies_grad!( Ham, psiks, g, Kg )
     println("E0 = ", E0)
 
-    psic = zeros_BlochWavefunc( Ham )
-    for Δ in 10.0 .^ range(1,stop=-9,step=-1)
-        psic = deepcopy( psiks )
-        do_step!( psiks, )
+    for i in 1:length(psiks)
+        @printf("i = %d dot(psiks,g) = %18.10e\n", i, 2*real(dot(psiks[i],g[i])))
     end
+
+    d = -Kg
+    #d = rand_BlochWavefunc( Ham )
+    #for i in 1:length(psiks)
+    #    ortho_sqrt!(d[i])
+    #end
+
+    println("Before constrain: ", 2.0*dot(d,d))
+    constrain_search_dir!( d, psiks )
+    println("After constrain: ", 2.0*dot(d,d))
+
+    #for i in 1:length(psiks)
+    #    c = real(dot(d[i],d[i]))
+    #    d[i] = d[i]/sqrt(c)
+    #end
+    println("dot(psiks,psiks) = ", dot(psiks,psiks))
+    println("dot(d,d) = ", dot(d,d))
+
+    psic = zeros_BlochWavefunc( Ham )
+    for α in 10.0 .^ range(1,stop=-10,step=-1)
+        #
+        dE = 2.0*real( dot(g, α*d) )
+        #@printf("α = %e, dE = %18.10e\n", α, dE)
+        #
+        psic = deepcopy( psiks )
+        do_step!( psic, α, d )
+        Etot = calc_energies_grad!( Ham, psic, g, Kg )
+        ratio = (Etot - E0)/dE
+        @printf("α = %e, ratio = %18.10e, diff = %e\n", α, ratio, abs(1-ratio))
+    end
+
+    println("Pass here")
 
 end
 
