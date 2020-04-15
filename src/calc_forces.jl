78b6483ab831d2bc917670d1ba967dc8f064dc39
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-04-15 21:37:30 +0700

    Draft version of calc_forces

---
+++ src/calc_forces.jl
@@ -0,0 +1,15 @@
+function calc_forces(
+    Ham::Hamiltonian,
+    psiks::BlochWavefunc,
+)
+
+    F_NN = calc_forces_NN( Ham.atoms )
+
+    F_Ps_loc = calc_forces_Ps_loc( Ham )
+
+    F_Ps_nloc = calc_forces_Ps_nloc( Ham, psiks )
+    symmetrize_vector!( Ham.pw.LatVecs, Ham.sym_info, F_Ps_nloc )
+    # early return in handled in symmetrize_vector!
+    
+    return F_NN + F_Ps_loc + F_Ps_nloc
+end
