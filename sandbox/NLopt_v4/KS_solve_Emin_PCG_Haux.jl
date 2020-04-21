5c69846d1dfb2d6b6362eacde244b63d064cef4a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-04-22 00:19:01 +0700

    Draft for KS_solve_Emin_PCG_Haux

---
+++ sandbox/NLopt_v4/KS_solve_Emin_PCG_Haux.jl
@@ -0,0 +1,185 @@
+function setup_guess_wavefunc!( Ham, psiks, startingrhoe, skip_initial_diag )
+    
+    Npoints = prod(Ham.pw.Ns)
+    Nspin = Ham.electrons.Nspin
+    Rhoe = zeros(Float64,Npoints,Nspin)
+    
+    if startingrhoe == :gaussian
+        @assert Nspin == 1
+        Rhoe[:,1] = guess_rhoe( Ham )
+    else
+        calc_rhoe!( Ham, psiks, Rhoe )
+    end
+    #
+    update!(Ham, Rhoe)
+    #
+    if !skip_initial_diag
+        _ =
+        diag_LOBPCG!( Ham, psiks, verbose=false, verbose_last=false, NiterMax=10 )
+    end
+    return
+end
+
+function KS_solve_Emin_PCG_Haux!( Ham, psiks;
+    etot_conv_thr=1e-6, skip_initial_diag=false, startingrhoe=:gaussian, NiterMax=5, kT=0.01
+)
+
+    Nkspin = length(psiks)
+
+    g = ElecGradient(Ham)
+    Kg = ElecGradient(Ham)
+    gPrev = ElecGradient(Ham)
+
+    Npoints = prod(Ham.pw.Ns)
+    Nspin = Ham.electrons.Nspin
+    Nstates = Ham.electrons.Nstates
+
+    setup_guess_wavefunc!( Ham, psiks, startingrhoe, skip_initial_diag )
+
+    evars = ElecVars( Ham, psiks )
+
+    # calculate E_NN
+    Ham.energies.NN = calc_E_NN( Ham.atoms )
+
+    # No need to orthonormalize
+    Etot = calc_energies_grad!( Ham, psiks, g, Kg )
+
+    d = deepcopy(Kg)
+
+    # Constrain
+    constrain_search_dir!( d, evars )
+
+    println("Pass here 52")
+    exit()
+
+    gPrevUsed = true
+
+    #minim_params = MinimizeParams()
+
+    #αt_start = minim_params.αt_start
+    #αt_min = minim_params.αt_min
+    updateTestStepSize = minim_params.updateTestStepSize
+    αt = αt_start
+    α = αt
+
+    β = 0.0
+    gKnorm = 0.0
+    gKnormPrev = 0.0
+    force_grad_dir = true
+
+    Etot_old = Etot
+    Nconverges = 0
+    
+    cg_test = 0.0
+    norm_g = 0.0
+
+    for iter in 1:NiterMax
+
+        #println("\nBegin iter = ", iter)
+
+        gKnorm = 2.0*real(dot(g, Kg))
+        
+        if !force_grad_dir
+            #dotgd = dot_BlochWavefunc(g, d)
+            dotgd = 2.0*real( dot(g,d) )
+            if gPrevUsed
+                #dotgPrevKg = dot_BlochWavefunc(gPrev, Kg)
+                dotgPrevKg = 2.0*real(dot(gPrev, Kg))
+            else
+                dotgPrevKg = 0.0
+            end
+            β = (gKnorm - dotgPrevKg)/gKnormPrev # Polak-Ribiere
+            if β < 0.0
+                println("Resetting β")
+                β = 0.0
+            end
+        end
+
+        force_grad_dir = false
+
+        # Check convergence here ....
+
+        # No convergence yet, continuing ...
+        
+        if gPrevUsed
+            gPrev = deepcopy(g)
+        end
+        gKnormPrev = copy(gKnorm)
+
+        # Update search direction
+        for i in 1:Nkspin
+            d_old[i] = copy(d[i])
+            d[i] = -Kg[i] + β*d[i]
+        end
+
+        constrain_search_dir!( d, psiks )
+
+        # Line minimization
+        linmin_success, α, αt = linmin_quad!( Ham, psiks, g, d, α, αt, Etot, minim_params )
+        #println("linmin_success = ", linmin_success)
+        #@printf("α = %18.10e, αt = %18.10e\n", α, αt)
+
+        #linmin_success, α = linmin_armijo!( Ham, psiks, g, d, Etot )
+
+        # Using alternative line minimization
+        #linmin_success, α = linmin_grad!( Ham, psiks, g, d, Etot )
+
+        if linmin_success
+            #
+            do_step!( psiks, α, d )
+            Etot = calc_energies_grad!( Ham, psiks, g, Kg )
+            #
+            if updateTestStepSize
+                αt = α
+                if αt < αt_min    # bad step size
+                    @printf("Bad step size: αt is reset to αt_start: %f\n", αt_start)
+                    αt = αt_start # make sure next test step size is not too bad
+                end
+            end
+        else
+            # linmin failed:
+            do_step!( psiks, -α, d )  # CHECK THIS ...
+            Etot = calc_energies_grad!( Ham, psiks, g, Kg )            
+            
+            @printf("linmin is failed: Update psiks by αt_min = %e, Etot = %18.10f\n", αt_min, Etot)
+
+            if β >= 1e-10   # should be compared with small number
+                # Failed, but not along the gradient direction:
+                @printf("Step failed: resetting search direction.\n")
+                forceGradDirection = true # reset search direction
+            else
+                # Failed along the gradient direction
+                @printf("Step failed along negative gradient direction.\n")
+                @printf("Probably at roundoff error limit. (Should stop)\n")
+                #return
+            end
+        end
+        
+        norm_g = norm(g) #/(Nkspin*Nstates)
+        #norm_g = 2*real(dot(g,g))
+        diffE = Etot_old - Etot
+        @printf("Emin_PCG_new step %8d = %18.10f  %10.7e  %10.7e\n", iter, Etot, diffE, norm_g)
+        if diffE < 0.0
+            println("*** WARNING: Etot is not decreasing")
+        end
+
+        if abs(diffE) < etot_conv_thr
+            Nconverges = Nconverges + 1
+        else
+            Nconverges = 0
+        end
+        
+        if Nconverges >= 2
+            @printf("\nEmin_PCG is converged in iter: %d\n", iter)
+            break
+        end
+
+        Etot_old = Etot
+
+    end
+
+    println("Leaving KS_solve_Emin_PCG_new")
+
+end
+
+
--- sandbox/NLopt_v4/test_emin_smearing.jl
+++ sandbox/NLopt_v4/test_emin_smearing.jl
@@ -11,6 +11,7 @@ include("create_Ham.jl")
 include("ElecVars.jl")
 include("test_ElecVars.jl")
 include("calc_energies_grad.jl")
+include("emin_smearing.jl")
 
 function main()
 
@@ -25,14 +26,38 @@ function main()
 
     #test_ElecVars(Ham)
 
+    Nstates = Ham.electrons.Nstates
+    Nspin = Ham.electrons.Nspin
+    Nkpt = Ham.pw.gvecw.kpoints.Nkpt
+    Nkspin = Nkpt*Nspin
+
     evars = ElecVars(Ham)
     g = ElecGradient(Ham)
     Kg = ElecGradient(Ham)
+
+    rotPrev = Vector{Matrix{ComplexF64}}(undef,Nkspin)
+    rotPrevC = Vector{Matrix{ComplexF64}}(undef,Nkspin)
+    rotPrevCinv = Vector{Matrix{ComplexF64}}(undef,Nkspin)
+    for i in 1:Nkspin
+        rotPrev[i] = diagm( 0 => ones(ComplexF64,Nstates) )
+        rotPrevC[i] = diagm( 0 => ones(ComplexF64,Nstates) )
+        rotPrevCinv[i] = diagm( 0 => ones(ComplexF64,Nstates) )
+    end
+
+
     Ham.energies.NN = calc_E_NN(Ham.atoms)
-    Etot = calc_energies_grad!( Ham, evars, g, Kg, kT )
+    
+    #Etot = calc_energies_grad!( Ham, evars, g, Kg, kT )
+    Etot = compute!( Ham, evars, g, Kg, kT, rotPrevCinv, rotPrev )
 
     println(Ham.energies)
     println("Etot = ", Etot)
+
+    d = deepcopy(Kg)
+
+    # Constrain
+    constrain_search_dir!( d, evars )
+
 
     println("Pass here")
 end
