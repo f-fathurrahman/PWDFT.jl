5eb0949fd3b9c50e6a0f0c210fee983f232d43bc
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-04-28 14:01:18 +0700

    Test Pt_atom without smearing

--- sandbox/NLopt_v4/test_SCF.jl
+++ sandbox/NLopt_v4/test_SCF.jl
@@ -17,11 +17,13 @@ function main()
     #Ham = create_Ham_atom_Si_smearing()
     #Ham = create_Ham_atom_Al_smearing()
     #Ham = create_Ham_Al_fcc_smearing()
-    Ham = create_Ham_atom_Pt_smearing()
+    #Ham = create_Ham_atom_Pt_smearing()
+    Ham = create_Ham_atom_Pt()
     #Ham = create_Ham_Pt_fcc_smearing()
 
-    KS_solve_SCF!( Ham, mix_method="anderson", use_smearing=true, kT=kT)
-    #KS_solve_SCF!( Ham, mix_method="anderson")
+    #KS_solve_SCF!( Ham, mix_method="anderson", use_smearing=true, kT=kT)
+    #KS_solve_SCF!( Ham, mix_method="rpulay", betamix=0.1)
+    KS_solve_Emin_PCG!( Ham)
     print_ebands(Ham.electrons, Ham.pw.gvecw.kpoints)
 end
 
