de751f905e3f33d41037262fc9d1b09c730b40f0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-05-14 21:34:54 +0700

    Reconsidering ortho_GS_gamma

--- sandbox/gamma_only/ortho_GS_gamma.jl
+++ sandbox/gamma_only/ortho_GS_gamma.jl
@@ -13,12 +13,21 @@ function ortho_GS_gamma( vin::Array{ComplexF64,2} )
 end
 
 function ortho_GS_gamma!( v::Array{ComplexF64,2} )
+    println()
+    println("Entering ortho_GS_gamma:")
+    
     Ncol = size(v)[2]
+    
     for ii = 1:Ncol
+        
         zz = 2*dot( v[:,ii], v[:,ii] ) - conj(v[1,ii])*v[1,ii]        
         v[:,ii] = v[:,ii]/sqrt(zz)
+        
+        println("ii = ", ii, " zz = ", zz)
+        
         for jj = ii+1 : Ncol
             puv = prj_gamma( v[:,ii], v[:,jj] )
+            println("puv = ", puv)
             v[:,jj] = v[:,jj] - puv*v[:,ii]
         end
     end
@@ -28,5 +37,5 @@ end
 function prj_gamma( u, v )
     num = 2*dot( u, v ) - conj(u[1])*v[1]
     denum = 2*dot( u, u ) - conj(u[1])*u[1]
-    return  num/denum
+    return real(num/denum)
 end
