1fa07b17a558bd7848a5644492f86b7bfc40d6ae
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-07-27 08:25:41 +0700

    Some simplification in test_operators

--- sandbox/gamma_only/test_operators.jl
+++ sandbox/gamma_only/test_operators.jl
@@ -24,6 +24,31 @@ include("op_H_gamma.jl")
 
 include("unfold_BlochWavefuncGamma.jl")
 
+function compare_potentials(Ham, Ham_)
+
+    println("\nV Ps loc comparison")
+    for ip in 1:5
+        @printf("%3d %18.10f %18.10f\n", ip, Ham.potentials.Ps_loc[ip], Ham_.potentials.Ps_loc[ip])
+    end
+
+    println("\nV Hartree comparison")
+    for ip in 1:5
+        @printf("%3d %18.10f %18.10f\n", ip, Ham.potentials.Hartree[ip], Ham_.potentials.Hartree[ip])
+    end
+
+    println("\nV XC comparison")
+    for ip in 1:5
+        @printf("%3d %18.10f %18.10f\n", ip, Ham.potentials.XC[ip,1], Ham_.potentials.XC[ip,1])
+    end
+
+    println("\nV Total comparison")
+    for ip in 1:5
+        @printf("%3d %18.10f %18.10f\n", ip, Ham.potentials.Total[ip,1], Ham_.potentials.Total[ip,1])
+    end
+
+    return
+end
+
 function test_01()
 
     Random.seed!(1234)
@@ -37,7 +62,7 @@ function test_01()
                 joinpath(DIR_PSP, "H-q1.gth")]
 
     # Initialize Hamiltonian
-    ecutwfc = 15.0
+    ecutwfc = 30.0
     Ham = HamiltonianGamma( atoms, pspfiles, ecutwfc )
 
     Ham_ = Hamiltonian( atoms, pspfiles, ecutwfc )
@@ -95,93 +120,70 @@ function test_01()
     println("Using dot_BlochWavefuncGamma: ", s2)
     println("diff: ", s1-s2, " (should be small)")
 
-    exit()
-
     Rhoe = calc_rhoe(Ham, psis)
     Rhoe_ = calc_rhoe(Ham_, psiks)
 
     update!(Ham, Rhoe)
     update!(Ham_, Rhoe_)
 
-    #
-    # Begin comparison of potentials
-    #
-
-    println("\nV Ps loc comparison")
-    for ip in 1:5
-        @printf("%3d %18.10f %18.10f\n", ip, Ham.potentials.Ps_loc[ip], Ham_.potentials.Ps_loc[ip])
-    end
-
-    println("\nV Hartree comparison")
-    for ip in 1:5
-        @printf("%3d %18.10f %18.10f\n", ip, Ham.potentials.Hartree[ip], Ham_.potentials.Hartree[ip])
-    end
-
-    println("\nV XC comparison")
-    for ip in 1:5
-        @printf("%3d %18.10f %18.10f\n", ip, Ham.potentials.XC[ip,1], Ham_.potentials.XC[ip,1])
-    end
-
-    println("\nV Total comparison")
-    for ip in 1:5
-        @printf("%3d %18.10f %18.10f\n", ip, Ham.potentials.Total[ip,1], Ham_.potentials.Total[ip,1])
-    end
+    #compare_potentials(Ham, Ham_)
 
     # Compare the result of applying operators
 
-    println()
-    Kpsis = op_K(Ham, psis)
-    s1 = sum(Kpsis.data[1])
-    println("sum Kpsis  = ", s1 + conj(s1))
+    println("\nCompare op_K result")
+    #
+    println("Using Gamma-point trick")
+    Kpsis = op_K(Ham, psis); print("time op_K: ")
+    @time Kpsis = op_K(Ham, psis)
+    s1 = dot_BlochWavefuncGamma(psis, Kpsis)
+    #
+    println("Using usual kpt")
+    Kpsiks = op_K(Ham_, psiks); print("time op_K: ")
+    @time Kpsiks = op_K(Ham_, psiks)
+    s2 = dot(psiks, Kpsiks)
+    #
+    println("dot(psis,Kpsis)   = ", s1)
+    println("dot(psiks,Hpsiks) = ", s2)
+    println("diff = ", s1 - s1, " (should be small)")
 
-    Kpsis_ = op_K(Ham_, psiks)
-    s2 = sum(Kpsis_[1])
-    println("sum Kpsis_ = ", s2)
 
-    println()
 
-    V_loc_psis = op_V_loc(Ham, psis)
-    s1 = sum(V_loc_psis.data[1])
-    s1 = s1 + conj(s1)
-    println("sum V_loc_psis  = ", s1)
+    println("\nCompare op_V_loc")
+    #
+    println("Using Gamma-point trick")
+    V_loc_psis = op_V_loc(Ham, psis); print("time op_V_loc: ")
+    @time V_loc_psis = op_V_loc(Ham, psis)
+    s1 = dot_BlochWavefuncGamma(psis, V_loc_psis)
 
-    V_loc_psis_ = op_V_loc(Ham_, psiks)
-    s2 = sum(V_loc_psis_[1])
-    println("sum V_loc_psis_ = ", s2)
+    println("Using usual kpt")
+    V_loc_psiks = op_V_loc(Ham_, psiks); print("time op_V_loc: ")
+    @time V_loc_psiks = op_V_loc(Ham_, psiks)
+    s2 = dot(psiks, V_loc_psiks)
+    
+    println("dot V_loc_psis  = ", s1)
+    println("dot V_loc_psiks = ", s2)
+    println("diff = ", s1 - s1, " (should be small)")
 
-    println("s1 - s2    = ", s1 - s2)
-    ss = 0.0 + 0.0*im
-    for ist in 1:size(psis.data[1],2)
-        ss = ss + V_loc_psis.data[1][1,ist]
-    end
-    println("V_loc_psis = ", ss)
 
-    ist = 1
-    for igw in 1:5
-        c1 = V_loc_psis.data[1][igw,ist]
-        c2 = V_loc_psis_[1][igw,ist]
-        @printf("%3d [%18.10f,%18.10f] [%18.10f,%18.10f]\n", igw, c1.re, c1.im, c2.re, c2.im)
-    end
+    println("\nCompare op_V_Ps_nloc")
+    #
+    println("Using Gamma-point trick")
+    V_psis = op_V_Ps_nloc(Ham, psis); print("time op_V_Ps_nloc: ")
+    @time V_psis = op_V_Ps_nloc(Ham, psis)
+    s1 = dot_BlochWavefuncGamma(psis, V_psis)
 
-    println("dot V_loc_psis")
-    println("Using dot (gamma)           : ", dot(V_loc_psis,V_loc_psis))
-    println("Using dot (usual)           : ", dot(V_loc_psis_,V_loc_psis_))
-    println("Using dot_BlochWavefuncGamma: ", dot_BlochWavefuncGamma(V_loc_psis,V_loc_psis))
+    println("Using usual kpt")
+    V_psiks = op_V_Ps_nloc(Ham_, psiks); print("time op_V_Ps_nloc: ")
+    @time V_psiks = op_V_Ps_nloc(Ham_, psiks)
+    s2 = dot(psiks, V_psiks)
+    
+    println("dot V_Ps_nloc_psis  = ", s1)
+    println("dot V_Ps_nloc_psiks = ", s2)
+    println("diff = ", s1 - s1, " (should be small)")
 
-    V_Ps_nloc_psis = op_V_Ps_nloc(Ham, psis)
-    V_Ps_nloc_psiks = op_V_Ps_nloc(Ham_, psiks)
 
-    println("dot V_Ps_nloc_psis")
-    println("Using dot (gamma)           : ", dot(V_Ps_nloc_psis,V_Ps_nloc_psis) )
-    println("Using dot (usual)           : ", dot(V_Ps_nloc_psiks,V_Ps_nloc_psiks) )
-    println("Using dot_BlochWavefuncGamma: ", dot_BlochWavefuncGamma(V_Ps_nloc_psis,V_Ps_nloc_psis) )
+    exit()
 
-    ist = 1
-    for igw in 1:5
-        c1 = V_Ps_nloc_psis.data[1][igw,ist]
-        c2 = V_Ps_nloc_psiks[1][igw,ist]
-        @printf("%3d [%18.10f,%18.10f] [%18.10f,%18.10f]\n", igw, c1.re, c1.im, c2.re, c2.im)
-    end
 
     Hpsis = op_H(Ham, psis)
     Hpsiks = op_H(Ham_, psiks)
