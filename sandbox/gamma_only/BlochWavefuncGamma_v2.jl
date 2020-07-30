9b2e7cfe0213e64fa2e66b5c986bf3440221c4ca
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-07-30 08:05:36 +0700

    Implement more methods

--- sandbox/gamma_only/BlochWavefuncGamma_v2.jl
+++ sandbox/gamma_only/BlochWavefuncGamma_v2.jl
@@ -29,20 +29,6 @@ function dot_BlochWavefuncGamma( v1::BlochWavefuncGamma, v2::BlochWavefuncGamma 
     return res
 end
 
-# The same as version 1
-function dot_BlochWavefuncGamma( v1::Array{ComplexF64,2}, v2::Array{ComplexF64,2} )
-    c = dot(v1, v2)
-    s = c + conj(c)
-    Nstates = size(v1,2)
-    ds = 0.0 + im*0.0
-    for ist in 1:Nstates
-        ds = ds + conj(v1[1,ist]) * v2[1,ist]
-    end
-    return s - ds
-
-end
-
-
 import Base: length
 function length(v::BlochWavefuncGamma)
     return length(v.data)
@@ -64,18 +50,19 @@ end
 
 
 
-#function zeros_BlochWavefuncGamma( Ham::HamiltonianGamma )
-#    return zeros_BlochWavefuncGamma(Ham.pw.gvecw.Ngw, Ham.electrons.Nstates, Nspin=Ham.electrons.Nspin)
-#end
+function zeros_BlochWavefuncGamma( Ham::HamiltonianGamma )
+    return zeros_BlochWavefuncGamma(Ham.pw.gvecw.Ngw, Ham.electrons.Nstates, Nspin=Ham.electrons.Nspin)
+end
 
-#function zeros_BlochWavefuncGamma( Nbasis::Int64, Nstates::Int64; Nspin=1 )
-#    data = Vector{Array{ComplexF64,2}}(undef,Nspin)
-#    for ispin in 1:Nspin
-#        data[ispin] = zeros(ComplexF64,Nbasis,Nstates)
-#    end
-#    return BlochWavefuncGamma(data)
-#end
-
+function zeros_BlochWavefuncGamma( Nbasis::Int64, Nstates::Int64; Nspin=1 )
+    dc = Vector{Vector{Float64}}(undef,Nspin)
+    data = Vector{Array{ComplexF64,2}}(undef,Nspin)
+    for i in 1:Nspin
+        dc[i] = zeros(Float64,Nstates)
+        data[i] = zeros(ComplexF64,Nbasis,Nstates)
+    end
+    return BlochWavefuncGamma(dc, data)
+end
 
 
 # random BlochWavefuncGamma from randn
@@ -85,77 +72,19 @@ function randn_BlochWavefuncGamma( Ham::HamiltonianGamma )
 end
 
 function randn_BlochWavefuncGamma( Nbasis::Int64, Nstates::Int64; Nspin=1 )
-    #
+    dc = Vector{Vector{Float64}}(undef,Nspin)
     data = Vector{Array{ComplexF64,2}}(undef,Nspin)
     for ispin in 1:Nspin
         data[ispin] = randn(ComplexF64,Nbasis,Nstates)
-        # Set DC component (ig=1) to real number
         for ist in 1:Nstates
-            data[ispin][1,ist] = data[ispin][1,ist] + conj(data[ispin][1,ist])
+            dc[ispin][ist] = real(data[ispin][1,ist])
+            data[ispin][1,ist] = 0.0 + im*0.0
         end
-        #ortho_GS_gamma!(data[ispin])
-        ortho_sqrt_gamma!(data[ispin])
+        ortho_sqrt_gamma!(dc[ispin], data[ispin])
     end
-
-    return BlochWavefuncGamma(data)
+    return BlochWavefuncGamma(dc, data)
 end
 
-import PWDFT: ortho_gram_schmidt!
-function ortho_gram_schmidt!( psis::BlochWavefuncGamma )
-    Nspin = length(psis)
-    for ispin in 1:Nspin
-        ortho_GS_gamma!( psis.data[ispin] )
-    end
-    return
-end
-
-function ortho_check( psis::BlochWavefuncGamma )
-    Nspin = length(psis)
-    for ispin in 1:Nspin
-        ortho_check_gamma( psis.data[ispin] )
-    end
-    return
-end
-
-function ortho_check_gamma( psis::BlochWavefuncGamma )
-    Nspin = length(psis)
-    for ispin in 1:Nspin
-        ortho_check_gamma( psis.data[ispin] )
-    end
-    return
-end
-
-function ortho_check_gamma( psi::Array{ComplexF64,2} )
-    Nstates = size(psi)[2]
-    @printf("\nNorm check:\n")
-    for ist = 1:Nstates
-        c = dot( psi[:,ist], psi[:,ist] )
-        c = c + conj(c) - conj(psi[1,ist])*psi[1,ist]
-        @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
-    end
-    @printf("\nOrtho check w.r.t state #1:\n")
-    for ist = 2:Nstates
-        c = dot( psi[:,ist], psi[:,1] )
-        c = c + conj(c) - conj(psi[1,ist])*psi[1,1]
-        @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
-    end
-    @printf("\n")
-    return
-end
-
-# For calculating psi2' * psi1
-# order of argument is importatn
-function overlap_gamma( psi1::Array{ComplexF64,2}, psi2::Array{ComplexF64,2} )
-    C = psi2' * psi1
-    Nstates = size(psi1,2)
-    v1g = zeros(ComplexF64,Nstates)
-    v2g = zeros(ComplexF64,Nstates)
-    for ist in 1:Nstates
-        v1g[ist] = psi2[1,ist] # psi is the array that is conj transposed in the orig expression
-        v2g[ist] = psi1[1,ist]  # v2g is the array that will be conj transposed
-    end
-    return C + conj(C) - v1g*v2g'
-end
 
 function overlap_gamma(
     dc1::Vector{Float64}, psi1::Array{ComplexF64,2},
@@ -166,19 +95,58 @@ function overlap_gamma(
 end
 
 function overlap_gamma( i::Int64, psis1::BlochWavefuncGamma, psis2::BlochWavefuncGamma )
-    return overlap_gamma(psis1.dc[i], psis1.data[i],
-                         psis2.dc[i], psis2.data[i])
+    return overlap_gamma( psis1.dc[i], psis1.data[i],
+                          psis2.dc[i], psis2.data[i] )
 end
 
 function ortho_sqrt_gamma!( psis::BlochWavefuncGamma )
-    Nspin = size(psis,1)
-    Nstates = size(psis.data,2)
-    C = zeros(ComplexF64,Nstates,Nstates)
+    Nspin = size(psis.data)
     for i in 1:Nspin
-        C = overlap_gamma(psis.data[i], psis.data[i])
-        psis.data[i][:,:] = psis.data[i][:,:]*inv(sqrt(C))
+        ortho_sqrt_gamma(psis.dc[i], psis.data[i])
     end
     return
+end
+
+function ortho_sqrt_gamma!( dc::Vector{Floa64}, psi::Array{ComplexF64,2} )
+    Udagger = inv(sqrt(overlap_gamma(psi, psi)))
+    psi[:,:] = psi[:,:]*Udagger
+    dc[:] = Udagger*dc[:]
+    return
+end
+
+# FIXME:
+# remove _gamma postfix for ortho_sqrt, differentiated by number of arguments
+# and arguments' type.
+
+
+#
+# Using raw array: dc components are the first element of each columns
+#
+
+#
+function dot_BlochWavefuncGamma( v1::Array{ComplexF64,2}, v2::Array{ComplexF64,2} )
+    c = dot(v1, v2)
+    s = c + conj(c)
+    Nstates = size(v1,2)
+    ds = 0.0 + im*0.0
+    for ist in 1:Nstates
+        ds = ds + conj(v1[1,ist]) * v2[1,ist]
+    end
+    return s - ds
+end
+
+# For calculating psi2' * psi1
+# order of argument is importatn
+function overlap_gamma( psi1::Array{ComplexF64,2}, psi2::Array{ComplexF64,2} )
+    C = psi2' * psi1
+    Nstates = size(psi1,2)
+    v1g = zeros(ComplexF64,Nstates)
+    v2g = zeros(ComplexF64,Nstates)
+    for ist in 1:Nstates
+        v1g[ist] = psi2[1,ist]  # v1g is the array that is conj transposed in the orig expression
+        v2g[ist] = psi1[1,ist]  # v2g is the array that will be conj transposed
+    end
+    return C + conj(C) - v1g*v2g'
 end
 
 function ortho_sqrt_gamma!( psi::Array{ComplexF64,2} )
@@ -187,6 +155,7 @@ function ortho_sqrt_gamma!( psi::Array{ComplexF64,2} )
     return
 end
 
+# Return new array
 function ortho_sqrt_gamma( psi::Array{ComplexF64,2} )
     C = overlap_gamma(psi, psi)
     return psi[:,:]*inv(sqrt(C))
