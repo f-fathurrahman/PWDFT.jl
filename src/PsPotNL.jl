7a77c928aed5d0ac0facd0faebbee67374552192
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-07-13 14:39:43 +0700

    Start refactor PsPotNL

--- src/PsPotNL.jl
+++ src/PsPotNL.jl
@@ -11,41 +11,14 @@ function PsPotNL()
     return PsPotNL(0, zeros(Int64,1,1,1,1), betaNL )
 end
 
-#function PsPotNL( atoms::Atoms, pw::PWGrid, Pspots::Array{PsPot_GTH,1}; check_norm=false )
-function PsPotNL( atoms::Atoms, pw::PWGrid, Pspots; check_norm=false )
+function PsPotNL( atoms::Atoms, pw::PWGrid, pspots::Array{PsPot_GTH,1}; check_norm=false )
+
     Natoms = atoms.Natoms
     atm2species = atoms.atm2species
     atpos = atoms.positions
-
     kpoints = pw.gvecw.kpoints
 
-    # 4: indexed from 0:3
-    # 0, 1, 2, 3  -> l indexed
-    # 1, 2, 3, 4  -> l + 1
-
-    # -3:3
-    # -3, -2, -1, 0, 1, 2, 3  -> m
-    #  1,  2,  3, 4, 5, 6, 7  -> 4 + m, lmax = 3 + 1
-
-    # -2, -1, 0, 1, 2  -> m
-    #  1,  2, 3, 4, 5  -> 3 + m, lmax = 2 + 1
-
-    prj2beta = Array{Int64}(undef,3,Natoms,4,7)
-    prj2beta[:,:,:,:] .= -1   # set to invalid index
-
-    NbetaNL = 0
-    for ia = 1:Natoms
-        isp = atm2species[ia]
-        psp = Pspots[isp]
-        for l = 0:psp.lmax
-            for m = -l:l
-                for iprj = 1:psp.Nproj_l[l+1]
-                    NbetaNL = NbetaNL + 1
-                    prj2beta[iprj,ia,l+1,m+psp.lmax+1] = NbetaNL
-                end
-            end
-        end
-    end
+    NbetaNL, prj2beta = _init_prj2beta(Natoms, atm2species, pspots)
 
     # No nonlocal components
     if NbetaNL == 0
@@ -66,29 +39,27 @@ function PsPotNL( atoms::Atoms, pw::PWGrid, Pspots; check_norm=false )
     G = pw.gvec.G
     g = zeros(3)
 
-    for ik = 1:Nkpt
+    for ik in 1:Nkpt
         ibeta = 0
-        for ia = 1:Natoms
+        for ia in 1:Natoms
             isp = atm2species[ia]
-            psp = Pspots[isp]
-            for l = 0:psp.lmax
-            for m = -l:l
-            for iprj = 1:psp.Nproj_l[l+1]
-                ibeta = ibeta + 1
-                idx_gw2g = pw.gvecw.idx_gw2g[ik]
-                for igk = 1:Ngw[ik]
-                    ig = idx_gw2g[igk]
-                    g[1] = G[1,ig] + k[1,ik]
-                    g[2] = G[2,ig] + k[2,ik]
-                    g[3] = G[3,ig] + k[3,ik]
-                    Gm = norm(g)
-                    GX = atpos[1,ia]*g[1] + atpos[2,ia]*g[2] + atpos[3,ia]*g[3]
-                    Sf = cos(GX) - im*sin(GX)
-                    betaNL[ik][igk,ibeta] =
-                    (-1.0*im)^l * Ylm_real(l,m,g)*eval_proj_G(psp,l,iprj,Gm,pw.CellVolume)*Sf
+            psp = pspots[isp]
+            for l in 0:psp.lmax, m in -l:l
+                for iprj in 1:psp.Nproj_l[l+1]
+                    ibeta = ibeta + 1
+                    idx_gw2g = pw.gvecw.idx_gw2g[ik]
+                    for igk in 1:Ngw[ik]
+                        ig = idx_gw2g[igk]
+                        g[1] = G[1,ig] + k[1,ik]
+                        g[2] = G[2,ig] + k[2,ik]
+                        g[3] = G[3,ig] + k[3,ik]
+                        Gm = norm(g)
+                        GX = atpos[1,ia]*g[1] + atpos[2,ia]*g[2] + atpos[3,ia]*g[3]
+                        Sf = cos(GX) - im*sin(GX)
+                        betaNL[ik][igk,ibeta] =
+                        (-1.0*im)^l * Ylm_real(l,m,g)*eval_proj_G(psp,l,iprj,Gm,pw.CellVolume)*Sf
+                    end
                 end
-            end
-            end
             end
         end
     end  # kpoints
@@ -100,6 +71,37 @@ function PsPotNL( atoms::Atoms, pw::PWGrid, Pspots; check_norm=false )
     return PsPotNL( NbetaNL, prj2beta, betaNL )
 
 end
+
+
+function _init_prj2beta(Natoms::Int64, atm2species, pspots)
+    # 4: indexed from 0:3
+    # 0, 1, 2, 3  -> l indexed
+    # 1, 2, 3, 4  -> l + 1
+
+    # -3:3
+    # -3, -2, -1, 0, 1, 2, 3  -> m
+    #  1,  2,  3, 4, 5, 6, 7  -> 4 + m, lmax = 3 + 1
+
+    # -2, -1, 0, 1, 2  -> m
+    #  1,  2, 3, 4, 5  -> 3 + m, lmax = 2 + 1
+
+    prj2beta = Array{Int64}(undef,3,Natoms,4,7)
+    prj2beta[:,:,:,:] .= -1   # set to invalid index
+
+    NbetaNL = 0
+    for ia in 1:Natoms
+        isp = atm2species[ia]
+        psp = pspots[isp]
+        for l in 0:psp.lmax, m in -l:l
+            for iprj in 1:psp.Nproj_l[l+1]
+                NbetaNL = NbetaNL + 1
+                prj2beta[iprj,ia,l+1,m+psp.lmax+1] = NbetaNL
+            end
+        end
+    end
+    return NbetaNL, prj2beta
+end
+
 
 
 function check_betaNL_norm(
