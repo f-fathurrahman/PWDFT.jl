517a9dd135e42d849edf9dfb24a404fd22587819
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-12-07 17:03:32 +0700

    Simplify Hamiltonian construction. Use default FFT grid. @time the execution of main function.

--- examples/myscf_01/run.jl
+++ examples/myscf_01/run.jl
@@ -22,22 +22,26 @@ function init_Ham_Si_fcc()
     #pspfiles = [joinpath(DIR_PSP, "Si-q4.gth")]
     pspfiles = [ "Si.upf" ]
     ecutwfc = 15.0
-    #return Hamiltonian( atoms, pspfiles, ecutwfc,
-    #    meshk=[3,3,3], Ns_=(32,32,32) )
-    return Hamiltonian( atoms, pspfiles, ecutwfc,
-        meshk=[3,3,3] )
+    return Hamiltonian( atoms, pspfiles, ecutwfc, meshk=[3,3,3] )
 end
 
 function main()
+    
     Random.seed!(1234)
-    Ham = init_Ham_Si_fcc()
-    #Ham = init_Ham_G2mols("H2O")
-    println(Ham)
+    
+    #Ham = init_Ham_Si_fcc()
+    Ham = init_Ham_G2mols("H2O")
+    #println(Ham)
+    
     psiks = rand_BlochWavefunc(Ham)
+
     my_scf!(Ham, psiks, betamix=0.5)
-    #KS_solve_SCF!(Ham, psiks, mix_method="broyden")
+
     #KS_solve_Emin_PCG!(Ham, psiks)
+
+    #KS_solve_SCF!(Ham, psiks, mix_method="broyden")
 end
 
-main()
+@time main()
+@time main()
 
