ac5d7748601450147d2fa9433d903eca16992b97
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-07-13 13:02:04 +0700

    Incorporate PWGridDual to PWGrid

--- src/PWGrid.jl
+++ src/PWGrid.jl
@@ -28,16 +28,21 @@ const PLANBW_TYPE = typeof(plan_ifft!(zeros(ComplexF64,(1,1,1))))
 The type for describing plane wave basis set for a given periodic unit cell.
 """
 struct PWGrid
+    using_dual_grid::Bool
     ecutwfc::Float64
     ecutrho::Float64
     Ns::Tuple{Int64,Int64,Int64}
+    Nss::Union{Tuple{Int64,Int64,Int64},Nothing}
     LatVecs::Array{Float64,2}
     RecVecs::Array{Float64,2}
     CellVolume::Float64
     gvec::GVectors
+    gvecs::Union{GVectors,Nothing}
     gvecw::GVectorsW
     planfw::PLANFW_TYPE
     planbw::PLANBW_TYPE
+    planfws::Union{PLANFW_TYPE,Nothing}
+    planbws::Union{PLANBW_TYPE,Nothing}
 end
 
 # Adding type annotation to planfw and planbw can reduce number of allocations
@@ -60,10 +65,10 @@ function PWGrid(
 
     @assert dual >= 4.0
 
-    ecutrho = dual*ecutwfc
-    #
-    RecVecs = 2*pi*inv(Matrix(LatVecs'))
+    using_dual_grid = dual > 4.0
 
+    ecutrho = dual*ecutwfc
+    RecVecs = 2*pi*inv(Matrix(LatVecs'))
     CellVolume = abs(det(LatVecs))
     #
     LatVecsLen = Array{Float64}(undef,3)
@@ -87,6 +92,44 @@ function PWGrid(
     Npoints = prod(Ns)
     
     gvec = init_gvec( Ns, RecVecs, ecutrho )
+    planfw = plan_fft!( zeros(ComplexF64,Ns) )
+    planbw = plan_ifft!( zeros(ComplexF64,Ns) )
+
+
+    # If dual is larger than 4 then we need to allocate a smooth grid:
+    # gvecs and its grid size, and the FFR plans associated with it
+    #
+    # "Smooth" here means that the functions described by the grid are assumed
+    # to be smooth enough such that 4*ecutwfc is enough.
+    #
+    # This is to be contrasted with "dense" grid.
+    #
+    # Probably it is better to name is a "coarse" grid.
+    #
+    # The usual gvec will be treated as "dense" grid.
+    if using_dual_grid
+        # Use 4*ecutwfc as the cutoff
+        Ns1 = 2*round( Int64, sqrt(4*ecutwfc/2)*LatVecsLen[1]/pi ) + 1
+        Ns2 = 2*round( Int64, sqrt(4*ecutwfc/2)*LatVecsLen[2]/pi ) + 1
+        Ns3 = 2*round( Int64, sqrt(4*ecutwfc/2)*LatVecsLen[3]/pi ) + 1
+        Ns1 = good_fft_order(Ns1)
+        Ns2 = good_fft_order(Ns2)
+        Ns3 = good_fft_order(Ns3)
+        Nss = (Ns1,Ns2,Ns3)
+        # XXX Setting Nss from the constructor is not yet supported
+        #
+        # TODO: simply copy from gvec instead of calling this function again
+        #       Need to calculate Ngs (no. of G-vectors for smooth grid)
+        #       gvecs will be a subset of gvec
+        gvecs = init_gvec( Ns, RecVecs, 4*ecutwfc )
+        planfws = plan_fft!( zeros(ComplexF64,Nss) )
+        planbws = plan_ifft!( zeros(ComplexF64,Nss) )
+    else
+        Nss = nothing
+        gvecs = nothing
+        planfws = nothing
+        planbws = nothing
+    end
 
     if kpoints == nothing
         kpoints = KPoints( 1, (1,1,1), zeros(3,1), [1.0], RecVecs )
@@ -94,11 +137,11 @@ function PWGrid(
 
     gvecw = init_gvecw( ecutwfc, gvec, kpoints )
 
-    planfw = plan_fft!( zeros(ComplexF64,Ns) )
-    planbw = plan_ifft!( zeros(ComplexF64,Ns) )
-
-    return PWGrid( ecutwfc, ecutrho, Ns, LatVecs, RecVecs, CellVolume, gvec, gvecw,
-                   planfw, planbw )
+    return PWGrid(
+        using_dual_grid,
+        ecutwfc, ecutrho, Ns, Nss, LatVecs, RecVecs, CellVolume,
+        gvec, gvecs, gvecw, planfw, planbw, planfws, planbws
+    )
 end
 
 """
