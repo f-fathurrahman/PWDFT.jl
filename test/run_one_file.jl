5f6421ed169af5fc73f032a17f7bc60381393612
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-01-29 12:22:09 +0700

    Add script to run one test

---
+++ test/run_one_file.jl
@@ -0,0 +1,12 @@
+using PWDFT
+using Random
+using LinearAlgebra
+using Test
+
+const DIR_PWDFT = joinpath( dirname(pathof(PWDFT)), "..")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+const DIR_PSP_GTH_LDA = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+
+@assert length(ARGS) == 1
+
+include(ARGS[1])
