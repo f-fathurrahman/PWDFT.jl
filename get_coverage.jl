3fc9f0f88e2c9683aae31246e0e74fd2e53ef96d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-01-29 21:49:01 +0700

    Add script to get coverage, enable test H_atom

---
+++ get_coverage.jl
@@ -0,0 +1,24 @@
+# From: https://github.com/JuliaCI/Coverage.jl
+
+using Coverage
+using Printf
+
+# process '*.cov' files
+coverage = process_folder() # defaults to src/; alternatively, supply the folder name as argument
+#coverage = append!(coverage, process_folder("deps"))  # useful if you want to analyze more than just src/
+
+# process '*.info' files, if you collected them
+coverage = merge_coverage_counts(coverage, filter!(
+    let prefixes = ( joinpath(pwd(), "src", ""),
+                    joinpath(pwd(), "deps", "") )
+        c -> any(p -> startswith(c.filename, p), prefixes)
+    end,
+    LCOV.readfolder("test")))
+
+# Get total coverage for all Julia files
+covered_lines, total_lines = get_summary(coverage)
+
+println()
+@printf("covered_lines = %d\n", covered_lines)
+@printf("total_lines   = %d\n", total_lines)
+@printf("Percentage    = %.1f%%\n", covered_lines/total_lines * 100)
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using Test
 
 const DIR_PWDFT = joinpath( dirname(pathof(PWDFT)), "..")
 const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+const DIR_PSP_GTH_LDA = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
 
 include("test_gen_lattice.jl")
 
@@ -17,7 +18,7 @@ include("test_xc.jl")
 include("test_Electrons.jl")
 
 #include("test_spglib.jl")
-#include("H_atom.jl")
+include("H_atom.jl")
 #include("H_atom_PBE.jl")
 #include("Si_fcc.jl")
 #include("Si_fcc_PBE.jl")
