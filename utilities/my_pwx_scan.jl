b9e7de982f665b2a7177bd17eccfc6f6593ab302
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-03-09 12:05:17 +0700

    Special case for SCAN

---
+++ utilities/my_pwx_scan.jl
@@ -0,0 +1,35 @@
+using Printf
+using OffsetArrays
+using LinearAlgebra
+
+using Random
+Random.seed!(1234)
+
+using PWDFT
+
+include("PWSCFInput.jl")
+include("init_Ham_from_pwinput.jl")
+
+function test_main()
+    Ham, pwinput = init_Ham_from_pwinput()
+
+    write_xsf("ATOMS_from_pwinput.xsf", Ham.atoms)
+    println(Ham)
+
+    # XXX: This should take into account the overlap operator
+    # XXX: Currently this is handled in _prepare_scf (called electrons_scf)
+    psiks = rand_BlochWavefunc(Ham)
+
+    use_smearing = false
+    kT = 0.0
+    if pwinput.occupations == "smearing"
+        use_smearing = true
+        kT = pwinput.degauss*0.5 # convert from Ry to Ha
+    end
+
+    #electrons_scf!(Ham, psiks, NiterMax=100, use_smearing=use_smearing, kT=kT)
+    #KS_solve_SCF!(Ham, psiks, NiterMax=100, use_smearing=use_smearing, kT=kT)
+    KS_solve_Emin_PCG!(Ham, psiks, NiterMax=100)
+end
+
+test_main()
