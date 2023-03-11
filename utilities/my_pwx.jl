99c93b8d50a613ff417b0e78b83906ad95693485
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-03-11 16:55:15 +0700

    Using KS_solve_PCG instead of electrons_scf

--- utilities/my_pwx.jl
+++ utilities/my_pwx.jl
@@ -27,7 +27,37 @@ function test_main()
         kT = pwinput.degauss*0.5 # convert from Ry to Ha
     end
 
-    electrons_scf!(Ham, psiks, NiterMax=100, use_smearing=use_smearing, kT=kT)
+    #electrons_scf!(Ham, psiks, NiterMax=100, use_smearing=use_smearing, kT=kT)
+
+    # Not yet working for smearing
+    KS_solve_Emin_PCG!(Ham, psiks, NiterMax=100)
+    energies = Ham.energies
+    println()
+    println(">>>> Final result:")
+    println()
+    
+    println("-------------------------------------")
+    println("Energy components in Ry")
+    println("-------------------------------------")
+    
+    @printf("Kinetic    energy: %18.10f Ry\n", 2*energies.Kinetic )
+    @printf("Ps_loc     energy: %18.10f Ry\n", 2*energies.Ps_loc )
+    @printf("Ps_nloc    energy: %18.10f Ry\n", 2*energies.Ps_nloc )
+    @printf("Hartree    energy: %18.10f Ry\n", 2*energies.Hartree )
+    @printf("XC         energy: %18.10f Ry\n", 2*energies.XC )
+    @printf("-TS              : %18.10f Ry\n", 2*energies.mTS)
+    @printf("-------------------------------------\n")
+    
+    E_elec = energies.Kinetic + energies.Ps_loc + energies.Ps_nloc +
+             energies.Hartree + energies.XC + energies.mTS
+    
+    @printf("Electronic energy: %18.10f Ry\n", 2*E_elec)
+    @printf("NN         energy: %18.10f Ry\n", 2*energies.NN )
+    @printf("-------------------------------------\n")
+
+    E_total = E_elec + energies.NN
+    @printf("! Total = %18.10f Ry\n", 2*E_total)
+
 end
 
 test_main()
