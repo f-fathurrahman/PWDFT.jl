639c730a41f87ffd7b9224f909f672f38bc5a3c7
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-03-11 16:55:53 +0700

    Display total energy components in Ry

--- utilities/my_pwx_scan.jl
+++ utilities/my_pwx_scan.jl
@@ -30,6 +30,34 @@ function test_main()
     #electrons_scf!(Ham, psiks, NiterMax=100, use_smearing=use_smearing, kT=kT)
     #KS_solve_SCF!(Ham, psiks, NiterMax=100, use_smearing=use_smearing, kT=kT)
     KS_solve_Emin_PCG!(Ham, psiks, NiterMax=100)
+
+    energies = Ham.energies
+    println()
+    println(">>>> Final result:")
+    println()
+    
+    println("-------------------------------------")
+    println("Energy components in Ry")
+    println("-------------------------------------")
+    
+    @printf("Kinetic    energy: %18.10f Ry\n", 2*energies.Kinetic )
+    @printf("Ps_loc     energy: %18.10f Ry\n", 2*energies.Ps_loc )
+    @printf("Ps_nloc    energy: %18.10f Ry\n", 2*energies.Ps_nloc )
+    @printf("Hartree    energy: %18.10f Ry\n", 2*energies.Hartree )
+    @printf("XC         energy: %18.10f Ry\n", 2*energies.XC )
+    @printf("-TS              : %18.10f Ry\n", 2*energies.mTS)
+    @printf("-------------------------------------\n")
+    
+    E_elec = energies.Kinetic + energies.Ps_loc + energies.Ps_nloc +
+             energies.Hartree + energies.XC + energies.mTS
+    
+    @printf("Electronic energy: %18.10f Ry\n", 2*E_elec)
+    @printf("NN         energy: %18.10f Ry\n", 2*energies.NN )
+    @printf("-------------------------------------\n")
+
+    E_total = E_elec + energies.NN
+    @printf("! Total = %18.10f Ry\n", 2*E_total)
+
 end
 
 test_main()
