f7da1d8bec3cc2907e3a7ec18e4859e4b658f05a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-06-16 11:02:35 +0700

    H2 using GBRV and PAW from PSLIB

---
+++ examples/H2/run_paw_pslib.jl
@@ -0,0 +1,41 @@
+using LinearAlgebra
+using Random
+using Printf
+
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "PSLIB_US_PAW_LDA")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+import InteractiveUtils
+InteractiveUtils.versioninfo()
+import Dates
+println("Now = ", Dates.now())
+
+function main( ; method="SCF" )
+
+    Random.seed!(1234)
+
+    # Atoms
+    atoms = Atoms( xyz_file=joinpath(DIR_STRUCTURES, "H2.xyz"),
+                   LatVecs = gen_lattice_sc(16.0) )
+
+    # Initialize Hamiltonian
+    pspfiles = [joinpath(DIR_PSP, "H.pz-kjpaw_psl.1.0.0.UPF")]
+    ecutwfc = 20.0 # or 40 Ry
+    ecutrho = 100.0 # or 200 Ry
+    dual = ecutrho/ecutwfc
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, dual=dual )
+    println(Ham)
+
+    # XXX: This should take into account the overlap operator
+    # XXX: Currently this is handled in _prepare_scf (called electrons_scf)
+    psiks = rand_BlochWavefunc(Ham)
+
+    # XXX: Only this solver is currently implemented
+    electrons_scf!(Ham, psiks)
+end
+
+@time main()
+@time main()
