0d4fb399118aa6fc59d05acc6ce474b810776229
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-06-28 06:46:22 +0700

    - Reformatting. - Initial implementation in case overlap is needed (not yet tested).

--- src/gen_wavefunc.jl
+++ src/gen_wavefunc.jl
@@ -20,11 +20,9 @@ function zeros_BlochWavefunc( pw::PWGrid, Nstates::Int64, Nspin::Int64)
 
     psiks = BlochWavefunc(undef,Nkspin)
 
-    for ispin = 1:Nspin
-    for ik = 1:Nkpt
+    for ispin in 1:Nspin, ik in 1:Nkpt
         ikspin = ik + (ispin-1)*Nkpt
         psiks[ikspin] = zeros(ComplexF64,Ngw[ik],Nstates)
-    end
     end
 	return psiks
 end
@@ -36,7 +34,24 @@ end
 # -----------------------------------------------------------------------------
 
 function rand_BlochWavefunc( Ham::Hamiltonian )
-    return rand_BlochWavefunc( Ham.pw, Ham.electrons.Nstates, Ham.electrons.Nspin )
+    # Overlap is not needed
+    if !Ham.need_overlap
+        return rand_BlochWavefunc( Ham.pw, Ham.electrons.Nstates, Ham.electrons.Nspin )
+    end
+
+    # Overlap is needed
+    Nkpt = pw.gvecw.kpoints.Nkpt
+    Ngw = pw.gvecw.Ngw
+    Nkspin = Nspin*Nkpt
+    psiks = BlochWavefunc(undef,Nkspin)
+    for ispin in 1:Nspin, ik in 1:Nkpt
+        Ham.ispin = ispin
+        Ham.ik = ik
+        ikspin = ik + (ispin-1)*Nkpt
+        psiks[ikspin] = rand_Wavefunc(Ngw[ik], Nstates)
+        ortho_sqrt!(Ham, psiks[ikspin])
+    end
+    return psiks
 end
 
 function rand_BlochWavefunc( pw::PWGrid, electrons::Electrons )
@@ -53,14 +68,11 @@ function rand_BlochWavefunc( pw::PWGrid, Nstates::Int64, Nspin::Int64 )
 
     psiks = BlochWavefunc(undef,Nkspin)
 
-    for ispin = 1:Nspin
-    for ik = 1:Nkpt
+    for ispin in 1:Nspin, ik in 1:Nkpt
         ikspin = ik + (ispin-1)*Nkpt
-        psiks[ikspin] = rand_Wavefunc(Ngw[ik],Nstates)
+        psiks[ikspin] = rand_Wavefunc(Ngw[ik], Nstates)
     end
-    end
-
-	return psiks
+    return psiks
 end
 
 function rand_Wavefunc( Nbasis, Nstates )
@@ -72,6 +84,9 @@ end
 #
 function rand_wfc( Ham::Hamiltonian )
 
+    # USPP and PAW cases are not yet tested
+    @assert !Ham.need_overlap
+
     Nstates = Ham.electrons.Nstates
     idx_gw2g = Ham.pw.gvecw.idx_gw2g
     G = Ham.pw.gvec.G
@@ -82,28 +97,30 @@ function rand_wfc( Ham::Hamiltonian )
 
     psiks = zeros_BlochWavefunc(Ham)
 
-    for ispin = 1:Nspin
-    for ik = 1:Nkpt
+    for ispin in 1:Nspin, ik in 1:Nkpt
         ikspin = ik + (ispin - 1)*Nkpt
-        for ist = 1:Nstates
-            for igk = 1:Ngw[ik]
-                ig = idx_gw2g[ik][igk]
-                rr = rand()
-                arg = 2*pi*rand()
-                num = rr*cos(arg) + im*rr*sin(arg)
-                Gw2 = (G[1,ig] + k[1,ik])^2 + (G[2,ig] + k[2,ik])^2 + (G[3,ig] + k[3,ik])^2
-                denum = Gw2 + 1.0
-                psiks[ikspin][igk,ist] = num/denum
-            end
+        for ist in 1:Nstates, igk in 1:Ngw[ik]
+            ig = idx_gw2g[ik][igk]
+            rr = rand()
+            arg = 2*pi*rand()
+            num = rr*cos(arg) + im*rr*sin(arg)
+            Gk1 = G[1,ig] + k[1,ik]
+            Gk2 = G[2,ig] + k[2,ik]
+            Gk3 = G[3,ig] + k[3,ik]
+            Gw2 = Gk1^2 + Gk2^2 + Gk3^2
+            denum = Gw2 + 1.0
+            psiks[ikspin][igk,ist] = num/denum
         end
         Ham.ispin = ispin
         Ham.ik = ik
         ortho_sqrt(psiks[ikspin])
         Hr = Hermitian(psiks[ikspin]' * op_H(Ham, psiks[ikspin]))
         evals, evecs = eigen(Hr)
+        
+        # Need to set this?
         #Ham.electrons.ebands[:,ik] = evals
-        psiks[ikspin] = psiks[ikspin]*evecs
-    end
+
+        psiks[ikspin] *= evecs # rotate
     end
 
     return psiks
