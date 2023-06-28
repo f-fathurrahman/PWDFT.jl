0ac5bfc17d30f385af7ea7f6d8635b4ec83f47e1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-06-28 19:23:20 +0700

    Use DIR_PSP instead of DIR_PSP_GTH_LDA

--- test/run_one_file.jl
+++ test/run_one_file.jl
@@ -5,7 +5,7 @@ using Test
 
 const DIR_PWDFT = joinpath( dirname(pathof(PWDFT)), "..")
 const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
-const DIR_PSP_GTH_LDA = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials")
 
 include("create_objects.jl")
 
