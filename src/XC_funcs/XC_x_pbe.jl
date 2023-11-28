25004d3965bb6e34a5eda82a7b287023cb58145b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-11-28 07:19:08 +0700

    Use threshold and minor change in naming variable

--- src/XC_funcs/XC_x_pbe.jl
+++ src/XC_funcs/XC_x_pbe.jl
@@ -2,8 +2,13 @@
 # iflag=1  J.P.Perdew, K.Burke, M.Ernzerhof, PRL 77, 3865 (1996)
 function XC_x_pbe( rho, grho )
 
-    SMALL = 1e-13
-    if rho <= SMALL
+    # From xc_gga_drivers.f90
+    SMALL1 = 1e-6 # rho_threshold
+    SMALL2 = 1e-10 # grho_threshold
+    if rho <= SMALL1
+        return 0.0, 0.0, 0.0
+    end
+    if grho <= SMALL2
         return 0.0, 0.0, 0.0
     end
 
@@ -27,16 +32,18 @@ function XC_x_pbe( rho, grho )
     f3 = k / f2
     fx = k - f3
     exunif = -c1 * kf
-    sx = exunif * fx
-
-    # Potential
+ 
+    sx_s = exunif * fx
+    
     dxunif = exunif * third
+    ds = -c5 * s1
+    
     dfx1 = f2 * f2
     dfx = 2.0 * mu * s1 / dfx1
     
-    v1x = sx + dxunif * fx + exunif * dfx * ds
+    v1x = sx_s + dxunif * fx + exunif * dfx * ds
     v2x = exunif * dfx * dsg / agrho
-    sx = sx * rho
+    sx  = sx_s * rho
 
     return sx, v1x, v2x
 end
