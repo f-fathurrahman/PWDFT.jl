ea8b43bde3c153bfe60d6650a8d5c8dadf01d493
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-09-11 19:21:19 +0700

    Debug

--- src/SymmetryInfo.jl
+++ src/SymmetryInfo.jl
@@ -854,32 +854,47 @@ function symmetrize_matrix!( LatVecs_, sym_info::SymmetryInfo, M )
         return
     end
 
+    work = zeros(Float64, 3, 3)
+
     alat = norm(LatVecs_[:,1])
     LatVecs = LatVecs_[:,:]/alat
     RecVecs = inv(Matrix(LatVecs'))
+    @info LatVecs * RecVecs'
 
     # bring matrix to crystal axis
     # CALL cart_to_crys( matr )
-    M_cryst = zeros(Float64, 3, 3)
-    for i in 1:3
-        M_cryst[:,i] = M[1,i]*LatVecs[1,:] + M[2,i]*LatVecs[2,:] + M[3,i]*LatVecs[3,:]
+    @info "Original M"
+    display(M)
+
+    fill!(work, 0.0)
+    for i in 1:3, j in 1:3, k in 1:3, l in 1:3
+        work[i,j] += M[k,l] * LatVecs[k,i] * LatVecs[l,j]
     end
+    M[:,:] .= work[:,:]
+
     # XXX: Check this!
+    @info "M"
+    display(M)
 
     s = convert(Array{Float64,3}, sym_info.s)
     # symmetrize in crystal axis
-    work = zeros(Float64, 3, 3)
+    fill!(work, 0.0)
     for isym in 1:Nsyms
         for i in 1:3, j in 1:3, k in 1:3, l in 1:3
-            work[i,j] += s[i,k,isym] * s[j,l,isym] * M_cryst[k,l]
+            work[i,j] += s[i,k,isym] * s[j,l,isym] * M[k,l]
         end
     end
-    M_cryst[:,:] = work[:,:] / Nsyms
+    M[:,:] = work[:,:] / Nsyms
     
     # bring matrix back to cartesian axis
-    for i in 1:3
-        M[:,i] = M_cryst[1,i]*RecVecs[:,1] + M_cryst[2,i]*RecVecs[:,2] + M_cryst[3,i]*RecVecs[:,3]
+    fill!(work, 0.0)
+    for i in 1:3, j in 1:3, k in 1:3, l in 1:3
+        work[i,j] += M[k,l] * RecVecs[i,k] * RecVecs[j,l]
     end
+    M[:,:] .= work[:,:]
+
+    @info "After M = "
+    display(M)
 
     return
 end
