eae7bf96cf0ca563793f4d79fe9df319ec8573f0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-01-03 06:25:17 +0700

    Fix for stack overflow error

--- src/LDA_VWN.jl
+++ src/LDA_VWN.jl
@@ -22,7 +22,7 @@ function calc_epsxc_Vxc_VWN!(
     Npoints = size(Rhoe, 1)
 
     if Nspin == 1
-        calc_epsxc_Vxc_VWN!(xc_calc, Rhoe, epsxc, Vxc)
+        calc_epsxc_Vxc_VWN!(xc_calc, Rhoe[:,1], epsxc, Vxc[:,1])
         return
     end
 
