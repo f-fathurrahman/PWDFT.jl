db03af0330e73ee330945dee9995cd38a6ca1528
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2026-09-17 16:49:51 +0700

    Simplify

--- src/op_H.jl
+++ src/op_H.jl
@@ -2,46 +2,12 @@
 # or Array{ComplexF64,2} (multicolumn)
 # This is handled by individual op_* functions
 function op_H( Ham::Hamiltonian, psi )
-    # XXX This can be simplified, just call op_H! ?
-    if isnothing(Ham.exx)
-        if Ham.pspotNL.NbetaNL > 0
-            return op_K( Ham, psi ) + op_V_loc( Ham, psi ) + op_V_Ps_nloc( Ham, psi ) + op_Vtau( Ham, psi )
-        else
-            return op_K( Ham, psi ) + op_V_loc( Ham, psi ) + op_Vtau( Ham, psi )
-        end
-    else
-        if Ham.exx.is_active
-            if Ham.pspotNL.NbetaNL > 0
-                return op_K( Ham, psi ) + op_V_loc( Ham, psi ) + op_V_Ps_nloc( Ham, psi ) + op_Vtau( Ham, psi ) + op_Vexx(Ham, psi)
-            else
-                return op_K( Ham, psi ) + op_V_loc( Ham, psi ) + op_Vtau( Ham, psi ) + op_Vexx(Ham, psi)
-            end
-        else
-            if Ham.pspotNL.NbetaNL > 0
-                return op_K( Ham, psi ) + op_V_loc( Ham, psi ) + op_V_Ps_nloc( Ham, psi ) + op_Vtau( Ham, psi )
-            else
-                return op_K( Ham, psi ) + op_V_loc( Ham, psi ) + op_Vtau( Ham, psi )
-            end 
-        end
-    end
+    Hpsi = similar(psi)
+    op_H!(Ham, psi, Hpsi)
+    return Hpsi
 end
 
-function zero_out!( psiks::BlochWavefunc )
-    N = length(psiks)
-    for i in 1:N
-        zero_out!(psiks[i])
-    end
-    return
-end
-
-function zero_out!( psi::Array{ComplexF64,2} )
-    N = length(psi)
-    for i in 1:N
-        psi[i] = 0.0 + im*0.0
-    end
-    return
-end
-
+#XXX Need this?
 import Base: fill!
 function fill!( psiks::BlochWavefunc, x )
     N = length(psiks)
